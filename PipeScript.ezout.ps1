337b219a06cccbd7a980c056284c31718e0ca281
James Brundage <@github.com>
2022-06-20 20:14:08 -0700

    Adding .ezout file (#18 , #23, #29)

---
+++ PipeScript.ezout.ps1
@@ -0,0 +1,37 @@
+#requires -Module EZOut
+#  Install-Module EZOut or https://github.com/StartAutomating/EZOut
+$myFile = $MyInvocation.MyCommand.ScriptBlock.File
+$myModuleName = 'PipeScript'
+$myRoot = $myFile | Split-Path
+Push-Location $myRoot
+$formatting = @(
+    # Add your own Write-FormatView here,
+    # or put them in a Formatting or Views directory
+    foreach ($potentialDirectory in 'Formatting','Views') {
+        Join-Path $myRoot $potentialDirectory |
+            Get-ChildItem -ea ignore |
+            Import-FormatView -FilePath {$_.Fullname}
+    }
+)
+
+$destinationRoot = $myRoot
+
+if ($formatting) {
+    $myFormatFile = Join-Path $destinationRoot "$myModuleName.format.ps1xml"
+    $formatting | Out-FormatData -Module $MyModuleName | Set-Content $myFormatFile -Encoding UTF8
+}
+
+$types = @(
+    # Add your own Write-TypeView statements here
+    # or declare them in the 'Types' directory
+    Join-Path $myRoot Types |
+        Get-Item -ea ignore |
+        Import-TypeView
+
+)
+
+if ($types) {
+    $myTypesFile = Join-Path $destinationRoot "$myModuleName.types.ps1xml"
+    $types | Out-TypeData | Set-Content $myTypesFile -Encoding UTF8
+}
+Pop-Location
