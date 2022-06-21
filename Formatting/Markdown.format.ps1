ae5c43e143cc8a8d03c3692bead7db1e1b308d97
James Brundage <@github.com>
2022-06-20 20:29:01 -0700

    Adding Markdown Formatting (#40).  Adding Inline OpenSCAD (#41)

---
+++ Formatting/Markdown.format.ps1
@@ -0,0 +1,17 @@
+Write-FormatView -TypeName Markdown -Action {
+    $thisObject = $_
+    if ($thisObject -is [string]) {        
+        $thisObject        
+    }
+    elseif ($thisObject.Table) {
+        $thisObject.Table | Format-Markdown
+    }
+    elseif ($thisObject.InputObject) {
+        $thisObject.InputObject | Format-Markdown
+    }
+    elseif ($thisObject.psobject.Properties.Length) {
+        $thisObject | Format-Markdown
+    } else {
+        ''
+    }    
+}
---
+++ Transpilers/Inline/Inline.OpenSCAD.psx.ps1
@@ -0,0 +1,76 @@
+<#
+.SYNOPSIS
+    OpenSCAD Inline PipeScript Transpiler.
+.DESCRIPTION
+    Transpiles OpenSCAD with Inline PipeScript into OpenSCAD.
+
+    Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
+
+    Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
+
+    This for Inline PipeScript to be used with operators, and still be valid OpenSCAD syntax. 
+
+    The OpenSCAD Inline Transpiler will consider the following syntax to be empty:
+    
+    * ```"[^"]+"```
+    * ```[\d\.]+```
+.EXAMPLE
+    .> {
+        $OpenScadWithInlinePipeScript = @'
+Shape = "cube" /*{'"cube"', '"sphere"', '"circle"' | Get-Random}*/;
+Size  = 1 /*{Get-Random -Min 1 -Max 100}*/ ;
+
+if (Shape == "cube") {
+    cube(Size);
+}
+if (Shape == "sphere") {
+    sphere(Size);
+}
+if (Shape == "circle") {
+    circle(Size);
+}
+'@
+
+        [OutputFile(".\RandomShapeAndSize.ps1.scad")]$OpenScadWithInlinePipeScript
+    }
+    
+    .> .\RandomShapeAndSize.ps1.scad
+#>
+[ValidateScript({
+    $cmdInfo = $_
+    if ($cmdInfo.Source -match '\.scad$') {
+        return $true
+    }
+    return $false    
+})]
+param(
+# The command information.  This will include the path to the file.
+[Parameter(Mandatory,ValueFromPipeline)]
+$CommandInfo
+)
+
+begin {
+    # We start off by declaring a number of regular expressions:
+    $startComment = '/\*' # * Start Comments ```\*```
+    $endComment   = '\*/' # * End Comments   ```/*```
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * IgnoredContext ```String.empty```, ```null```, blank strings and characters
+    $IgnoredContext = "(?<ignore>(?>$('[\d\.]+','"[^"]+"' -join '|'))\s{0,}){0,1}"
+    # * StartRegex     ```$IgnoredContext + $StartComment + '{' + $Whitespace```
+    $startRegex = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
+    # * EndRegex       ```$whitespace + '}' + $EndComment + $ignoredContext```
+    $endRegex   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+
+    $sourcePattern  = [Regex]::New("(?>$(
+        $startRegex, $endRegex -join ([Environment]::NewLine + '|' + [Environment]::NewLine)
+    ))", "IgnoreCase, IgnorePatternWhitespace", "00:00:05")
+}
+
+process {
+    
+    $fileInfo = $commandInfo.Source -as [IO.FileInfo]
+    $fileText      = [IO.File]::ReadAllText($fileInfo.Fullname)
+
+    .>PipeScript.Inline -SourceFile $CommandInfo.Source -SourceText $fileText -SourcePattern $sourcePattern    
+}
+
