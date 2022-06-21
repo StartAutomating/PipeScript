b01b6cd3bc8e7256e974f583acb11d1b8596409d
James Brundage <@github.com>
2022-06-20 20:24:49 -0700

    Adding Inline Java (#36)

---
+++ Transpilers/Inline/Inline.Java.psx.ps1
@@ -0,0 +1,55 @@
+<#
+.SYNOPSIS
+    Java Inline PipeScript Transpiler.
+.DESCRIPTION
+    Transpiles Java with Inline PipeScript into Java.
+
+    Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
+
+    Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
+
+    This for Inline PipeScript to be used with operators, and still be valid JavaScript/TypeScript syntax. 
+
+    The Java Inline PipeScript Transpiler will consider the following syntax to be empty:
+
+    * ```null```
+    * ```""```
+    * ```''```
+#>
+[ValidateScript({
+    $cmdInfo = $_
+    if ($cmdInfo.Source -match '\.(?>java)$') {
+        return $true
+    }
+    return $false    
+})]
+param(
+# The command information.  This will include the path to the file.
+[Parameter(Mandatory,ValueFromPipeline)]
+$CommandInfo
+)
+
+begin {
+    # We start off by declaring a number of regular expressions:
+    $startComment = '/\*' # * Start Comments ```\*```
+    $endComment   = '\*/' # * End Comments   ```/*```
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * IgnoredContext ```String.empty```, ```null```, blank strings and characters
+    $IgnoredContext = "(?<ignore>(?>$("null", '""', "''" -join '|'))\s{0,}){0,1}"
+    # * StartRegex     ```$IgnoredContext + $StartComment + '{' + $Whitespace```
+    $startRegex = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
+    # * EndRegex       ```$whitespace + '}' + $EndComment + $ignoredContext```
+    $endRegex   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+
+    $sourcePattern  = [Regex]::New("(?>$(
+        $startRegex, $endRegex -join ([Environment]::NewLine + '|' + [Environment]::NewLine)
+    ))", "IgnoreCase, IgnorePatternWhitespace", "00:00:05")
+}
+
+process {
+    
+    $fileInfo = $commandInfo.Source -as [IO.FileInfo]
+    $fileText      = [IO.File]::ReadAllText($fileInfo.Fullname)
+
+    .>PipeScript.Inline -SourceFile $CommandInfo.Source -SourceText $fileText -SourcePattern $sourcePattern
+}
