0866b0706e00ed10feba8bd61d41ce13a2ca8a68
James Brundage <@github.com>
2022-07-02 14:32:36 -0700

    Updating Module Source (Fixing #100)

--- PipeScript.ps1.psm1
+++ PipeScript.ps1.psm1
@@ -8,7 +8,9 @@ foreach ($transpilerCmd in Get-Transpiler) {
     Set-Alias ".<$($transpilerCmd.DisplayName)>" Use-PipeScript
 }
 
+$MyModule = $MyInvocation.MyCommand.ScriptBlock.Module
 foreach ($cmd in $ExecutionContext.SessionState.InvokeCommand.GetCommands('*','Function', $true)) {
+    if ($cmd.ScriptBlock.Module -ne $MyModule) { continue }
     if ($cmd.ScriptBlock.Attributes.AliasNames) {
         $aliasNames += $cmd.ScriptBlock.Attributes.AliasNames
     }
