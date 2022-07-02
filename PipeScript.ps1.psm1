511efa4378c4db726e403dd4b0442dc4274ea76c
James Brundage <@github.com>
2022-07-02 13:03:29 -0700

    Adding PipeScript.ps1.psm1 (#95)

---
+++ PipeScript.ps1.psm1
@@ -0,0 +1,17 @@
+﻿[Include('*-*')]$psScriptRoot
+
+$aliasNames = @()
+foreach ($transpilerCmd in Get-Transpiler) {
+    $aliasNames += ".>$($transpilerCmd.DisplayName)"
+    Set-Alias ".>$($transpilerCmd.DisplayName)" Use-PipeScript
+    $aliasNames += ".<$($transpilerCmd.DisplayName)>"
+    Set-Alias ".<$($transpilerCmd.DisplayName)>" Use-PipeScript
+}
+
+foreach ($cmd in $ExecutionContext.SessionState.InvokeCommand.GetCommands('*','Function', $true)) {
+    if ($cmd.ScriptBlock.Attributes.AliasNames) {
+        $aliasNames += $cmd.ScriptBlock.Attributes.AliasNames
+    }
+}
+
+Export-ModuleMember -Function * -Alias $aliasNames
