b15c78de2fe672c9581acad67d164c5e3d543709
James Brundage <@github.com>
2022-08-05 13:33:48 -0700

    CommandAST: Fixing .GetParameter (Fixes #175)

--- Types/CommandAST/get_Parameter.ps1
+++ Types/CommandAST/get_Parameter.ps1
@@ -1,21 +1,22 @@
+$commandAst = $this
 $NamedParameters = [Ordered]@{}
 $parameterAstType = [Management.Automation.Language.CommandParameterAst]
 
 for (
     $commandElementIndex = 1
-    $commandElementIndex -lt $this.CommandElements.Count
+    $commandElementIndex -lt $commandAst.CommandElements.Count
     $commandElementIndex++
 )
 {
-    $commandElement = $this.CommandElements[$commandElementIndex]
-    $nextElement    = $this.CommandElements[$commandElementIndex + 1]
+    $commandElement = $commandAst.CommandElements[$commandElementIndex]
+    $nextElement    = $commandAst.CommandElements[$commandElementIndex + 1]
     if ($commandElement -is $parameterAstType) {
         if ($commandElement.Argument) {            
             $NamedParameters[$commandElement.ParameterName] =                 
                 $commandElement.Argument.ConvertFromAst()
         } elseif ($nextElement -and $nextElement -isnot $parameterAstType)  {
             $NamedParameters[$commandElement.ParameterName] = 
-                $nextElement.Argument.ConvertFromAst()
+                $nextElement.ConvertFromAst()
             $commandElementIndex++
         } else {
             $NamedParameters[$commandElement.ParameterName] = $true
