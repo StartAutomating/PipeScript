f62cffe240442c66cb3f9df5fca25f508f740bd1
James Brundage <@github.com>
2022-08-13 16:24:13 -0700

    [CommandAst].ResolvedCommand:  Checking transpilers first (Fixes #186)

--- Types/CommandAST/get_ResolvedCommand.ps1
+++ Types/CommandAST/get_ResolvedCommand.ps1
@@ -1,1 +1,14 @@
-$ExecutionContext.SessionState.InvokeCommand.GetCommand($this.CommandElements[0].ToString(), 'All')
+
+$commandName = $this.CommandElements[0].ToString()
+$foundTranspiler = Get-Transpiler -TranspilerName $commandName
+if ($foundTranspiler) {
+    foreach ($transpiler in $foundTranspiler) {
+        if ($transpiler.Validate($this)) { 
+            $transpiler
+        }
+    }
+} else {
+    $ExecutionContext.SessionState.InvokeCommand.GetCommands($commandName, 'All', $true)
+}
+
+
