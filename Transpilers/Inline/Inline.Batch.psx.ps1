87c579da46fe09f0c0e784aff5992a500ad9d5dd
James Brundage <@github.com>
2022-09-07 18:39:27 -0700

    Inline Batch support: improving inner docs (re #198)

--- Transpilers/Inline/Inline.Batch.psx.ps1
+++ Transpilers/Inline/Inline.Batch.psx.ps1
@@ -62,16 +62,15 @@ $ArgumentList
 begin {
     # We start off by declaring a number of regular expressions:
     $startComment = '(?>(?>\:\:|rem)\s{0,}(?:PipeScript)?\s{0,}\{)'
-    $endComment   = '(?>(?>\:\:|rem)\s{0,}(?:PipeScript)?\s{0,}\})'    
-    # * StartRegex     ```$StartComment + '{' + $Whitespace```
+    $endComment   = '(?>(?>\:\:|rem)\s{0,}(?:PipeScript)?\s{0,}\})'        
     $startRegex = "(?<PSStart>${startComment})"
-    # * EndRegex       ```$whitespace + '}' + $EndComment```
     $endRegex   = "(?<PSEnd>${endComment})"
 
     # Create a splat containing arguments to the core inline transpiler
     $Splat      = [Ordered]@{
         StartPattern  = $startRegex
         EndPattern    = $endRegex
+         # Using -LinePattern will skip any inline code not starting with :: or rem.
         LinePattern   = "^\s{0,}(?>\:\:|rem)\s{0,}"
     }
 }
