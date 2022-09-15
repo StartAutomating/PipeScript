fbac4a4573cd441b3d537a82cca12fe28073d79f
James Brundage <@github.com>
2022-09-14 19:36:53 -0700

    Updating [ScriptBlock].Transpile() (throwing) (Fixes #212)

--- Types/ScriptBlock/Transpile.ps1
+++ Types/ScriptBlock/Transpile.ps1
@@ -1,1 +1,23 @@
-$this | .>PipeScript
+$TranspilerErrors   = @()
+$TranspilerWarnings = @()
+
+$ErrorsAndWarnings  = @{ErrorVariable='TranspilerErrors';WarningVariable='TranspilerWarnings'}
+$this | .>PipeScript @ErrorsAndWarnings
+
+if ($TranspilerErrors) {
+    $failedMessage = @(
+        foreach ($transpilerError in $TranspilerErrors) {
+            "$($transpilerError)"
+        }
+        "$($TranspilerErrors.Count) error(s)"
+        if ($transpilerWarnings) {
+            "$($TranspilerWarnings.Count) warning(s)"
+        }
+    ) -join ','
+    throw $failedMessage
+}
+elseif ($TranspilerWarnings) {
+    foreach ($TranspilerWarning in $TranspilerWarnings) {
+        Write-Warning "$TranspilerWarning "
+    }
+}
