9b84a819c23e3a2d813d02383a3aa2e89b1137a6
James Brundage <@github.com>
2022-10-20 19:03:07 -0700

    Adding Inline HAXE transpiler (Fixes #259)

---
+++ Transpilers/Inline/Inline.HAXE.psx.ps1
@@ -0,0 +1,63 @@
+<#
+.SYNOPSIS
+    Haxe PipeScript Transpiler.
+.DESCRIPTION
+    Transpiles Haxe with Inline PipeScript into Haxe.
+
+    Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
+
+    Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
+
+    This for Inline PipeScript to be used with operators, and still be valid Haxe syntax. 
+
+    The Haxe Inline Transpiler will consider the following syntax to be empty:
+
+    * ```null```
+    * ```""```
+    * ```''```
+#>
+[ValidatePattern('\.hx$')]
+param(
+# The command information.  This will include the path to the file.
+[Parameter(Mandatory,ValueFromPipeline)]
+[Management.Automation.CommandInfo]
+$CommandInfo,
+
+# A dictionary of parameters.
+[Collections.IDictionary]
+$Parameter,
+
+# A list of arguments.
+[PSObject[]]
+$ArgumentList
+)
+
+begin {
+    # We start off by declaring a number of regular expressions:
+    $startComment = '/\*' # * Start Comments ```\*```
+    $endComment   = '\*/' # * End Comments   ```/*```
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * IgnoredContext ```String.empty```, ```null```, blank strings and characters
+    $IgnoredContext = "(?<ignore>(?>$("null", '""', "''" -join '|'))\s{0,}){0,1}"
+    # * StartRegex     ```$IgnoredContext + $StartComment + '{' + $Whitespace```
+    $startRegex = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
+    # * EndRegex       ```$whitespace + '}' + $EndComment + $ignoredContext```
+    $endRegex   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+
+    # Create a splat containing arguments to the core inline transpiler
+    $Splat      = [Ordered]@{
+        StartPattern  = $startRegex
+        EndPattern    = $endRegex
+    }
+}
+
+process {
+    # Add parameters related to the file
+    $Splat.SourceFile = $commandInfo.Source -as [IO.FileInfo]
+    $Splat.SourceText = [IO.File]::ReadAllText($commandInfo.Source)
+    if ($Parameter) { $splat.Parameter = $Parameter }
+    if ($ArgumentList) { $splat.ArgumentList = $ArgumentList }
+
+    # Call the core inline transpiler.
+    .>PipeScript.Inline @Splat
+}
