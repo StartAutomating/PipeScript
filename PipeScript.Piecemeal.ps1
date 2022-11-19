3609c8f1dafc1491892c9ad7a8a0f4ac81d5542a
James Brundage <@github.com>
2022-11-19 15:01:14 -0800

    Updating PipeScript.Piecemeal.ps1 (adding some docs)

--- PipeScript.Piecemeal.ps1
+++ PipeScript.Piecemeal.ps1
@@ -1,10 +1,14 @@
 #require -Module Piecemeal
-Push-Location $PSScriptRoot
 
+# Push to this directory
+Push-Location $PSScriptRoot 
+# Get-Transpiler is generated with Piecemeal
 Install-Piecemeal -ExtensionNoun 'Transpiler' -ExtensionPattern '\.psx\.ps1$' -ExtensionTypeName 'PipeScript.Transpiler' -OutputPath '.\Get-Transpiler.ps1' |
-    Add-Member Noteproperty CommitMessage "Get-Transpiler: Updating Piecemeal Version" -Force -PassThru 
+    Add-Member Noteproperty CommitMessage "Get-Transpiler: Updating Piecemeal Version" -Force -PassThru
+
+# So is Get-PipeScript
 Install-Piecemeal -ExtensionNoun 'PipeScript' -ExtensionPattern '\.psx\.ps1{0,1}$','\.ps1{0,1}\.(?<ext>[^.]+$)','\.ps1{0,1}$' -ExtensionTypeName 'PipeScript' -OutputPath '.\Get-PipeScript.ps1' |
     Add-Member Noteproperty CommitMessage "Get-PipeScript: Updating Piecemeal Version" -Force -PassThru
 
-
+# Pop back to wherever we were
 Pop-Location
