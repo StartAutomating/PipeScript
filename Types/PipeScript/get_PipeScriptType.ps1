5d2e8e2c947ab597c6061abb4c2a9a12fce3da3a
James Brundage <@github.com>
2022-12-24 00:30:17 -0800

    PipeScript.PipeScriptType:  Adding 'ExtensionScript' categorization (Fixes #313)

--- Types/PipeScript/get_PipeScriptType.ps1
+++ Types/PipeScript/get_PipeScriptType.ps1
@@ -4,8 +4,11 @@ if ($this.Source -match '\.psx\.ps1{0,1}$') {
 elseif ($this.Source -match "\.ps1{0,1}\.(?<ext>[^.]+$)") {
     "SourceGenerator"
 }
-elseif (($this.Source -match '\.[^\.\\/]+\.ps1$') -or ($this.Source -match 'build\.ps1$')) {
+elseif ($this.Source -match '(?<=(?>^|\.))build\.ps1$') {
     "BuildScript"
+}
+elseif (($this.Source -match '\.[^\.\\/]+\.ps1$')) {
+    "ExtensionScript"
 }
 elseif ($this.Source) {
     "PipeScriptFile"
