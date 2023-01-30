1565d36ee45ff33295543c374e1726a7273035c1
James Brundage <@github.com>
2023-01-29 16:38:45 -0800

    Adding [CommandInfo].get_CommandNamespace (Fixes #335)

---
+++ Types/CommandInfo/get_CommandNamespace.ps1
@@ -0,0 +1,13 @@
+if ($this.Namespace) {
+    $this.Namespace
+} else {
+    $matched = $this.NamespaceSeparator.Match($this.Name)
+    if ($matched.Success -and $matched.Index -gt 0) {
+        $this.Name.Substring(0,$matched.Index)
+    }
+    else {
+        ''
+    }
+}
+
+
