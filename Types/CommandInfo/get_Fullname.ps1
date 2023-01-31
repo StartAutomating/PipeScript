29c0d6d1179c7d261d640b3185678fe2abdbe808
James Brundage <@github.com>
2023-01-30 17:01:10 -0800

    [CommandInfo].FullName (Adding .Root support)

--- Types/CommandInfo/get_Fullname.ps1
+++ Types/CommandInfo/get_Fullname.ps1
@@ -1,9 +1,9 @@
 if ($this -is [Management.Automation.ExternalScriptInfo] -or 
     $this -is [Management.Automation.ApplicationInfo]) {
     if ($this.Root -and 
-        $this.Source.StartsWith -and 
+        $this.Source.StartsWith -and
         $this.Source.StartsWith($this.Root, "OrdinalIgnoreCase")) {
-        $this.Source.Substring($this.Root)
+        $this.Source.Substring($this.Root.Length)
     } else {
         $this.Source
     }    
