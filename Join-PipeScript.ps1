760355fe34ce3c1844451b9d6133fa2ea710e114
James Brundage <@github.com>
2023-03-04 17:41:27 -0800

    Join-PipeScript: Adding -Indent (Fixes #365)

--- Join-PipeScript.ps1
+++ Join-PipeScript.ps1
@@ -62,7 +62,13 @@ function Join-PipeScript
     # A list of parameters to exclude.  Can contain wildcards.
     # Excluded parameters with default values will declare the default value at the beginnning of the command.
     [string[]]
-    $ExcludeParameter
+    $ExcludeParameter,
+
+    # The amount of indentation to use for parameters and named blocks.  By default, four spaces.
+    [ValidateRange(0,12)]
+    [Alias('Indentation')]
+    [int]
+    $Indent = 4
     )
 
     begin {
@@ -176,14 +182,16 @@ function Join-PipeScript
                 $allHeaderBlocks = @(
                     # If we're including -Help and we have $combinedHelp
                     if ($IncludeBlockType -contains 'Help' -and $combinedHelp) {                    
-                        "<#"  # create a comment block               
-                        $combinedHelp -join [Environment]::NewLine # and join the content.
-                        '#>'                        
+                        (' ' * $Indent) + "<#"  # create a comment block               
+                        $combinedHelp -split '(?>\r\n|\n)' -join (' ' * $Indent) + [Environment]::NewLine # and join the content.
+                        (' ' * $Indent) + '#>'
                     }
                 ) + @(
                     # If we're including headers, add the rest of the headers.
                     if ($IncludeBlockType -contains 'Header') {
-                        $restOfHeaders
+                        (' ' * $Indent) + (
+                            $restOfHeaders -split '(?>\r\n|\n)' -join (' ' * $Indent) + [Environment]::NewLine
+                        )
                     }
                 )
 
@@ -314,6 +322,10 @@ function Join-PipeScript
                                     $parameter.Parent.Extent.ToString().Substring($relativeOffset,$distance) -replace '^[\,\s\r\n]+'
                                 }
                             
+                            $inlineParameterHelp = $inlineParameterHelp -split '(?>\r\n|\n)' -join (
+                                (' ' * $indent) + [Environment]::NewLine
+                            )
+
                             # Then output the included parameter.
                             $alreadyIncludedParameter[$variableName]  = $inlineParameterHelp + $parameter.Extent.ToString()
                             $alreadyIncludedParameter[$variableName]
@@ -332,8 +344,10 @@ function Join-PipeScript
                     }                                        
                 }) 
                 
-                # Join all parameters by a comma and two newlines.
-                $paramOut -notmatch '^[\s\r\n]$' -join (',' + ([Environment]::NewLine * 2))
+                # Join all parameters by a comma, two newlines, and four indented spaces.
+                (' ' * $Indent) + (
+                    $paramOut -notmatch '^[\s\r\n]$' -join (',' + ([Environment]::NewLine * 2) + (' ' * $Indent))
+                )
                 
                 # and close out the parameter block.
                 if (@($AllScriptBlocks.Ast.ParamBlock) -ne $null) {
@@ -368,10 +382,14 @@ function Join-PipeScript
                             $blockOpen = $true # open it
                             if ($StatementsToAdd) {
                                 $StatementsToAdd -join [Environment]::NewLine
-                                $block.Extent.ToString() -replace "^$blockName\s{0,}\{" -replace '\}$'
+                                (' ' * $indent) + (
+                                    $block.Extent.ToString() -replace "^$blockName\s{0,}\{" -replace '\}$'
+                                )
                                 $StatementsToAdd = $null
                             } else {
-                                ($block | IndentAst) -replace '\}$' # and remove trailing curly braces.
+                                (' ' * $indent) + (
+                                    ($block | IndentAst) -replace '\}$' # and remove trailing curly braces.
+                                )
                             }                       
                             
                         } else {
@@ -379,7 +397,7 @@ function Join-PipeScript
                             $block.Extent.ToString() -replace "^$blockName\s{0,}\{" -replace '\}$'
                         }
                     }
-                    ' ' * ($block | MeasureIndent) + '}'
+                    ' ' * ([Math]::Max($indent,($block | MeasureIndent))) + '}'
                 }
             }
 
