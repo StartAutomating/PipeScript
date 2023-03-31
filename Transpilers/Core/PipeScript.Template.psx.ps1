ac31383659513b66a82259e9d0003968d69b3f15
James Brundage <@github.com>
2023-03-30 20:06:32 -0700

    PipeScript Template Transpiler - Fixing #392
    No longer using dot within core template transpiler

--- Transpilers/Core/PipeScript.Template.psx.ps1
+++ Transpilers/Core/PipeScript.Template.psx.ps1
@@ -463,28 +463,64 @@ process {
         $languageString = $($foundTemplateTranspiler.ExtensionCommand.DisplayName -replace '(?>Template|Inline)\.' -replace '^\.')
         if (-not $TemplateName) { $TemplateName = $languageString }
         $createdSb = [scriptblock]::Create(@"
-.Name = '$($TemplateName -replace "'", "''")'.Trim() .Language = '$languageString' .SourceFile = '' .PSTypeName = 'PipeScript.Template' .FilePattern = '$($filePattern -replace "'", "''")' .Pattern = (
-    [regex]::new('$($replacePattern -replace "'", "''")','IgnoreCase,IgnorePatternWhitespace', '00:00:05')
-) .Evaluator = {
-$replacementEvaluator
-} .GetInlineScript = {
-$GetInlineScript
-} .Evaluate = {
-$EvaluateTemplate
-} .Save = {
-$SaveTemplate
-} .ToString = {
-$TemplateToString
-} .Context = (New-Module -ScriptBlock {}
-) .ForeachObject = $(if ($ForeachObject) { "{ 
-$foreachObject
-}"} else {'$null'}) .Begin = $(if ($Begin) { "{ 
-$begin
-}"} else {'$null'}) .End = $(if ($End) { "{ 
-$end
-}"} else {'$null'}) .Template = $templateElements
-"@)
-        $createdSb | .>Pipescript        
+`$(
+`$templateObject = [PSCustomObject][Ordered]@{
+    PSTypeName = 'PipeScript.Template'
+    Name = '$($TemplateName -replace "'", "''")'
+    Language = '$languageString'
+    SourceFile = ''
+    FilePattern = '$($filePattern -replace "'", "''")'
+    Pattern = 
+       [regex]::new(@'
+$replacePattern
+'@,'IgnoreCase,IgnorePatternWhitespace', '00:00:05')
+    Context = New-Module -ScriptBlock {}
+    ForeachObject = $(if ($ForeachObject) { "{ 
+        $foreachObject
+    }"} else {'{}'})
+    Begin = $(if ($Begin) { "{ 
+        $begin
+    }"} else {'{}'})
+    End = $(if ($End) { "{ 
+        $end
+    }"} else {'{}'})
+    Template = $templateElements
+}
+`$templateObject.psobject.members.Add([PSScriptMethod]::new(
+    'Evaluator', {
+        $replacementEvaluator
+    }
+), `$true)
+`$templateObject.psobject.members.Add([PSScriptMethod]::new(
+    'Evaluate', {
+        $evaluateTemplate
+    }
+), `$true)
+`$templateObject.psobject.members.Add([PSScriptMethod]::new(
+    'GetInlineScript', {
+        $GetInlineScript
+    }
+), `$true)
+`$templateObject.psobject.members.Add([PSScriptMethod]::new(
+    'EvaluateTemplate', {
+        $EvaluateTemplate
+    }
+), `$true)
+`$templateObject.psobject.members.Add([PSScriptMethod]::new(
+    'SaveTemplate', {
+        $SaveTemplate
+    }
+), `$true)
+`$templateObject.psobject.members.Add([PSScriptMethod]::new(
+    'ToString', {
+        $TemplateToString
+    }
+), `$true)
+`$templateObject
+)
+"@
+)
+        $createdSb
         
         return        
     }
