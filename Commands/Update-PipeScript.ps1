ac65f7a412c3c8552349e8e69731c34bf194aec1
James Brundage <@github.com>
2023-04-12 14:09:37 -0700

    Update-PipeScript - Insertion Support
    Fixes #405, Fixes #406, Fixes #408

--- Commands/Update-PipeScript.ps1
+++ Commands/Update-PipeScript.ps1
@@ -88,10 +88,73 @@
     [Collections.IDictionary]
     $InsertAfter,
 
+    # A dictionary of items to insert after an AST element.
+    [ValidateScript({
+        $badKeys =
+            foreach ($k in $_.Keys) {
+                if ($k -isnot [Management.Automation.Language.Ast]) {
+                    $k
+                }
+            }
+        if ($badKeys) {
+            throw "-AstReplacement contains bad keys: $badKeys"
+        }
+        return $true
+    })]
+    [Collections.IDictionary]
+    $InsertAfterAST = [Ordered]@{},
+
+    # A dictionary of items to insert before an AST element.
+    [ValidateScript({
+        $badKeys =
+            foreach ($k in $_.Keys) {
+                if ($k -isnot [Management.Automation.Language.Ast]) {
+                    $k
+                }
+            }
+        if ($badKeys) {
+            throw "-AstReplacement contains bad keys: $badKeys"
+        }
+        return $true
+    })]
+    [Collections.IDictionary]
+    $InsertBeforeAst = [Ordered]@{},
+
+
+    # A dictionary of text insertions.
+    # The key is the insertion index.
+    # The value is the insertion.    
+    [ValidateScript({
+        if (-not $_.Count) { return $true }
+        $badKeys = $_.Keys -notmatch '^\d+$'
+        if ($badKeys) {
+            throw "-TextInsertion keys must be indexes: $badKeys"
+        }
+        return $true
+    })]
+    [Collections.IDictionary]
+    $TextInsertion = [Ordered]@{},
+
+    # A dictionary of regex based insertions
+    # This works similarly to -RegexReplacement.
+    [Alias('RegexInsertions')]
+    [Collections.IDictionary]    
+    $RegexInsertion = [Ordered]@{},
+
     # A collection of variables to rename.
     [Collections.IDictionary]
     [Alias('RenameParameter')]
     $RenameVariable,
+
+    # Content to append.
+    # Appended ScriptBlocks will be added to the last block of a ScriptBlock.
+    # Appended Text will be added to the end.
+    $Append,
+
+    # Content to prepend
+    # Prepended ScriptBlocks will be added to the first block of a ScriptBlock.
+    # Prepended text will be added at the start.
+    $Prepend,    
 
     # If set, will transpile the updated script block.
     [switch]
@@ -352,39 +415,6 @@
         }
         #endregion Replace Regions
 
-        #region Insert Before/After
-
-        # If we have any -InsertBefore 
-        if ($InsertBefore.Count) {
-            # Walk thru each insertion
-            foreach ($kv in @($InsertBefore.GetEnumerator())) {                
-                if (                    
-                    $kv.Value -is [string] -and # If the value was a string and
-                    -not $kv.Value.Contains('$&') # it did not include the substitution.
-                ) {
-                    $kv.Value += '$&' # subsitute the match at the end.
-                }
-                # Add our insertion to the regular expression replacements.
-                $RegexReplacement[$kv.Key] = $kv.Value
-            }
-        }
-
-        # If we had any -InsertAfter
-        if ($InsertAfter.Count) {
-            # Walk thru each insertion
-            foreach ($kv in @($InsertAfter.GetEnumerator())) {
-                if (                    
-                    $kv.Value -is [string] -and # If the value was a string and
-                    -not $kv.Value.Contains('$&') # it did not include the substitution.
-                ) {
-                    $kv.Value = '$&' + $kv.Value # subsitute the match at the start.
-                }
-                # Add our insertion to the regular expression replacements.
-                $RegexReplacement[$kv.Key] = $kv.Value
-            }
-        }
-        #endregion Insert Before/After
-
         #region Replace Regex
         if ($RegexReplacement.Count) {
             foreach ($repl in $RegexReplacement.GetEnumerator()) {
@@ -397,6 +427,249 @@
             }
         }
         #endregion Replace Regex
+        
+        #region Insertions
+
+        #region Append and Prepend
+        # Prepending and appending to text is easy:
+        if (-not $ScriptBlock) {
+            # Just prepend the text at the start
+            if ($Prepend) {
+                $TextInsertion["0"] = $Prepend -join [Environment]::NewLine
+            }
+
+            # and append anything at the end.
+            if ($append) {
+                $TextInsertion["$($text.Length - 1)"] = $Append -join [Environment]::NewLine
+            }
+        } else {
+            # If we're prepending or appending to a ScriptBlock,
+            $updatedScriptBlock = [ScriptBlock]::Create($updatedText)
+            # the challenge is figuring out where we append or prepend.
+            if ($updatedScriptBlock) {
+                $ScriptBlock = $updatedScriptBlock
+                $text = $updatedText = "$ScriptBlock"
+            }
+            
+            $ScriptBlockAst = $ScriptBlock.Ast
+            # If the [ScriptBlock] is a function definition, 
+            if ($ScriptBlockAst.Body) {
+                $ScriptBlockAst = $ScriptBlockAst.Body # look in it's body
+            }
+
+            # List all of the named blocks, in order
+            $potentialInsertLocations = @(
+                $ScriptBlockAst.ParamBlock,
+                $ScriptBlockAst.DynamicParamBlock,
+                $ScriptBlockAst.BeginBlock,
+                $ScriptBlockAst.ProcessBlock,
+                $ScriptBlockAst.EndBlock -ne $null
+            ) # (and force what is not null into an array).
+
+            # Walk thru each prepend
+            foreach ($pre in $Prepend) {
+                $potentialInsertAst = $null
+                $potentialInsert =
+                    # If we are prepending a ScriptBlock
+                    if ($pre -is [scriptblock]) {                        
+                        foreach ($potentialInsert in $potentialInsertLocations) {
+                            # it cannot go in the param block
+                            if ($potentialInsert -is [Management.Automation.Language.ParamBlockAst]) {
+                                continue
+                            }
+                            $potentialInsertAst = $potentialInsert
+                            # and we'd like to put it before the first statement in the first block.
+                            @($potentialInsert.Statements)[0]
+                            break                            
+                        }
+                    } else {
+                        # If we're prepending text, it should go before the block                        
+                        $potentialInsertAst = $potentialInsertLocations[0]
+                        $potentialInsertAst
+                    }
+
+                # If we could not find an insertion statement
+                if (-not $potentialInsert) {
+                    # the block is empty, so find it's start
+                    $match = [Regex]::new("$($potentialInsertAst.BlockKind)\s{0,}\{\s{0,}",'IgnoreCase').Match($Text,
+                        [Math]::Max(
+                            $potentialInsertAst.Extent.StartOffset - $ScriptBlockAst.StartOffset - 5, 0
+                        )
+                    )
+                    
+                    if ($match.Success) {
+                        # Turn this into an index, and then set -TextInsertion at that point.
+                        $insertAt = $match.Index + $match.Length
+                        if (-not $TextInsertion["$insertAt"]) {
+                            $TextInsertion["$insertAt"] = @($pre)
+                        } else {
+                            $TextInsertion["$insertAt"] += $pre
+                        }                        
+                    }
+                } else {
+                    # Otherwise, use -InsertBeforeAst
+                    if (-not $InsertBeforeAst[$potentialInsert]) {
+                        $InsertBeforeAst[$potentialInsert] = @($pre)
+                    } else {
+                        $InsertBeforeAst[$potentialInsert] += $pre
+                    }
+                }
+            }
+
+            # If we're appending
+            foreach ($app in $append) {
+                # Find the last statement in the last block
+                $statementList = @($potentialInsertLocations[-1].Statements)
+                $potentialInsert = $statementList[-1]
+                # If no statement was found, find the last block
+                if (-not $potentialInsert) {
+                    $match = [Regex]::new("$($potentialInsertLocations[-1].BlockKind)\s{0,}\{\s{0,}",'IgnoreCase').Match($Text,
+                        [Math]::Max(
+                            $potentialInsertLocations[-1].Extent.StartOffset - $ScriptBlockAst.StartOffset - 5, 0
+                        )
+                    )
+                    if ($match.Success) {
+                        # and use -TextInsertion at that location.
+                        $insertAt = $match.Index + $match.Length
+                        if (-not $TextInsertion["$insertAt"]) {
+                            $TextInsertion["$insertAt"] = @($app)
+                        } else {
+                            $TextInsertion["$insertAt"] += $app
+                        }                        
+                    }                    
+                } else {
+                    # If a statement was found, insert after that point.
+                    if (-not $InsertAfterAst[$potentialInsert]) {
+                        $InsertAfterAst[$potentialInsert] = $App
+                    } else {
+                        $InsertAfterAst[$potentialInsert] += $App
+                    }
+                }
+            }                            
+
+        }
+        #endregion Append and Prepend
+
+        #region Insert Before AST
+        # If we had any AST insertions
+        if ($InsertBeforeAst.Count) {
+            $myOffset  = 0
+            $index     = 0
+            # sort them by their start.
+            foreach ($item in @(
+                $InsertBeforeAst.GetEnumerator() |
+                Sort-Object { $_.Key.Extent.StartOffset } |
+                Select-Object -ExpandProperty Key
+            )) {
+                # and turn it into a text range
+                $start = $Text.IndexOf($item.Extent.Text, $myOffset)       
+                # then update the text replacements.
+                if (-not $TextInsertion["$($start - 1)"]) {
+                    $TextInsertion["$($start - 1)"] = @($InsertBeforeAst[$item])
+                } else {
+                    $TextInsertion["$($start - 1)"] += @($InsertBeforeAst[$item])
+                }
+                
+                $myOffset = $end
+            }
+        }
+        #endregion Insert Before AST
+
+        #region Insert After AST
+        # If we had any AST insertions
+        if ($InsertAfterAst.Count) {
+            $myOffset  = 0
+            $index     = 0
+            # sort them by their start.
+            foreach ($item in @(
+                $InsertAfterAST.GetEnumerator() |
+                Sort-Object { $_.Key.Extent.StartOffset } |
+                Select-Object -ExpandProperty Key
+            )) {
+                # and turn it into a text range
+                $start = $Text.IndexOf($item.Extent.Text, $myOffset)
+                $end   = $start + $item.Extent.Text.Length
+                # then update the text replacements.
+                if (-not $TextInsertion["$end"]) {
+                    $TextInsertion["$end"] = @($InsertAfterAST[$item])
+                } else {
+                    $TextInsertion["$end"] += @($InsertAfterAST[$item])
+                }
+                
+                $myOffset = $end
+            }
+        }
+        #endregion Insert After AST
+
+        
+        # If we had any text insertions
+        if ($TextInsertion.Count) {
+            # keep track of how much we insert
+            $insertedLength = 0
+            foreach ($kv in $TextInsertion.GetEnumerator()) {
+                $offset = [Math]::Max(($kv.Key -as [int]), 0)
+                # find the indenting for the line                
+                $lineStartMatch = $lineStart.Match($text, $offset + $insertedLength)
+                # and perform the replacement                
+                $insertion = $kv.Value | ReplaceWith -lineStartMatch $lineStartMatch
+                if ($ScriptBlock) {
+                    # (if we're in a ScriptBlock, surround our replacement with newlines so it remains valid)
+                    $insertion = [Environment]::NewLine + "$insertion" + [Environment]::NewLine
+                }
+
+                # Update the text
+                $text = $updatedText = $updatedText.Insert($offset + $insertedLength, $insertion)
+                # and update our insertsion length.
+                $insertedLength += $insertion.Length
+            }
+        }
+
+
+        # If we have any -InsertBefore 
+        if ($InsertBefore.Count) {
+            # Walk thru each insertion
+            foreach ($kv in @($InsertBefore.GetEnumerator())) {                
+                if (                    
+                    $kv.Value -is [string] -and # If the value was a string and
+                    -not $kv.Value.Contains('$&') # it did not include the substitution.
+                ) {
+                    $kv.Value += '$&' # substitute the match at the end.
+                }
+                # Add our insertion to the regular expression replacements.
+                $RegexInsertion[$kv.Key] = $kv.Value
+            }
+        }
+
+        # If we had any -InsertAfter
+        if ($InsertAfter.Count) {
+            # Walk thru each insertion
+            foreach ($kv in @($InsertAfter.GetEnumerator())) {
+                if (                    
+                    $kv.Value -is [string] -and # If the value was a string and
+                    -not $kv.Value.Contains('$&') # it did not include the substitution.
+                ) {
+                    $kv.Value = '$&' + $kv.Value # substitute the match at the start.
+                }
+                # Add our insertion to the regular expression replacements.
+                $RegexInsertion[$kv.Key] = $kv.Value
+            }
+        }
+        #endregion Insert Before/After
+
+        #region Insert Regex
+        if ($RegexInsertion.Count) {
+            foreach ($repl in $RegexInsertion.GetEnumerator()) {
+                $replRegex = if ($repl.Key -is [Regex]) {
+                    $repl.Key
+                } else {
+                    [Regex]::new($repl.Key,'IgnoreCase,IgnorePatternWhitespace','00:00:05')
+                }
+                $updatedText = $text = $replRegex.Replace($text, $repl.Value)
+            }
+        }
+        #endregion Insert Regex
+
+        #endregion
 
         if ($ScriptBlock) {
             $updatedScriptBlock = [ScriptBlock]::Create($updatedText)
--- Tests/Update-PipeScript.tests.ps1
+++ Tests/Update-PipeScript.tests.ps1
@@ -16,5 +16,23 @@ describe Update-PipeScript {
             Update-PipeScript -ScriptBlock { "hello" } -InsertAfter @{"hello" = " world"} |
                 Should -BeLike '*"hello world"*'
         }
+
+        it 'Can -Prepend' {
+            Update-PipeScript -ScriptBlock { goodbye } -Prepend { hello } | Should -BeLike '*hello*goodbye*'
+
+            Update-PipeScript -ScriptBlock { dynamicParam { goodbye } } -Prepend { hello } | Should -BeLike '*dynamicParam*{*hello*goodbye*}*'
+
+            Update-PipeScript -ScriptBlock { begin { goodbye } } -Prepend { hello } | Should -BeLike '*begin*{*hello*goodbye*}*'
+            
+            Update-PipeScript -ScriptBlock { process { goodbye } } -Prepend { hello } | Should -BeLike '*process*{*hello*goodbye*}*'
+        }
+
+        it 'Can -Append' {
+            Update-PipeScript -ScriptBlock { hello } -Append { goodbye } | Should -BeLike '*hello*goodbye*'
+
+            Update-PipeScript -ScriptBlock { dynamicParam { hello } } -Append { goodbye } | Should -BeLike '*dynamicParam*{*hello*goodbye*}*'
+
+            Update-PipeScript -ScriptBlock { dynamicParam { hello } end { } } -Append { goodbye } | Should -BeLike '*dynamicParam*{*hello*goodbye*}*'
+        }
     }
 }
