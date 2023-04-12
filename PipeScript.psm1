b4e0917d4e49727fc5d28c9e4ec0f755dc556341
StartAutomating <StartAutomating@github.com>
2023-04-12 22:54:40 +0000

    Adding PipeScript.Optimizer.ConsolidateAspects (Fixes #413)

--- PipeScript.psm1
+++ PipeScript.psm1
@@ -1,3 +1,5 @@
+
+
 foreach ($file in (Get-ChildItem -Path "$psScriptRoot" -Filter "*-*" -Recurse)) {
     if ($file.Extension -ne '.ps1')      { continue }  # Skip if the extension is not .ps1
     if ($file.Name -match '\.[^\.]+\.ps1$') { continue }  # Skip if the file is an unrelated file.
