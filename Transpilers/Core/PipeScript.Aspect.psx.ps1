1043d2448456e7932d01f6f5ea684fb9d00e755d
StartAutomating <StartAutomating@github.com>
2023-04-12 22:54:40 +0000

    Adding PipeScript.Optimizer.ConsolidateAspects (Fixes #413)

--- Transpilers/Core/PipeScript.Aspect.psx.ps1
+++ Transpilers/Core/PipeScript.Aspect.psx.ps1
@@ -39,7 +39,9 @@ $AspectCommandAst
 )
 
 process {
-    $aspectName = "Aspect?$(@($AspectCommandAst.CommandElements)[0])"
+   
+
+ $aspectName = "Aspect?$(@($AspectCommandAst.CommandElements)[0])"
     $aspectCommands = @($ExecutionContext.SessionState.InvokeCommand.GetCommands($aspectName, 'Function,Alias', $true))
     
     if (-not $aspectCommands) { return }
