3886c301e6b8f7ec7d18d9e42c658c05ccb90c16
StartAutomating <StartAutomating@github.com>
2023-05-21 20:20:10 +0000

    Adding IsPipedFrom (Fixes #431)

--- Commands/AutomaticVariables/Is-AutomaticVariables.ps1
+++ Commands/AutomaticVariables/Is-AutomaticVariables.ps1
@@ -15,3 +15,21 @@ function PipeScript.Automatic.Variable.IsPipedTo {
 }
 
 
+
+
+function PipeScript.Automatic.Variable.IsPipedFrom {
+    <#
+    .SYNOPSIS
+        $IsPipedFrom
+    .DESCRIPTION
+        $IsPipedFrom is an automatic variable that determines if the pipeline continues after this command.
+    .EXAMPLE
+        & (Use-PipeScript { $IsPipedFrom }) # Should -Be $False
+    .EXAMPLE
+        & (Use-PipeScript { $IsPipedFrom }) | Foreach-Object { $_ } # Should -Be $False
+    #>    
+    param()
+    $myInvocation.PipelinePosition -lt $myInvocation.PipelineLength
+}
+
+
