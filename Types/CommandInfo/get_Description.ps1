10cd8edfb7af86f1b4dc0770782c1d8159eecaa9
James Brundage <@github.com>
2023-05-21 17:34:22 -0700

    Resolving AliasInfo for [CommandInfo].get_Description (Fixes #437)

--- Types/CommandInfo/get_Description.ps1
+++ Types/CommandInfo/get_Description.ps1
@@ -1,1 +1,11 @@
-@($this.GetHelpField("Description"))[0] -replace '^\s+'
+if ($this -is [Manangement.Automation.AliasInfo]) {
+    $resolveThis = $this
+    while ($resolveThis.ResolvedCommand) {
+        $resolveThis = $resolveThis.ResolvedCommand
+    }
+    if ($resolveThis) {
+        @($resolveThis.GetHelpField("Description"))[0] -replace '^\s+'
+    }
+} else {
+    @($this.GetHelpField("Description"))[0] -replace '^\s+'
+}
