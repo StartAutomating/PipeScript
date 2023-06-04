e2a02c66e8e91e0a825ac45825eaac6de473190a
James Brundage <@github.com>
2023-06-04 14:40:45 -0700

    Updating Types Documentation

--- Types/AST/GetLineage.ps1
+++ Types/AST/GetLineage.ps1
@@ -1,3 +1,11 @@
+<#
+.SYNOPSIS
+    Gets AST Lineage
+.DESCRIPTION
+    Gets the Lineage of an Abstract Syntax Tree element.
+    
+    The Lineage is all of the Abstract Syntax Tree's parents.
+#>
 $thisParent = $this.Parent
 while ($thisParent) {
     $thisParent
