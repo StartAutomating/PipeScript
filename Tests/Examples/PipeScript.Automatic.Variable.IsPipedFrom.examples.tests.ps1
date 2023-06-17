a244bd713d396a850065761e890b90ea0cab76b1
StartAutomating <StartAutomating@github.com>
2023-06-17 21:22:19 +0000

    Updating ExamplesShouldWork (fixing regex and adding timeout) (re #461)

--- Tests/Examples/PipeScript.Automatic.Variable.IsPipedFrom.examples.tests.ps1
+++ Tests/Examples/PipeScript.Automatic.Variable.IsPipedFrom.examples.tests.ps1
@@ -2,10 +2,10 @@
 describe 'PipeScript.Automatic.Variable.IsPipedFrom' {
     it 'PipeScript.Automatic.Variable.IsPipedFrom Example 1' {
         $PipedFrom = & (Use-PipeScript { $IsPipedFrom })
-        $PipedFrom | Should -Be $False
+        $PipedFrom |  Should -Be $False
     }
     it 'PipeScript.Automatic.Variable.IsPipedFrom Example 2' {
-        & (Use-PipeScript { $IsPipedFrom }) | Foreach-Object { $_ } | Should -Be $true
+        & (Use-PipeScript { $IsPipedFrom }) | Foreach-Object { $_ } |  Should -Be $true
     }
 }
 
