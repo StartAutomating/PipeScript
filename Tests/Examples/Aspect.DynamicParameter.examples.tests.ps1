142264aeb090a4e2b215b88fe860f6697b71f9df
StartAutomating <StartAutomating@github.com>
2023-06-17 21:22:19 +0000

    Updating ExamplesShouldWork (fixing regex and adding timeout) (re #461)

---
+++ Tests/Examples/Aspect.DynamicParameter.examples.tests.ps1
@@ -0,0 +1,12 @@
+
+describe 'Aspect.DynamicParameter' {
+    it 'Aspect.DynamicParameter Example 1' {
+        Get-Command Get-Command | 
+            Aspect.DynamicParameter
+    }
+    it 'Aspect.DynamicParameter Example 2' {
+        Get-Command Get-Process | 
+            Aspect.DynamicParameter -IncludeParameter Name | Select -Expand Key |  Should -Be Name
+    }
+}
+
