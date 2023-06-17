43eedbce6d4ebf8dcc7a861cdb8d60aeb95a23a9
StartAutomating <StartAutomating@github.com>
2023-06-17 02:26:14 +0000

    Fixing ExamplesShouldWork.build.ps1 (Fixes #461)
    Using module relative path

---
+++ Tests/examples-PipeScript.PostProcess.PartialFunction.tests.ps1
@@ -0,0 +1,14 @@
+
+describe 'PipeScript.PostProcess.PartialFunction' {
+    it 'PipeScript.PostProcess.PartialFunction Example 1' {
+        Import-PipeScript {
+            partial function testPartialFunction {
+                "This will be added to a command name TestPartialFunction"
+            }
+            
+            function testPartialFunction {}
+        }
+        testPartialFunction | Should -BeLike '*TestPartialFunction*'
+    }
+}
+
