e20924423fc43277169e0ab15c8a98ddde947551
StartAutomating <StartAutomating@github.com>
2023-06-17 02:26:13 +0000

    Fixing ExamplesShouldWork.build.ps1 (Fixes #461)
    Using module relative path

---
+++ Tests/examples-PipeScript.Automatic.Variable.IsPipedFrom.tests.ps1
@@ -0,0 +1,10 @@
+
+describe 'PipeScript.Automatic.Variable.IsPipedFrom' {
+    it 'PipeScript.Automatic.Variable.IsPipedFrom Example 1' {
+        & (Use-PipeScript { $IsPipedFrom }) | Should -Be $False
+    }
+    it 'PipeScript.Automatic.Variable.IsPipedFrom Example 2' {
+        & (Use-PipeScript { $IsPipedFrom }) | Foreach-Object { $_ } | Should -Be $true
+    }
+}
+
