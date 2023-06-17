3c1269c62385624a19e5dec8a074ad1e95448a50
StartAutomating <StartAutomating@github.com>
2023-06-17 02:47:14 +0000

    Moving Examples tests into a subdirectory (re #461)

---
+++ Tests/Examples/PipeScript.Automatic.Variable.IsPipedFrom.examples.tests.ps1
@@ -0,0 +1,11 @@
+
+describe 'PipeScript.Automatic.Variable.IsPipedFrom' {
+    it 'PipeScript.Automatic.Variable.IsPipedFrom Example 1' {
+        $PipedFrom = & (Use-PipeScript { $IsPipedFrom })
+        $PipedFrom | Should -Be $False
+    }
+    it 'PipeScript.Automatic.Variable.IsPipedFrom Example 2' {
+        & (Use-PipeScript { $IsPipedFrom }) | Foreach-Object { $_ } | Should -Be $true
+    }
+}
+
