d1f765e75732e36903459576ec0972176de8f96c
StartAutomating <StartAutomating@github.com>
2023-06-17 21:22:19 +0000

    Updating ExamplesShouldWork (fixing regex and adding timeout) (re #461)

--- Tests/Examples/Aspect.ModuleExtendedCommand.examples.tests.ps1
+++ Tests/Examples/Aspect.ModuleExtendedCommand.examples.tests.ps1
@@ -1,7 +1,7 @@
 
 describe 'Aspect.ModuleExtendedCommand' {
     it 'Aspect.ModuleExtendedCommand Example 1' {
-                                  Aspect.ModuleCommandPattern -Module PipeScript | Should -BeOfType ([Regex])
+                                  Aspect.ModuleCommandPattern -Module PipeScript |  Should -BeOfType ([Regex])
     }
 }
 
