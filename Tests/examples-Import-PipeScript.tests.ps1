a1e70c095bcc4ca42f3a0beae3699a5071b5aad3
StartAutomating <StartAutomating@github.com>
2023-06-17 02:26:13 +0000

    Fixing ExamplesShouldWork.build.ps1 (Fixes #461)
    Using module relative path

---
+++ Tests/examples-Import-PipeScript.tests.ps1
@@ -0,0 +1,19 @@
+
+describe 'Import-PipeScript' {
+    it 'Import-PipeScript Example 1' {
+        Import-PipeScript -ScriptBlock {
+            function gh {
+                [Inherit('gh',CommandType='Application')]
+                param()
+            }
+        }
+    }
+    it 'Import-PipeScript Example 2' {
+        Import-PipeScript -ScriptBlock {
+            partial function f() {
+                "This will be added to any function named f."
+            }
+        }
+    }
+}
+
