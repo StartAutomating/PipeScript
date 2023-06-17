465a4bbd82bb5f9fe4b21260416b4ca8e5fad0bf
James Brundage <@github.com>
2023-06-16 19:36:10 -0700

    IsPipedFrom - Fixing Example Test (re #461)

--- Commands/AutomaticVariables/Is-AutomaticVariables.ps.ps1
+++ Commands/AutomaticVariables/Is-AutomaticVariables.ps.ps1
@@ -21,7 +21,8 @@ PipeScript.Automatic.Variable function IsPipedFrom {
     .DESCRIPTION
         $IsPipedFrom determines if the pipeline continues after this command.
     .EXAMPLE
-        & (Use-PipeScript { $IsPipedFrom }) # Should -Be $False
+        $PipedFrom = & (Use-PipeScript { $IsPipedFrom })
+        $PipedFrom # Should -Be $False
     .EXAMPLE
         & (Use-PipeScript { $IsPipedFrom }) | Foreach-Object { $_ } # Should -Be $true
     #>    
