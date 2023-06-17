ee241e3f13db28959af634e0a6438d3faf43de95
StartAutomating <StartAutomating@github.com>
2023-06-17 02:26:13 +0000

    Fixing ExamplesShouldWork.build.ps1 (Fixes #461)
    Using module relative path

---
+++ Tests/examples-Export-Pipescript.tests.ps1
@@ -0,0 +1,10 @@
+
+describe 'Export-Pipescript' {
+    it 'Export-Pipescript Example 1' {
+        Export-PipeScript
+    }
+    it 'Export-Pipescript Example 2' {
+        Build-PipeScript
+    }
+}
+
