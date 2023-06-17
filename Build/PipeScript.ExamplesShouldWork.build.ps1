ae8b69a22afe8f85f64ad49ae0fdcbc65d882334
James Brundage <@github.com>
2023-06-16 19:19:56 -0700

    Adding ExamplesShouldWork.build.ps1 (Fixes #461)

---
+++ Build/PipeScript.ExamplesShouldWork.build.ps1
@@ -0,0 +1,32 @@
+param(
+$ModuleName = 'PipeScript'
+)
+
+$moduleInfo = Get-Module $ModuleName
+$commandsInModule = Get-Command -Module $moduleName 
+
+$commandsWithExamples = $commandsInModule | Where-Object { $_.Examples }
+
+$examplePattern = [Regex]::new('(?<ws>[\s\r\n]{0,})\#\sShould\s(?<C>.+?)$', 'Multiline,IgnoreCase,IgnorePatternWhitespace')
+
+$testsDirectory = Split-Path $pwd | Join-Path -ChildPath Tests
+
+foreach ($commandShouldWork in $commandsWithExamples) {
+    $exampleCounter = 0
+    $exampleFileContent = 
+    @(
+    ''
+    "describe '$($commandShouldWork)' {"
+    foreach ($commandExample in $commandShouldWork.Examples) {
+        $exampleCounter++
+        "    it '$commandShouldWork Example $($exampleCounter)' {"
+            $examplePattern.Replace($commandExample, ' | Should ${C}')    
+        "    }"
+    }
+    "}"
+    '') -join [Environment]::newLine
+
+    $testFilePath = Join-Path $testsDirectory "examples-$CommandShouldWork.tests.ps1"
+    $exampleFileContent | Set-Content $testFilePath
+    Get-Item $testFilePath    
+}
