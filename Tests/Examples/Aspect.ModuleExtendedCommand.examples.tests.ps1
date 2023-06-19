310e87a9b540152d3da0713eb20f4f9dfc7327c8
StartAutomating <StartAutomating@github.com>
2023-06-19 21:28:48 +0000

    New-PipeScript - Adding -ReferenceObject (Fixes #456)
    Also, making parameters a queue.

--- Tests/Examples/Aspect.ModuleExtendedCommand.examples.tests.ps1
+++ Tests/Examples/Aspect.ModuleExtendedCommand.examples.tests.ps1
@@ -1,6 +1,9 @@
 
 describe 'Aspect.ModuleExtendedCommand' {
     it 'Aspect.ModuleExtendedCommand Example 1' {
+        Aspect.ModuleExtendedCommand -Module PipeScript |  Should -BeOfType ([Management.Automation.CommandInfo])
+    }
+    it 'Aspect.ModuleExtendedCommand Example 2' {
                                   Aspect.ModuleCommandPattern -Module PipeScript |  Should -BeOfType ([Regex])
     }
 }
