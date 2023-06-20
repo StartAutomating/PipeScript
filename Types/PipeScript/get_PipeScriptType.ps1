b5cd6844816c88a1f9f79eb64d90eaae947666a0
James Brundage <@github.com>
2023-06-19 21:18:08 -0700

    Updating PipeScript.PipeScriptType
    Preparing for #463

--- Types/PipeScript/get_PipeScriptType.ps1
+++ Types/PipeScript/get_PipeScriptType.ps1
@@ -1,18 +1,23 @@
-if ($this.Source -match '\.psx\.ps1{0,1}$') {
-    "Transpiler"
-}
-elseif ($this.Source -match "\.ps1{0,1}\.(?<ext>[^.]+$)") {
-    "Template"
-}
-elseif ($this.Source -match '(?<=(?>^|\.))build\.ps1$') {
-    "BuildScript"
-}
-elseif (($this.Source -match '\.[^\.\\/]+\.ps1$')) {
-    "ExtensionScript"
-}
-elseif ($this.Source) {
-    "PipeScriptFile"
-}
-else {
-    "Function"
+
+if ($this.pstypenames -like '*.Command') {
+    $this.pstypenames -like '*.Command' -replace '\.Command'
+} else {
+    if ($this.Source -match '\.psx\.ps1{0,1}$') {
+        "Transpiler"
+    }
+    elseif ($this.Source -match "\.ps1{0,1}\.(?<ext>[^.]+$)") {
+        "Template"
+    }
+    elseif ($this.Source -match '(?<=(?>^|\.))build\.ps1$') {
+        "BuildScript"
+    }
+    elseif (($this.Source -match '\.[^\.\\/]+\.ps1$')) {
+        "ExtensionScript"
+    }
+    elseif ($this.Source) {
+        "PipeScriptFile"
+    }
+    else {
+        "Function"
+    }
 }
