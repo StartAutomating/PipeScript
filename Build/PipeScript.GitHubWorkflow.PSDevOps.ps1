c88cf3c2efae23c84ece1f39e9c0721b5ddf5c78
James Brundage <@github.com>
2023-06-20 19:05:12 -0700

    Updating Workflow Definition

--- Build/PipeScript.GitHubWorkflow.PSDevOps.ps1
+++ Build/PipeScript.GitHubWorkflow.PSDevOps.ps1
@@ -1,7 +1,10 @@
 #requires -Module PSDevOps
 Import-BuildStep -ModuleName PipeScript
 Push-Location ($PSScriptRoot | Split-Path)
-New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, BuildPipeScript -Environment @{
-    NoCoverage = $true
-} -OutputPath .\.github\workflows\TestAndPublish.yml
+New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push,
+    PullRequest, 
+    Demand -Job PowerShellStaticAnalysis, 
+    TestPowerShellOnLinux, 
+    TagReleaseAndPublish, 
+    BuildPipeScript -OutputPath .\.github\workflows\TestAndPublish.yml
 Pop-Location
