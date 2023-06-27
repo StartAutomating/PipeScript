85aa335b012501939b99ae7500b6095d43e0386a
James Brundage <@github.com>
2023-06-27 15:01:41 -0700

    Exporting All Aliases (re #452)

--- Tests/Examples/Aspect.ModuleCommandPattern.examples.tests.ps1
+++
@@ -1,7 +0,0 @@
-
-describe 'Aspect.ModuleCommandPattern' {
-    it 'Aspect.ModuleCommandPattern Example 1' {
-        Aspect.ModuleCommandPattern -Module PipeScript |  Should -BeOfType ([Regex])
-    }
-}
-
--- Tests/Examples/Aspect.ModuleCommandType.examples.tests.ps1
+++
@@ -1,9 +0,0 @@
-
-describe 'Aspect.ModuleCommandType' {
-    it 'Aspect.ModuleCommandType Example 1' {
-        # Outputs a series of PSObjects with information about command types.
-        # The two primary pieces of information are the `.Name` and `.Pattern`.
-        Aspect.ModuleCommandType -Module PipeScript |  Should -BeOfType ([PSObject])
-    }
-}
-
