7df99777abe2dfac04fdbced8f4b90f672d5a878
James Brundage <@github.com>
2023-06-27 14:31:18 -0700

    Renaming Module Aspects and Improving Extended Command Filtering (Fixes #482)
    (re #452, #460, #463)

--- Commands/Aspects/Module-Aspects.ps.ps1
+++ Commands/Aspects/Module-Aspects.ps.ps1
@@ -1,15 +1,16 @@
-Aspect function ModuleCommandType {
+Aspect function ModuleExtensionType {
     <#
     .SYNOPSIS
-        Outputs a module's command types
+        Outputs a module's extension types
     .DESCRIPTION
-        Outputs the command types defined in a module's manifest.
+        Outputs the extension types defined in a module's manifest.
     .EXAMPLE
-        # Outputs a series of PSObjects with information about command types.
+        # Outputs a PSObject with information about extension command types.
+        
         # The two primary pieces of information are the `.Name` and `.Pattern`.
-        Aspect.ModuleCommandType -Module PipeScript # Should -BeOfType ([PSObject])
+        Aspect.ModuleExtensionType -Module PipeScript # Should -BeOfType ([PSObject])
     #>
-    [Alias('Aspect.ModuleCommandTypes')]
+    [Alias('Aspect.ModuleCommandTypes','Aspect.ModuleCommandType','Aspect.ModuleExtensionTypes')]
     param(
     # The name of a module, or a module info object.
     [vbn(Mandatory)]
@@ -17,50 +18,89 @@ Aspect function ModuleCommandType {
     $Module
     )
 
+    begin {
+        $ExtensionCollectionNames = 
+            "Extension", "Command", "Cmdlet", "Function", "Alias", "Script", "Application", "File","Configuration"
+        $ExtensionCollectionNames = @($ExtensionCollectionNames -replace '.+$','${0}Type') + @($ExtensionCollectionNames -replace '.+$','${0}Types')
+    }
+
     process {
+        #region Resolve Module Info
         if ($Module -is [string]) {
             $Module = Get-Module $Module
         }
-        $ModuleInfo = $module
-        
-
-        #region Search for Module Command Types
+        $ModuleInfo = $module                
         return if -not $ModuleInfo
-        $ModuleCommandTypes = 
-            @($ModuleInfo.PrivateData.CommandType,
-            $ModuleInfo.PrivateData.CommandTypes,
-            $ModuleInfo.PrivateData.PSData.CommandType,
-            $ModuleInfo.PrivateData.PSData.CommandType -ne $null)[0]
+        #endregion Resolve Module Info
 
-        
-        return if -not $ModuleCommandTypes
-            
-        foreach ($commandType in @($ModuleCommandTypes.GetEnumerator() | Sort-Object Key)) {
-            if ($commandType.Value -is [Collections.IDictionary]) {
-                if (-not $commandType.Value.Name) {
-                    $commandType.Value["Name"] = $commandType.Key
-                }
-                [PSCustomObject]$commandType.Value
-                
-            } else {
-                [PSCustomObject]@{
-                    Name    = $commandType.Key
-                    Pattern = $commandType.Value
-                }
-            }   
+        #region Check Cache and Hopefully Return
+        if (-not $script:ModuleExtensionTypeCache) {
+            $script:ModuleExtensionTypeCache = @{}
         }
+        
+        if ($script:ModuleExtensionTypeCache[$ModuleInfo]) {
+            return $script:ModuleExtensionTypeCache[$ModuleInfo]
+        }
+        #endregion Check Cache and Hopefully Return
+
+        #region Find Extension Types
+        $modulePrivateData  = $ModuleInfo.PrivateData
+
+        $SortedExtensionTypes = [Ordered]@{}
+        foreach ($TypeOfExtensionCollection in $ExtensionCollectionNames) {
+            $moduleExtensionTypes = 
+                if ($modulePrivateData.$TypeOfExtensionCollection) {
+                    $modulePrivateData.$TypeOfExtensionCollection
+                } elseif ($modulePrivateData.PSData.$TypeOfExtensionCollection) {
+                    $modulePrivateData.PSData.$TypeOfExtensionCollection
+                } else {
+                    $null
+                }
+
+            continue if -not $moduleExtensionTypes
+
+            foreach ($commandType in @($ModuleExtensionTypes.GetEnumerator() | Sort-Object Key)) {
+                if ($commandType.Value -is [Collections.IDictionary]) {
+                    if (-not $commandType.Value.Name) {
+                        $commandType.Value["Name"] = $commandType.Key
+                    }
+                    if (-not $commandType.Value.PSTypeName) {
+                        $commandType.Value["PSTypeName"] = "$($module.Name).ExtensionCommandType"
+                    }
+                    $SortedExtensionTypes[$commandType.Name] = $commandType.Value
+                } else {
+                    $SortedExtensionTypes[$commandType.Name] = [Ordered]@{
+                        PSTypeName = "$($module.Name).ExtensionCommandType"
+                        Name    = $commandType.Key
+                        Pattern = $commandType.Value
+                    }
+                }
+                if ($TypeOfExtensionCollection -notmatch '(?>Extension|Command|Cmdlet)') {
+                    $SortedExtensionTypes[$commandType.Name].CommandType = $TypeOfExtensionCollection -replace 'Type(?:s)?$'
+                } elseif ($TypeOfExtensionCollection -match 'Cmdlet') {
+                    $SortedExtensionTypes[$commandType.Name].CommandType = "(?>Alias|Function|Filter|Cmdlet)"
+                }
+            }
+        }
+        
+        $SortedExtensionTypes.PSTypeName="$($Module.Name).ExtensionCommandTypes"
+        
+        [PSCustomObject]$SortedExtensionTypes
+        #endregion Find Extension Types
+
     }    
 }
 
-Aspect function ModuleCommandPattern {
+Aspect function ModuleExtensionPattern {
     <#
     .SYNOPSIS
-        Outputs a module's command pattern
+        Outputs a module's extension pattern
     .DESCRIPTION
-        Outputs a regular expression that can be used to match any command pattern.
+        Outputs a regular expression that will match any possible pattern.
     .EXAMPLE
         Aspect.ModuleCommandPattern -Module PipeScript # Should -BeOfType ([Regex])
     #>
+    [Alias('Aspect.ModuleCommandPattern')]
     param(
     # The name of a module, or a module info object.
     [vbn(Mandatory)]
@@ -86,36 +126,27 @@ Aspect function ModuleCommandPattern {
         $ModuleInfo = $module
 
 
-        #region Search for Module Command Types
+        #region Search for Module Extension Types
         return if -not $ModuleInfo
-        $ModuleCommandTypes = 
-            @($ModuleInfo.PrivateData.CommandType,
-            $ModuleInfo.PrivateData.CommandTypes,
-            $ModuleInfo.PrivateData.PSData.CommandType,
-            $ModuleInfo.PrivateData.PSData.CommandType -ne $null)[0]
+        $ModuleExtensionTypes = Aspect.ModuleExtensionTypes -Module $moduleInfo
 
         
-        return if -not $ModuleCommandTypes
+        return if -not $ModuleExtensionTypes
             
         # With some clever understanding of Regular expressions, we can make match any/all of our potential command types.
         # Essentially: Regular Expressions can look ahead (matching without changing the position), and be optional.
         # So we can say "any/all" by making a series of optional lookaheads.
         
         # We'll go thru each pattern in order
-        $combinedRegex = @(foreach ($categoryKeyValue in $ModuleCommandTypes.GetEnumerator() | Sort-Object Key) {
-            $categoryPattern = 
-                if ($categoryKeyValue.Value -is [string]) {
-                    $categoryKeyValue.Value
-                } else {
-                    $categoryKeyValue.Value.Pattern
-                }
+        $combinedRegex = @(foreach ($categoryExtensionTypeInfo in @($ModuleExtensionTypes.psobject.properties)) {
+            $categoryPattern = $categoryExtensionTypeInfo.Value.Pattern                
             # ( and skip anyone that does not have a pattern)
             continue if -not $categoryPattern
 
             '(?=' + # Start a lookahead
                 '.{0,}' + # match any or no characters
                 # followed by the command pattern
-                "(?<$Prefix$($categoryKeyValue.Key -replace '\p{P}', '_')$Suffix>$categoryPattern)" +
+                "(?<$Prefix$($categoryExtensionTypeInfo.Name -replace '\p{P}', '_')$Suffix>$categoryPattern)" +
                 ')?' # made optional                            
         }) -join [Environment]::NewLine
 
@@ -124,7 +155,7 @@ Aspect function ModuleCommandPattern {
     }
 }
 
-Aspect function ModuleExtendedCommand {
+Aspect function ModuleExtensionCommand {
     <#
     .SYNOPSIS
         Returns a module's extended commands
@@ -136,14 +167,20 @@ Aspect function ModuleExtendedCommand {
     .LINK
         Aspect.ModuleCommandPattern
     .EXAMPLE
-        Aspect.ModuleExtendedCommand -Module PipeScript # Should -BeOfType ([Management.Automation.CommandInfo])
+        Aspect.ModuleExtensionCommand -Module PipeScript # Should -BeOfType ([Management.Automation.CommandInfo])
     #>
-    [Alias('Aspect.ModuleExtensionCommand')]
+    [Alias('Aspect.ModuleExtendedCommand')]
     param(
     # The name of a module, or a module info object.
     [vbn(Mandatory)]
     [ValidateTypes(TypeName={[string],[Management.Automation.PSModuleInfo]})]
     $Module,
+    
+    # A list of commands.
+    # If this is provided, each command that is a valid extension will be returned.
+    [vbn()]
+    [Management.Automation.CommandInfo[]]
+    $Commands,
 
     # The suffix to apply to each named capture.
     # Defaults to '_Command'
@@ -161,6 +198,11 @@ Aspect function ModuleExtendedCommand {
     [Alias('Fullname')]    
     $FilePath,
 
+    # The PowerShell command type.  If this is provided, will only get commands of this type.
+    [vbn()]
+    [Management.Automation.CommandTypes]
+    $CommandType,
+
     # The base PSTypeName(s).
     # If provided, any commands that match the pattern will apply these typenames, too.
     [string[]]
@@ -175,61 +217,61 @@ Aspect function ModuleExtendedCommand {
 
         return if -not $ModuleInfo
         
-        $CommandPattern = ModuleCommandPattern $ModuleInfo -Prefix $prefix -Suffix $Suffix
-
-        if ($PSBoundParameters['FilePath']) {
-            all scripts applications in $FilePath foreach {
-                $cmd = $_
-                $matched = $CommandPattern.Match("$cmd")
-                if (-not $matched.Success) { continue }
-                $NamedGroupMatch = $false
-                foreach ($group in $matched.Groups) {
-                    if (-not $group.Success) { continue }
-                    if ($null -ne ($group.Name -as [int])) { continue }
-                    $NamedGroupMatch = $true
-                    $groupName = $group.Name.Replace('_','.')
-                    if ($PSTypeName) {
-                        foreach ($psuedoType in $PSTypeName) {
-                            if ($cmd.pstypenames -notcontains $psuedoType) {
-                                $cmd.pstypenames.insert(0, $psuedoType)        
-                            }
-                        }
-                    }
-                    if ($cmd.pstypenames -notcontains $groupName) {
-                        $cmd.pstypenames.insert(0, $groupName)
-                    }
-                }
-                if ($NamedGroupMatch) {
-                    $cmd
-                } 
+        $ModuleCommandPattern = ModuleCommandPattern $ModuleInfo -Prefix $prefix -Suffix $Suffix
+        $ModuleCommandTypes   = ModuleExtensionType $ModuleInfo
+        
+        $commands    =
+            @(
+            if ($PSBoundParameters['Commands']) {
+                $commands
             }
-        }
-        else {
-            all functions cmdlets aliases foreach {
-                $cmd = $_                
-                $matched = $CommandPattern.Match("$cmd")
-                if (-not $matched.Success) { continue }
-                $NamedGroupMatch = $false
-                foreach ($group in $matched.Groups) {
-                    if (-not $group.Success) { continue }
-                    if ($null -ne ($group.Name -as [int])) { continue }
-                    $NamedGroupMatch = $true
-                    $groupName = $group.Name -replace '_', '.'
-                    if ($PSTypeName) {
-                        foreach ($psuedoType in $PSTypeName) {
-                            if ($cmd.pstypenames -notcontains $psuedoType) {
-                                $cmd.pstypenames.insert(0, $psuedoType)        
-                            }
+            if ($PSBoundParameters['FilePath']) {
+                if (-not $commandType) {
+                    $commandType = 'Application,ExternalScript'
+                }
+                foreach ($file in Get-ChildItem -File -Path $PSBoundParameters['FilePath'] -Recurse) {
+                    $ExecutionContext.SessionState.InvokeCommand.GetCommand($file.FullName, $commandType)
+                }
+            } else {
+                if (-not $CommandType) {
+                    $commandType = 'Function,Alias,Filter,Cmdlet'
+                }
+                $ExecutionContext.SessionState.InvokeCommand.GetCommands('*', $commandType, $true)
+            })
+
+        :nextCommand foreach ($cmd in $commands) {            
+            $matched = $ModuleCommandPattern.Match("$cmd")
+            if (-not $matched.Success) { continue }
+            $NamedGroupMatch = $false
+            :nextCommandType foreach ($group in $matched.Groups) {
+                if (-not $group.Success) { continue }
+                if ($null -ne ($group.Name -as [int])) { continue }                
+                $CommandTypeName     = $group.Name.Replace('_','.')
+                $ThisCommandsType    = $ModuleCommandTypes.($group.Name -replace "^$prefix" -replace "$suffix$")
+                if ($ThisCommandsType) {
+                    $ThisTypeFilter = @($ThisCommandsType.CommandType,$ThisCommandsType.CommandTypes -ne $null)[0]
+                    if ($ThisTypeFilter -and ($cmd.CommandType -notmatch $ThisTypeFilter)) {
+                        continue nextCommandType
+                    }
+                    $ThisExcludeFilter = @($ThisCommandsType.ExcludeCommandType,$ThisCommandsType.ExcludeCommandTypes -ne $null)[0]
+                    if ($ThisExcludeFilter -and ($cmd.CommandType -match $ThisExcludeFilter)) {
+                        continue nextCommandType
+                    }
+                }
+                $NamedGroupMatch     = $true
+                if ($PSTypeName) {
+                    foreach ($psuedoType in $PSTypeName) {
+                        if ($cmd.pstypenames -notcontains $psuedoType) {
+                            $cmd.pstypenames.insert(0, $psuedoType)        
                         }
                     }
-                    if ($cmd.pstypenames -notcontains $groupName) {
-                        $cmd.pstypenames.insert(0, $groupName)
-                    }
                 }
-                if ($NamedGroupMatch) {
-                    $cmd
+                if ($cmd.pstypenames -notcontains $CommandTypeName) {
+                    $cmd.pstypenames.insert(0, $CommandTypeName)
                 }
-                
+            }
+            if ($NamedGroupMatch) {
+                $cmd
             }
         }
     }
