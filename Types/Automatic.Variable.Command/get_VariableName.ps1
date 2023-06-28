5c90559fbde909c0d8419156a03fc4960a25feaf
James Brundage <@github.com>
2023-06-28 16:19:54 -0700

    Completing Rename of Automatic.Variable.Command to AutomaticVariable.Command

--- Types/Automatic.Variable.Command/get_VariableName.ps1
+++
@@ -1,9 +0,0 @@
-<#
-.SYNOPSIS
-    Gets the automatic variable name
-.DESCRIPTION
-    Gets the name of an automatic variable that is defined in an Automatic?Variable* command.
-#>
-$this -replace '(?>Magic|Automatic)\p{P}Variable\p{P}' -replace 
-    '^(?>PowerShell|PipeScript)' -replace
-    '^\p{P}' -replace '\p{P}$'
