ebb04647d552a53095204eb99b3043565b254611
StartAutomating <StartAutomating@github.com>
2023-06-28 20:55:17 +0000

    Updating Examples Should Work (adding docs) (re #461)

--- Tests/Examples/Get-PipeScript.examples.tests.ps1
+++ Tests/Examples/Get-PipeScript.examples.tests.ps1
@@ -21,40 +21,5 @@ describe 'Get-PipeScript' {
         { partial function f { } }  |
             PipeScript Import -PassThru |  Should -BeOfType ([Management.Automation.PSModuleInfo])
     }
-    it 'Get-PipeScript Example 6' {
-                                                        Aspect.ModuleExtensionCommand -Module PipeScript |  Should -BeOfType ([Management.Automation.CommandInfo])
-    }
-    it 'Get-PipeScript Example 7' {
-                                                                                        Aspect.ModuleCommandPattern -Module PipeScript |  Should -BeOfType ([Regex])
-    }
-    it 'Get-PipeScript Example 8' {
-                                                                                                                        # Outputs a PSObject with information about extension command types.
-                                                                                                                        
-                                                                                                                        # The two primary pieces of information are the `.Name` and `.Pattern`.
-                                                                                                                        Aspect.ModuleExtensionType -Module PipeScript |  Should -BeOfType ([PSObject])
-    }
-    it 'Get-PipeScript Example 9' {
-                                                                                        # Outputs a PSObject with information about extension command types.
-                                                                                        
-                                                                                        # The two primary pieces of information are the `.Name` and `.Pattern`.
-                                                                                        Aspect.ModuleExtensionType -Module PipeScript |  Should -BeOfType ([PSObject])
-    }
-    it 'Get-PipeScript Example 10' {
-                                                                                        Aspect.ModuleCommandPattern -Module PipeScript |  Should -BeOfType ([Regex])
-    }
-    it 'Get-PipeScript Example 11' {
-                                                                                                                        # Outputs a PSObject with information about extension command types.
-                                                                                                                        
-                                                                                                                        # The two primary pieces of information are the `.Name` and `.Pattern`.
-                                                                                                                        Aspect.ModuleExtensionType -Module PipeScript |  Should -BeOfType ([PSObject])
-    }
-    it 'Get-PipeScript Example 12' {
-                            Get-Command Get-Command | 
-                                Aspect.DynamicParameter
-    }
-    it 'Get-PipeScript Example 13' {
-                            Get-Command Get-Process | 
-                                Aspect.DynamicParameter -IncludeParameter Name | Select -Expand Keys |  Should -Be Name
-    }
 }
 
