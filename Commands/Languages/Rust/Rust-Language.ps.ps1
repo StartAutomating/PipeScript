3f24f1c8b11347acad83402bf08217866f625771
James Brundage <@github.com>
2023-11-11 12:46:39 -0800

    fix: Fixing Rust Language Examples (re #623)

--- Commands/Languages/Rust/Rust-Language.ps.ps1
+++ Commands/Languages/Rust/Rust-Language.ps.ps1
@@ -28,16 +28,7 @@ Language function Rust {
             '
         
             $HelloWorldRust.Evaluate('hi')
-            $HelloWorldRust.Save(@{Message='Hello'}) |
-                Foreach-Object { 
-                    $file = $_
-                    if (Get-Command rustc -commandType Application) {
-                        $null = rustc $file.FullName
-                        & ".\$($file.Name.Replace($file.Extension, '.exe'))"
-                    } else {
-                        Write-Error "Go install Rust"
-                    }
-                }
+            $HelloWorldRust.Save(@{Message='Hello'})
         }
     .EXAMPLE
         '    
@@ -57,16 +48,7 @@ Language function Rust {
         }
         " | Set-Content .\HelloWorld_Rust.ps1.rs
 
-        Invoke-PipeScript .\HelloWorld_Rust.ps1.rs -Parameter @{message='hi'} |
-            Foreach-Object { 
-                $file = $_
-                if (Get-Command rustc -commandType Application) {
-                    $null = rustc $file.FullName
-                    & ".\$($file.Name.Replace($file.Extension, '.exe'))"
-                } else {
-                    Write-Error "Go install Rust"
-                }
-            }
+        Invoke-PipeScript .\HelloWorld_Rust.ps1.rs -Parameter @{message='hi'}
     #>
     [ValidatePattern('\.rs$')]
     [Alias('Rust-Language','Language-Rust')]
