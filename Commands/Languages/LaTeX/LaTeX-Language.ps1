ddb7a195fe186dfaf1836e5a66910b54b8641995
StartAutomating <StartAutomating@github.com>
2023-11-11 22:37:17 +0000

    feat: Porting LaTeX Template to Language Function (Fixes #608)

---
+++ Commands/Languages/LaTeX/LaTeX-Language.ps1
@@ -0,0 +1,41 @@
+
+function Language.LaTeX {
+<#
+    .SYNOPSIS
+        LaTeX Language Definition.
+    .DESCRIPTION
+        Allows PipeScript to generate Latex and Tex files.
+        Multiline comments with %{}% will be treated as blocks of PipeScript.    
+    #>
+[ValidatePattern('\.(?>latex|tex)$')]
+param(
+                    
+                )
+$this = $myInvocation.MyCommand
+if (-not $this.Self) {
+$languageDefinition =
+New-Module {
+    <#
+    .SYNOPSIS
+        LaTeX Language Definition.
+    .DESCRIPTION
+        Allows PipeScript to generate Latex and Tex files.
+        Multiline comments with %{}% will be treated as blocks of PipeScript.    
+    #>
+    [ValidatePattern('\.(?>latex|tex)$')]
+    param()
+    # We start off by declaring a number of regular expressions:
+    $startComment = '\%\{' # * Start Comments ```%{```
+    $endComment   = '\}\%' # * End Comments   ```}%```
+    $Whitespace   = '[\s\n\r]{0,}'        
+    $StartPattern = "(?<PSStart>${startComment})"    
+    $EndPattern   = "(?<PSEnd>${endComment}\s{0,})"
+    Export-ModuleMember -Variable * -Function * -Alias *
+} -AsCustomObject
+$languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language.LaTeX")
+$this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
+}
+$this.Self
+}
+
