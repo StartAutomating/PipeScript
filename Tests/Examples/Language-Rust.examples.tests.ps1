b80ac8104006b04b984e410ffa72046e83822fe0
StartAutomating <StartAutomating@github.com>
2023-11-11 10:43:05 +0000

    feat: Porting ADA Template Transpiler to Language Function (Fixes #586)

---
+++ Tests/Examples/Language-Rust.examples.tests.ps1
@@ -0,0 +1,62 @@
+
+describe 'Language-Rust' {
+    it 'Language-Rust Example 1' {
+        $HelloWorldRust = HelloWorld_Rust.rs template '    
+        fn main() {
+            let msg = /*{param($msg = ''hello world'') "`"$msg`""}*/ ;
+            println!("{}",msg);
+        }
+        '
+        "$HelloWorldRust"
+    }
+    it 'Language-Rust Example 2' {
+        $HelloWorldRust = HelloWorld_Rust.rs template '    
+        $HelloWorld = {param([Alias(''msg'')]$message = "Hello world") "`"$message`""}
+        fn main() {
+            let msg = /*{param($msg = ''hello world'') "`"$msg`""}*/ ;
+            println!("{}",msg);
+        }
+        '
+        
+        $HelloWorldRust.Evaluate('hi')
+        $HelloWorldRust.Save(@{Message='Hello'}) |
+            Foreach-Object { 
+                $file = $_
+                if (Get-Command rustc -commandType Application) {
+                    $null = rustc $file.FullName
+                    & ".\$($file.Name.Replace($file.Extension, '.exe'))"
+                } else {
+                    Write-Error "Go install Rust"
+                }
+            }
+    }
+    it 'Language-Rust Example 3' {
+        '    
+        fn main() {
+            let msg = /*{param($msg = ''hello world'') "`"$msg`""}*/ ;
+            println!("{}",msg);
+        }
+        ' | Set-Content .\HelloWorld_Rust.ps.rs
+        Invoke-PipeScript .\HelloWorld_Rust.ps.rs
+    }
+    it 'Language-Rust Example 4' {
+        $HelloWorld = {param([Alias('msg')]$message = "Hello world") "`"$message`""}
+        "    
+        fn main() {
+            let msg = /*{$HelloWorld}*/ ;
+            println!(`"{}`",msg);
+        }
+        " | Set-Content .\HelloWorld_Rust.ps1.rs
+        Invoke-PipeScript .\HelloWorld_Rust.ps1.rs -Parameter @{message='hi'} |
+            Foreach-Object { 
+                $file = $_
+                if (Get-Command rustc -commandType Application) {
+                    $null = rustc $file.FullName
+                    & ".\$($file.Name.Replace($file.Extension, '.exe'))"
+                } else {
+                    Write-Error "Go install Rust"
+                }
+            }
+    }
+}
+
