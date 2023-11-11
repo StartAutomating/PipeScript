46ae2966e417baa0efc78ce3c583b86e43bff360
James Brundage <@github.com>
2023-11-11 13:21:37 -0800

    feat: Porting CSS Template to Language Function (Fixes #595)

--- Commands/Languages/CSS/CSS-Language.ps.ps1
+++ Commands/Languages/CSS/CSS-Language.ps.ps1
@@ -1,55 +1,39 @@
-<#
-.SYNOPSIS
-    CSS Template Transpiler.
-.DESCRIPTION
-    Allows PipeScript to generate CSS.
+Language function CSS {
+    <#
+    .SYNOPSIS
+        CSS Language Definition.
+    .DESCRIPTION
+        Allows PipeScript to generate CSS.
 
-    Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
+        Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
 
-    Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
+        Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
 
-    The CSS Inline Transpiler will consider the following syntax to be empty:
+        The CSS Inline Transpiler will consider the following syntax to be empty:
 
-    * ```(?<q>["'])\#[a-f0-9]{3}(\k<q>)```
-    * ```\#[a-f0-9]{6}```
-    * ```[\d\.](?>pt|px|em)```
-    * ```auto```
-.EXAMPLE
-    .> {
-        $StyleSheet = @'
-MyClass {
-    text-color: "#000000" /*{
-"'red'", "'green'","'blue'" | Get-Random
-    }*/;
-}
-'@
-        [Save(".\StyleSheet.ps1.css")]$StyleSheet
+        * ```(?<q>["'])\#[a-f0-9]{3}(\k<q>)```
+        * ```\#[a-f0-9]{6}```
+        * ```[\d\.](?>pt|px|em)```
+        * ```auto```
+    .EXAMPLE
+        .> {
+            $StyleSheet = '
+    MyClass {
+        text-color: "#000000" /*{
+    "''red''", "''green''","''blue''" | Get-Random
+        }*/;
     }
+    '
+            [Save(".\StyleSheet.ps1.css")]$StyleSheet
+        }
 
-    .> .\StyleSheet.ps1.css
-#>
-[ValidatePattern('\.s{0,1}css$')]
-param(
-# The command information.  This will include the path to the file.
-[Parameter(Mandatory,ValueFromPipeline,ParameterSetName='TemplateFile')]
-[Management.Automation.CommandInfo]
-$CommandInfo,
+        .> .\StyleSheet.ps1.css
+    #>
+    [ValidatePattern('\.s{0,1}css$')]
+    param(
+    )
 
-# If set, will return the information required to dynamically apply this template to any text.
-[Parameter(Mandatory,ParameterSetName='TemplateObject')]
-[switch]
-$AsTemplateObject,
 
-# A dictionary of parameters.
-[Collections.IDictionary]
-$Parameter,
-
-# A list of arguments.
-[PSObject[]]
-$ArgumentList
-)
-
-begin {
     # We start off by declaring a number of regular expressions:
     $startComment = '/\*' # * Start Comments ```\*```
     $endComment   = '\*/' # * End Comments   ```/*```
@@ -62,29 +46,10 @@ begin {
 
     $IgnoredContext = "(?<ignore>(?>$($ignoreEach -join '|'))\s{0,}){0,1}"
     # * StartRegex     ```$IgnoredContext + $StartComment + '{' + $Whitespace```
-    $startRegex = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
+    $StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
     # * EndRegex       ```$whitespace + '}' + $EndComment + $ignoredContext```
-    $endRegex   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
-    
-    # Create a splat containing arguments to the core inline transpiler
-    $Splat      = [Ordered]@{
-        StartPattern  = $startRegex
-        EndPattern    = $endRegex
-    }
-}
-
-process {
-    # If we have been passed a command
-    if ($CommandInfo) {
-        # add parameters related to the file.
-        $Splat.SourceFile = $commandInfo.Source -as [IO.FileInfo]
-        $Splat.SourceText = [IO.File]::ReadAllText($commandInfo.Source)
-    }
-    
-    if ($Parameter)    { $splat.Parameter = $Parameter }
-    if ($ArgumentList) { $splat.ArgumentList = $ArgumentList }
- 
-    $Splat.Begin = {
+    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+    $begin = {
         filter OutputCSS($depth) {
             $in = $_ # Capture the input object into a variable.
             if ($in -is [string]) { # If the input was a string
@@ -131,15 +96,8 @@ process {
             }
         }
     }
-    $Splat.ForeachObject = {
+    
+    $ForeachObject = {
         $_ | OutputCSS        
-    }
- 
-    # If we are being used within a keyword,
-    if ($AsTemplateObject) {
-        $splat # output the parameters we would use to evaluate this file.
-    } else {
-        # Otherwise, call the core template transpiler
-        .>PipeScript.Template @Splat # and output the changed file.
     }
 }
