6af9c43661cbf940568a702bc14389aec3ff51d4
James Brundage <@github.com>
2023-11-11 12:26:57 -0800

    feat: Porting Arudino Template to Language Function (Fixes #587)

---
+++ Commands/Languages/Arduino/Arduino-Language.ps.ps1
@@ -0,0 +1,32 @@
+Language function Arduino {
+<#
+.SYNOPSIS
+    Arduino Template Transpiler.
+.DESCRIPTION
+    Allows PipeScript to generate Arduino files.
+
+    Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
+
+    Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
+
+    The C++ Inline Transpiler will consider the following syntax to be empty:
+
+    * ```null```
+    * ```""```
+    * ```''```
+#>
+[ValidatePattern('\.(?>ino)$')]
+param()
+
+# Any Language can be parsed with a series of regular expresssions.
+
+$startComment = '/\*' # * Start Comments ```\*```
+$endComment   = '\*/' # * End Comments   ```/*```
+$Whitespace   = '[\s\n\r]{0,}'
+# * IgnoredContext ```String.empty```, ```null```, blank strings and characters
+$IgnoredContext = "(?<ignore>(?>$("null", '""', "''" -join '|'))\s{0,}){0,1}"
+# To support templates, a language has to declare `$StartPattern` and `$EndPattern`:
+$StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
+$endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+
+}
