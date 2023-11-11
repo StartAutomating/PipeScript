75011c434e77f35fad10f78c327dac6702be923d
James Brundage <@github.com>
2023-11-11 14:15:56 -0800

    feat: Porting JavaScript Template to Language Function (Fixes #604)

---
+++ Commands/Languages/JavaScript/JavaScript-Language.ps.ps1
@@ -0,0 +1,59 @@
+Language function JavaScript {
+<#
+.SYNOPSIS
+    JavaScript Language Definition.
+.DESCRIPTION
+    Allows PipeScript to generate JavaScript.
+
+    Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
+
+    String output from these blocks will be embedded directly.  All other output will be converted to JSON.
+
+    Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
+
+    The JavaScript Inline Transpiler will consider the following syntax to be empty:
+
+    * ```undefined```
+    * ```null```
+    * ```""```
+    * ```''```
+.EXAMPLE
+    $helloJs = Hello.js template '
+    msg = null /*{param($msg = ''hello world'') "`"$msg`""}*/ ;
+    if (console) {
+        console.log(msg);
+    }
+    '
+.EXAMPLE
+    $helloMsg = {param($msg = 'hello world') "`"$msg`""}
+    $helloJs = HelloWorld.js template "
+    msg = null /*{$helloMsg}*/;
+    if (console) {
+        console.log(msg);
+    }
+    "
+#>
+[ValidatePattern('\.js$')]
+param(
+)
+
+    # We start off by declaring a number of regular expressions:
+    $startComment = '/\*' # * Start Comments ```\*```
+    $endComment   = '\*/' # * End Comments   ```/*```
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * IgnoredContext ```String.empty```, ```null```, blank strings and characters
+    $IgnoredContext = "(?<ignore>(?>$("undefined", "null", '""', "''" -join '|'))\s{0,}){0,1}"
+    
+    $StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"    
+    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+
+    $ForeachObject = {
+        $in = $_
+        if (($in -is [string]) -or 
+            ($in -ne $null -and $in.GetType().IsPrimitive)) {
+            "$in"
+        } else {
+            "$(ConvertTo-Json -Depth 100 -InputObject $in)"
+        }
+    }
+}
