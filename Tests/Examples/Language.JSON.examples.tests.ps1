43e627a22192ad491aca556ff1d4056cbb2bb1db
StartAutomating <StartAutomating@github.com>
2023-11-11 23:57:21 +0000

    feat: Porting Perl Template to Language Function (Fixes #613)

--- Tests/Examples/Language.JSON.examples.tests.ps1
+++ Tests/Examples/Language.JSON.examples.tests.ps1
@@ -1,9 +1,11 @@
 
 describe 'Language.JSON' {
     it 'Language.JSON Example 1' {
-        a.json template "{
-        procs : null/*{Get-Process | Select Name, ID}*/
-        }"
+        Invoke-PipeScript {
+            a.json template "{
+            procs : null/*{Get-Process | Select Name, ID}*/
+            }"
+        }
     }
 }
 
