86e900ce7be975c899521c246db977704664ddf4
James Brundage <@github.com>
2023-11-11 14:34:50 -0800

    feat: Porting LaTeX Template to Language Function (Fixes #608)

---
+++ Commands/Languages/LaTeX/LaTeX-Language.ps.ps1
@@ -0,0 +1,19 @@
+Language function LaTeX {
+    <#
+    .SYNOPSIS
+        LaTeX Language Definition.
+    .DESCRIPTION
+        Allows PipeScript to generate Latex and Tex files.
+
+        Multiline comments with %{}% will be treated as blocks of PipeScript.    
+    #>
+    [ValidatePattern('\.(?>latex|tex)$')]
+    param()
+
+    # We start off by declaring a number of regular expressions:
+    $startComment = '\%\{' # * Start Comments ```%{```
+    $endComment   = '\}\%' # * End Comments   ```}%```
+    $Whitespace   = '[\s\n\r]{0,}'        
+    $StartPattern = "(?<PSStart>${startComment})"    
+    $EndPattern   = "(?<PSEnd>${endComment}\s{0,})"
+}
