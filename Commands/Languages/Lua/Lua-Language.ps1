05ed423bc7985e893fd2adfd6041d5be913bf8f7
StartAutomating <StartAutomating@github.com>
2023-11-11 22:41:03 +0000

    feat: Porting Lua Template to Language Function (Fixes #609)

---
+++ Commands/Languages/Lua/Lua-Language.ps1
@@ -0,0 +1,42 @@
+
+function Language.Lua {
+<#
+    .SYNOPSIS
+        LUA Template Transpiler.
+    .DESCRIPTION
+        Allows PipeScript to generate LUA.
+        Multiline comments like ```--{[[```  ```--}]]``` will be treated as blocks of PipeScript.    
+    #>
+[ValidatePattern('\.lua$')]
+param(
+                    
+                )
+$this = $myInvocation.MyCommand
+if (-not $this.Self) {
+$languageDefinition =
+New-Module {
+    <#
+    .SYNOPSIS
+        LUA Template Transpiler.
+    .DESCRIPTION
+        Allows PipeScript to generate LUA.
+        Multiline comments like ```--{[[```  ```--}]]``` will be treated as blocks of PipeScript.    
+    #>
+    [ValidatePattern('\.lua$')]
+    param()
+    # We start off by declaring a number of regular expressions:
+    $startComment = '\-\-\[\[\{' # * Start Comments ```--[[{```
+    $endComment   = '--\}\]\]'   # * End Comments   ```--}]]```
+    $Whitespace   = '[\s\n\r]{0,}'        
+    $StartPattern = "(?<PSStart>${startComment})"    
+    $EndPattern   = "(?<PSEnd>${endComment}\s{0,})"
+    Export-ModuleMember -Variable * -Function * -Alias *
+} -AsCustomObject
+$languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language.Lua")
+$this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
+}
+$this.Self
+}
+
+
