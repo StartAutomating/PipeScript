b385dad99c9e19286bb536e8ff50a031e63e98bf
StartAutomating <StartAutomating@github.com>
2023-11-11 23:57:21 +0000

    feat: Porting Perl Template to Language Function (Fixes #613)

--- Tests/Examples/Language.JavaScript.examples.tests.ps1
+++ Tests/Examples/Language.JavaScript.examples.tests.ps1
@@ -1,21 +1,27 @@
 
 describe 'Language.JavaScript' {
     it 'Language.JavaScript Example 1' {
-    $helloJs = Hello.js template '
-    msg = null /*{param($msg = ''hello world'') "`"$msg`""}*/ ;
-    if (console) {
-        console.log(msg);
+    Invoke-PipeScript {
+        Hello.js template '
+        msg = null /*{param($msg = ''hello world'') "`"$msg`""}*/ ;
+        if (console) {
+            console.log(msg);
+        }
+        '
+        
     }
-    '
     }
     it 'Language.JavaScript Example 2' {
-    $helloMsg = {param($msg = 'hello world') "`"$msg`""}
-    $helloJs = HelloWorld.js template "
-    msg = null /*{$helloMsg}*/;
-    if (console) {
-        console.log(msg);
+    Invoke-PipeScript {
+        $helloMsg = {param($msg = 'hello world') "`"$msg`""}
+        $helloJs = HelloWorld.js template "
+        msg = null /*{$helloMsg}*/;
+        if (console) {
+            console.log(msg);
+        }
+        "
+        $helloJs
     }
-    "
     }
 }
 
