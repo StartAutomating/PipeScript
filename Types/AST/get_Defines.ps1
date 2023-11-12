345154bfd701d49e021268fd8c8d763c1446256e
James Brundage <@github.com>
2023-11-11 18:59:38 -0800

    feat: [Ast].Defines (Fixes #643)

--- Types/AST/get_Defines.ps1
+++ Types/AST/get_Defines.ps1
@@ -1,3 +1,12 @@
+<#
+.SYNOPSIS
+    Gets all Definitions within an AST
+.DESCRIPTION
+    Gets all Function and Type Definitions within a PowerShell Abstract Syntax Tree
+.EXAMPLE
+    {function foo() { "foo"} class bar { $bar = "none"} }.Ast.Defines
+#>
+
 , @(
     foreach ($node in $this.ByType[@(
         [Management.Automation.Language.FunctionDefinitionAst]
