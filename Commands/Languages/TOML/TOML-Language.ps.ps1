b3e080e36aa46285b8b5c64b16b1bf52973a7a58
James Brundage <@github.com>
2023-11-11 17:03:55 -0800

    feat: Porting TOML Template to Language Function (Fixes #628)

---
+++ Commands/Languages/TOML/TOML-Language.ps.ps1
@@ -0,0 +1,32 @@
+Language function TOML {
+<#
+.SYNOPSIS
+    TOML Language Definition.
+.DESCRIPTION
+    Allows PipeScript to generate TOML.
+
+    Because TOML does not support comment blocks, PipeScript can be written inline inside of specialized Multiline string
+
+    PipeScript can be included in a TOML string that starts and ends with ```{}```, for example ```"""{}"""```
+.Example
+    .> {
+        $tomlContent = @'
+[seed]
+RandomNumber = """{Get-Random}"""
+'@
+        [OutputFile('.\RandomExample.ps1.toml')]$tomlContent
+    }
+
+    .> .\RandomExample.ps1.toml
+#>
+[ValidatePattern('\.toml$')]
+param()
+
+    # We start off by declaring a number of regular expressions:
+    
+    $startComment = '(?>"""\{)'
+    $endComment   = '(?>\}""")'
+    
+    $startPattern = "(?<PSStart>${startComment})"    
+    $endPattern   = "(?<PSEnd>${endComment})"
+}
