6667586a5f0fae9866dbb64bd89d4eb0cd4664f1
James Brundage <@github.com>
2023-11-11 19:14:48 -0800

    fix: SyntaxNode/Tree.Defines (Fixes #646)

---
+++ Types/Microsoft.CodeAnalysis/SyntaxTree/get_Defines.ps1
@@ -0,0 +1,21 @@
+<#
+.SYNOPSIS
+    Gets all Definitions within an AST
+.DESCRIPTION
+    Gets all Class and Type Definitions within a CSharp Abstract Syntax Tree
+.EXAMPLE
+    Parse-CSharp ('
+        public class MyClass {
+            public void MyMethod();    
+        }
+    ').Defines
+#>
+
+, @(
+    foreach ($node in $this.ByType[@(
+        [Microsoft.CodeAnalysis.CSharp.Syntax.ClassDeclarationSyntax]
+        [Microsoft.CodeAnalysis.CSharp.Syntax.ClassOrStructConstraintSyntax]
+    )]) {
+        $node
+    }
+)
