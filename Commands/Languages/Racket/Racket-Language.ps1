f46d2ef419a7d4786fcb48b8d9d2aaa629bc952c
StartAutomating <StartAutomating@github.com>
2023-11-12 00:36:27 +0000

    feat: Porting R Template to Language Function (Fixes #618)

---
+++ Commands/Languages/Racket/Racket-Language.ps1
@@ -0,0 +1,51 @@
+
+function Language.Racket {
+<#
+.SYNOPSIS
+    Racket Template Transpiler.
+.DESCRIPTION
+    Allows PipeScript to generate Racket.
+    Multiline comments blocks enclosed with {} will be treated as Blocks of PipeScript.
+    Multiline comments can be preceeded or followed by single-quoted strings, which will be ignored.
+    * ```''```
+    * ```{}```
+#>
+[ValidatePattern('\.rkt$')]
+param(
+                    
+                )
+$this = $myInvocation.MyCommand
+if (-not $this.Self) {
+$languageDefinition =
+New-Module {
+<#
+.SYNOPSIS
+    Racket Template Transpiler.
+.DESCRIPTION
+    Allows PipeScript to generate Racket.
+    Multiline comments blocks enclosed with {} will be treated as Blocks of PipeScript.
+    Multiline comments can be preceeded or followed by single-quoted strings, which will be ignored.
+    * ```''```
+    * ```{}```
+#>
+[ValidatePattern('\.rkt$')]
+param()
+    # We start off by declaring a number of regular expressions:
+    $startComment = '\#\|' # * Start Comments ```#|```
+    $endComment   = '\|\#' # * End Comments   ```|#```
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * IgnoredContext (single-quoted strings)    
+    # * StartPattern     ```$IgnoredContext + $StartComment + '{' + $Whitespace```
+    $startPattern = [Regex]::New("(?<PSStart>${startComment}\{$Whitespace)", 'IgnorePatternWhitespace')
+    # * EndPattern       ```$whitespace + '}' + $EndComment + $ignoredContext```
+    $endPattern   = "(?<PSEnd>$Whitespace\}${endComment}[\s-[\r\n]]{0,})"
+    Export-ModuleMember -Variable * -Function * -Alias *
+} -AsCustomObject
+$languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language.Racket")
+$this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
+}
+$this.Self
+}
+
+
