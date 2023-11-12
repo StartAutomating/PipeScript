4233f11418a8cb931a741b96b89da3dfa20c2d02
StartAutomating <StartAutomating@github.com>
2023-11-12 01:12:37 +0000

    feat: Porting WebAssembly Template to Language Function (Fixes #630)

---
+++ Commands/Languages/WebAssembly/WebAssembly-Language.ps1
@@ -0,0 +1,47 @@
+
+function Language.WebAssembly {
+<#
+.SYNOPSIS
+    WebAssembly Template Transpiler.
+.DESCRIPTION
+    Allows PipeScript to generate WebAssembly.    
+    Multiline comments blocks like this ```(;{
+    };)``` will be treated as blocks of PipeScript.
+#>
+[ValidatePattern('\.wat$')]
+param(
+                    
+                )
+$this = $myInvocation.MyCommand
+if (-not $this.Self) {
+$languageDefinition =
+New-Module {
+<#
+.SYNOPSIS
+    WebAssembly Template Transpiler.
+.DESCRIPTION
+    Allows PipeScript to generate WebAssembly.    
+    Multiline comments blocks like this ```(;{
+    };)``` will be treated as blocks of PipeScript.
+#>
+[ValidatePattern('\.wat$')]
+param()
+    # We start off by declaring a number of regular expressions:
+    $startComment = '\(\;' # * Start Comments ```(;```
+    $endComment   = '\;\)'   # * End Comments   ```;)```
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * StartPattern     ```$StartComment + '{' + $Whitespace```
+    $startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+    # * EndPattern       ```$whitespace + '}' + $EndComment```
+    $endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+    
+    Export-ModuleMember -Variable * -Function * -Alias *
+} -AsCustomObject
+$languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language.WebAssembly")
+$this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
+}
+$this.Self
+}
+
+
