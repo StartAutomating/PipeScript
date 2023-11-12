8044d568f024ca4b5a6cfdc46401ba3055cb4e04
James Brundage <@github.com>
2023-11-11 17:13:37 -0800

    feat: Porting XAML Template to Language Function (Fixes #631)

---
+++ Commands/Languages/XAML/XAML-Language.ps.ps1
@@ -0,0 +1,23 @@
+Language function XAML {
+<#
+.SYNOPSIS
+    XAML Language Definition.
+.DESCRIPTION
+    Allows PipeScript to generate XAML.
+
+    Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
+
+    Executed output will be converted to XAML
+#>
+[ValidatePattern('\.xaml$')]
+param()
+
+    # We start off by declaring a number of regular expressions:
+    $startComment = '<\!--' # * Start Comments ```<!--```
+    $endComment   = '-->'   # * End Comments   ```-->```
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * StartPattern     ```$StartComment + '{' + $Whitespace```
+    $startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+    # * EndPattern       ```$whitespace + '}' + $EndComment```
+    $endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+}
