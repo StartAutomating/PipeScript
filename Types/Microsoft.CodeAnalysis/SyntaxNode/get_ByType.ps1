13b1e3c6ecae8d558eccb54b22409d843c5cae37
James Brundage <@github.com>
2023-11-11 19:07:59 -0800

    fix: SyntaxNode.ByType Docs (Fixes #644)

--- Types/Microsoft.CodeAnalysis/SyntaxNode/get_ByType.ps1
+++ Types/Microsoft.CodeAnalysis/SyntaxNode/get_ByType.ps1
@@ -1,3 +1,12 @@
+<#
+.SYNOPSIS
+    Gets CSharp AST Nodes by type
+.DESCRIPTION
+    Gets a dictionary of all nodes in a CSharp AST beneath this point, grouped by type.
+.EXAMPLE
+    (Parse-CSharp '"Hello World";').ByType
+#>
+
 if (-not $this.'.ByType') {
     $ByType = [Collections.Generic.Dictionary[Type,Collections.Generic.List[PSObject]]]::new()
     foreach ($node in $this.FindAll({$true}, $true)) {
