71ca2be59c69b601c80ea0b0516f9c61fcb4653d
StartAutomating <StartAutomating@github.com>
2023-11-12 01:00:01 +0000

    feat: Porting SVG Template to Language Function (Fixes #626)

---
+++ Commands/Languages/SVG/SVG-Language.ps1
@@ -0,0 +1,86 @@
+
+function Language.SVG {
+<#
+.SYNOPSIS
+    SVG Language Definition.
+.DESCRIPTION
+    Allows PipeScript to generate SVG.
+    Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
+.EXAMPLE
+    $starsTemplate = Invoke-PipeScript {
+        Stars.svg template '
+            <!--{
+                Invoke-RestMethod https://pssvg.start-automating.com/Examples/Stars.svg
+            }-->
+        '
+    }
+    
+    $starsTemplate.Save("$pwd\Stars.svg")
+#>
+[ValidatePattern('\.svg$')]
+param(
+                    
+                )
+$this = $myInvocation.MyCommand
+if (-not $this.Self) {
+$languageDefinition =
+New-Module {
+<#
+.SYNOPSIS
+    SVG Language Definition.
+.DESCRIPTION
+    Allows PipeScript to generate SVG.
+    Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
+.EXAMPLE
+    $starsTemplate = Invoke-PipeScript {
+        Stars.svg template '
+            <!--{
+                Invoke-RestMethod https://pssvg.start-automating.com/Examples/Stars.svg
+            }-->
+        '
+    }
+    
+    $starsTemplate.Save("$pwd\Stars.svg")
+#>
+[ValidatePattern('\.svg$')]
+param()
+    # We start off by declaring a number of regular expressions:
+    $startComment = '<\!--' # * Start Comments ```<!--```
+    $endComment   = '-->'   # * End Comments   ```-->```
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * StartPattern     ```$StartComment + '{' + $Whitespace```
+    $startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+    # * EndPattern       ```$whitespace + '}' + $EndComment```
+    $endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+    
+    $ForeachObject = {
+        $in = $_
+        if (($in -is [string]) -or 
+            ($in.GetType -and $in.GetType().IsPrimitive)) {
+            $in
+        } elseif ($in.ChildNodes) {
+            foreach ($inChildNode in $in.ChildNodes) {
+                if ($inChildNode.NodeType -ne 'XmlDeclaration') {
+                    $inChildNode.OuterXml
+                }
+            }
+        } else {
+            $inXml = (ConvertTo-Xml -Depth 100 -InputObject $in)
+            foreach ($inChildNode in $inXml.ChildNodes) {
+                if ($inChildNode.NodeType -ne 'XmlDeclaration') {
+                    $inChildNode.OuterXml
+                }
+            }
+        }
+    }
+    
+    Export-ModuleMember -Variable * -Function * -Alias *
+} -AsCustomObject
+$languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language.SVG")
+$this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
+}
+$this.Self
+}
+
+
