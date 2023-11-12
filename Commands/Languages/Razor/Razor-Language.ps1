5a563e63c270a9783f8d3f07a9dada8aad01676f
StartAutomating <StartAutomating@github.com>
2023-11-12 00:39:20 +0000

    feat: Porting Razor Template to Language Function (Fixes #620)

---
+++ Commands/Languages/Razor/Razor-Language.ps1
@@ -0,0 +1,50 @@
+
+function Language.Razor {
+<#
+.SYNOPSIS
+    Razor Template Transpiler.    
+.DESCRIPTION
+    Allows PipeScript to generate Razor.
+    Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
+    JavaScript/CSS comment blocks like ```/*{}*/``` will also be treated as blocks of PipeScript.
+    Razor comment blocks like ```@*{}*@``` will also be treated as blocks of PipeScript.
+#>
+[ValidatePattern('\.(cshtml|razor)$')]
+param(
+                    
+                )
+$this = $myInvocation.MyCommand
+if (-not $this.Self) {
+$languageDefinition =
+New-Module {
+<#
+.SYNOPSIS
+    Razor Template Transpiler.    
+.DESCRIPTION
+    Allows PipeScript to generate Razor.
+    Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
+    JavaScript/CSS comment blocks like ```/*{}*/``` will also be treated as blocks of PipeScript.
+    Razor comment blocks like ```@*{}*@``` will also be treated as blocks of PipeScript.
+#>
+[ValidatePattern('\.(cshtml|razor)$')]
+param(
+)
+    # We start off by declaring a number of regular expressions:
+    $startComment = '(?><\!--|/\*|\@\*)' 
+    $endComment   = '(?>-->|\*/|\*@)'
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * StartPattern     ```$StartComment + '{' + $Whitespace```
+    $startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+    # * EndPattern       ```$whitespace + '}' + $EndComment```
+    $endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+    
+    Export-ModuleMember -Variable * -Function * -Alias *
+} -AsCustomObject
+$languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language.Razor")
+$this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
+}
+$this.Self
+}
+
+
