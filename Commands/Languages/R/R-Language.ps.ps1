2795db034a5fd00acdb4a5270107a22a89813856
James Brundage <@github.com>
2023-11-11 16:34:19 -0800

    feat: Porting R Template to Language Function (Fixes #618)

--- Commands/Languages/R/R-Language.ps.ps1
+++ Commands/Languages/R/R-Language.ps.ps1
@@ -1,6 +1,7 @@
+Language function R {
 <#
 .SYNOPSIS
-    R Template Transpiler.
+    R Language Definition.
 .DESCRIPTION
     Allows PipeScript to generate R.
 
@@ -41,59 +42,17 @@
     Invoke-PipeScript .\HelloWorld.ps1.r
 #>
 [ValidatePattern('\.r$')]
-param(
-# The command information.  This will include the path to the file.
-[Parameter(Mandatory,ValueFromPipeline,ParameterSetName='TemplateFile')]
-[Management.Automation.CommandInfo]
-$CommandInfo,
+param()
 
-# If set, will return the information required to dynamically apply this template to any text.
-[Parameter(Mandatory,ParameterSetName='TemplateObject')]
-[switch]
-$AsTemplateObject,
 
-# A dictionary of parameters.
-[Collections.IDictionary]
-$Parameter,
-
-# A list of arguments.
-[PSObject[]]
-$ArgumentList    
-)
-
-begin {
     # We start off by declaring a number of regular expressions:
     $startComment = '(?>(?<IsSingleLine>\#)\s{0,}(?:PipeScript)?\s{0,}\{)'
     $endComment   = '(?>\#\s{0,}\}\s{0,}(?:PipeScript)?\s{0,})'        
-    $startRegex = "(?<PSStart>${startComment})"
-    $endRegex   = "(?<PSEnd>${endComment})"
+    $startPattern = "(?<PSStart>${startComment})"
+    $endPattern   = "(?<PSEnd>${endComment})"
 
-    # Create a splat containing arguments to the core inline transpiler
-    $Splat      = [Ordered]@{
-        StartPattern  = $startRegex
-        EndPattern    = $endRegex
-         # Using -LinePattern will skip any inline code not starting with #
-        LinePattern   = "^\s{0,}\#\s{0,}"
-    }
-}
+        # Using -LinePattern will skip any inline code not starting with #
+    $LinePattern   = "^\s{0,}\#\s{0,}"
 
-process {
-    # If we have been passed a command
-    if ($CommandInfo) {
-        # add parameters related to the file.
-        $Splat.SourceFile = $commandInfo.Source -as [IO.FileInfo]
-        $Splat.SourceText = [IO.File]::ReadAllText($commandInfo.Source)
-    }
-    
-    if ($Parameter) { $splat.Parameter = $Parameter }
-    if ($ArgumentList) { $splat.ArgumentList = $ArgumentList }
-
-    # If we are being used within a keyword,
-    if ($AsTemplateObject) {
-        $splat # output the parameters we would use to evaluate this file.
-    } else {
-        # Otherwise, call the core template transpiler
-        .>PipeScript.Template @Splat # and output the changed file.
-    }
 }
 
