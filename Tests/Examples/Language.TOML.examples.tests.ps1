4d0f6ff8e79a2ce4fcf2ba85be8ae38677c4f27a
StartAutomating <StartAutomating@github.com>
2023-11-12 01:09:16 +0000

    feat: Porting TypeScript Template to Language Function (Fixes #629)

---
+++ Tests/Examples/Language.TOML.examples.tests.ps1
@@ -0,0 +1,14 @@
+
+describe 'Language.TOML' {
+    it 'Language.TOML Example 1' {
+    .> {
+        $tomlContent = @'
+[seed]
+RandomNumber = """{Get-Random}"""
+'@
+        [OutputFile('.\RandomExample.ps1.toml')]$tomlContent
+    }
+    .> .\RandomExample.ps1.toml
+    }
+}
+
