82b4364c2a70a607fd59a16a68ef37adeda3d663
StartAutomating <StartAutomating@github.com>
2023-11-12 00:36:25 +0000

    feat: Porting R Template to Language Function (Fixes #618)

--- Tests/Examples/Language.BASIC.examples.tests.ps1
+++ Tests/Examples/Language.BASIC.examples.tests.ps1
@@ -4,7 +4,7 @@ describe 'Language.BASIC' {
         Invoke-PipeScript {
             $VBScript = '    
         rem {
-        Uncommented lines between these two points will be ignored
+        rem # Uncommented lines between these two points will be ignored
         rem # Commented lines will become PipeScript / PowerShell.
         rem param($message = "hello world")
         rem "CreateObject(`"WScript.Shell`").Popup(`"$message`")" 
