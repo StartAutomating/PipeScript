f9bb9293f96f640a83defe80e6c9f9bdaee1561b
StartAutomating <StartAutomating@github.com>
2023-11-12 00:44:56 +0000

    feat: Porting RSS Template to Language Function (Fixes #621)

---
+++ Commands/Languages/RSS/RSS-Language.ps1
@@ -0,0 +1,45 @@
+
+function Language.RSS {
+<#
+.SYNOPSIS
+    RSS Language Definition.
+.DESCRIPTION
+    Allows PipeScript to generate RSS.
+    Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
+#>
+[ValidatePattern('\.rss$')]
+param(
+                    
+                )
+$this = $myInvocation.MyCommand
+if (-not $this.Self) {
+$languageDefinition =
+New-Module {
+<#
+.SYNOPSIS
+    RSS Language Definition.
+.DESCRIPTION
+    Allows PipeScript to generate RSS.
+    Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
+#>
+[ValidatePattern('\.rss$')]
+param()
+    # We start off by declaring a number of regular expressions:
+    $startComment = '<\!--' # * Start Comments ```<!--```
+    $endComment   = '-->'   # * End Comments   ```-->```
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * StartPattern     ```$StartComment + '{' + $Whitespace```
+    $startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+    # * EndPattern       ```$whitespace + '}' + $EndComment```
+    $endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+    
+    Export-ModuleMember -Variable * -Function * -Alias *
+} -AsCustomObject
+$languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language.RSS")
+$this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
+}
+$this.Self
+}
+
+
