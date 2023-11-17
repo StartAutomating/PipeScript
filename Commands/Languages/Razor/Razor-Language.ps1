95f76e69f4ad8221bf3ec1673b7e473030c5d73a
StartAutomating <StartAutomating@github.com>
2023-11-17 07:25:30 +0000

    feat: [ValidatePattern].Validate (Fixes #679)

--- Commands/Languages/Razor/Razor-Language.ps1
+++ Commands/Languages/Razor/Razor-Language.ps1
@@ -17,6 +17,7 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition =
 New-Module {
+    
 <#
 .SYNOPSIS
     Razor Template Transpiler.    
@@ -41,6 +42,7 @@ param(
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language")
 $languageDefinition.pstypenames.add("Language.Razor")
 $this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
 }
