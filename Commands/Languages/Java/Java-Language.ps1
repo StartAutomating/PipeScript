00e4aa5b7aa8c4f6a2aca6cd7d7878d6b6931424
StartAutomating <StartAutomating@github.com>
2023-11-17 07:25:29 +0000

    feat: [ValidatePattern].Validate (Fixes #679)

--- Commands/Languages/Java/Java-Language.ps1
+++ Commands/Languages/Java/Java-Language.ps1
@@ -20,6 +20,7 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition =
 New-Module {
+    
     <#
     .SYNOPSIS
         Java Template Transpiler.
@@ -46,6 +47,7 @@ New-Module {
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language")
 $languageDefinition.pstypenames.add("Language.Java")
 $this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
 }
