a1c288daec9f089d2069eda34f13fc382c2f4b21
StartAutomating <StartAutomating@github.com>
2023-11-17 07:25:28 +0000

    feat: [ValidatePattern].Validate (Fixes #679)

--- Commands/Languages/ADA/ADA-Language.ps1
+++ Commands/Languages/ADA/ADA-Language.ps1
@@ -37,6 +37,7 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition =
 New-Module {
+    
    
     <#
     .SYNOPSIS
@@ -82,6 +83,7 @@ New-Module {
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language")
 $languageDefinition.pstypenames.add("Language.ADA")
 $this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
 }
