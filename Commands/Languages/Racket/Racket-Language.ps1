9fb7a7586c1c4b203b9498cec8eb6d2d42237295
StartAutomating <StartAutomating@github.com>
2023-11-17 07:25:30 +0000

    feat: [ValidatePattern].Validate (Fixes #679)

--- Commands/Languages/Racket/Racket-Language.ps1
+++ Commands/Languages/Racket/Racket-Language.ps1
@@ -18,6 +18,7 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition =
 New-Module {
+    
 <#
 .SYNOPSIS
     Racket Template Transpiler.
@@ -42,6 +43,7 @@ param()
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language")
 $languageDefinition.pstypenames.add("Language.Racket")
 $this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
 }
