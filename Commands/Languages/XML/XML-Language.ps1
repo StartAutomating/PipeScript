b4500acb273f598cd7b30edd273301ca370c2c92
StartAutomating <StartAutomating@github.com>
2023-11-17 07:25:30 +0000

    feat: [ValidatePattern].Validate (Fixes #679)

--- Commands/Languages/XML/XML-Language.ps1
+++ Commands/Languages/XML/XML-Language.ps1
@@ -15,6 +15,7 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition =
 New-Module {
+    
 <#
 .SYNOPSIS
     XML Template Transpiler.
@@ -56,6 +57,7 @@ param()
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language")
 $languageDefinition.pstypenames.add("Language.XML")
 $this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
 }
