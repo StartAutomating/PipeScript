53376af56ef217ac61c3c48c87047b62bf7188e1
StartAutomating <StartAutomating@github.com>
2023-11-17 07:25:29 +0000

    feat: [ValidatePattern].Validate (Fixes #679)

--- Commands/Languages/Lua/Lua-Language.ps1
+++ Commands/Languages/Lua/Lua-Language.ps1
@@ -15,6 +15,7 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition =
 New-Module {
+    
     <#
     .SYNOPSIS
         LUA Template Transpiler.
@@ -33,6 +34,7 @@ New-Module {
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language")
 $languageDefinition.pstypenames.add("Language.Lua")
 $this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
 }
