53f1bfac2f3463e91f29aa02f25f24ba0488cccf
StartAutomating <StartAutomating@github.com>
2023-11-17 07:25:29 +0000

    feat: [ValidatePattern].Validate (Fixes #679)

--- Commands/Languages/Go/Go-Language.ps1
+++ Commands/Languages/Go/Go-Language.ps1
@@ -52,6 +52,7 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition =
 New-Module {
+    
     <#
     .SYNOPSIS
         Go Template Transpiler.
@@ -122,6 +123,7 @@ New-Module {
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language")
 $languageDefinition.pstypenames.add("Language.Go")
 $this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
 }
