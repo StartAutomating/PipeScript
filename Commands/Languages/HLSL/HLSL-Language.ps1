d81d2e600fe720aff2098069df4eb3a6e8636c41
StartAutomating <StartAutomating@github.com>
2023-11-17 07:25:29 +0000

    feat: [ValidatePattern].Validate (Fixes #679)

--- Commands/Languages/HLSL/HLSL-Language.ps1
+++ Commands/Languages/HLSL/HLSL-Language.ps1
@@ -15,6 +15,7 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition =
 New-Module {
+    
     <#
     .SYNOPSIS
         HLSL Language Definition.
@@ -37,6 +38,7 @@ New-Module {
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language")
 $languageDefinition.pstypenames.add("Language.HLSL")
 $this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
 }
