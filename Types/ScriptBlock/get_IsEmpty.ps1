b9de7314d17daa4ba367a935a2d847b7dc0a49b1
James Brundage <@github.com>
2023-11-18 13:45:45 -0800

    feat: [ScriptBlock]/[ScriptBlockAst].IsEmpty (Fixes #494)

---
+++ Types/ScriptBlock/get_IsEmpty.ps1
@@ -0,0 +1,19 @@
+<#
+.SYNOPSIS
+    Determines if a ScriptBlock is empty
+.DESCRIPTION
+    Determines if a ScriptBlock is empty.
+
+    A ScriptBlock is considered empty if it's Abstract Syntax Tree contains no statements or parameters.
+#>
+$ast = $this.Ast
+if ($ast.Body) {
+    $ast = $ast.Body
+}
+foreach ($property in $ast.psobject.Properties) {
+    if ($property.Name -notmatch 'Block$') { continue }
+    if ($property.Value.Statements.Count) { return $false }
+    if ($property.Value.Parameters.Count) { return $false }
+}
+
+return $true
