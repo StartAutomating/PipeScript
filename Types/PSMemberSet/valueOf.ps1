942a4932a52b7a6130fd1ddb22872fc3a87d4ee4
James Brundage <@github.com>
2023-11-18 16:55:15 -0800

    feat: PSMemberSet.propertyIsEnumerable (Fixes #685)

--- Types/PSMemberSet/valueOf.ps1
+++ Types/PSMemberSet/valueOf.ps1
@@ -1,14 +1,14 @@
 <#
 .SYNOPSIS
     Returns the Value Of an object
-.DESCRIPTION
-    This makes .PSObject more similar to a JavaScript prototype.
-    
+.DESCRIPTION    
     valueOf allows you to override the returned value (in _some_ circumstances).
 
     Defining a member named `valueOf` will make .PSObject.valueOf return that member's value or result.
 
     Otherwise, `.valueOf()` will return the .ImmediateBaseObject.
+.NOTES
+    This makes .PSObject more similar to a JavaScript prototype.
 #>
 param()
 $myName = 'valueOf'
