8436142379f27967f748a685e569830d23a5f025
James Brundage <@github.com>
2023-11-20 15:18:49 -0800

    fix: Fixing Routing Examples (re #696, #697)

--- Tests/Examples/Route.Uptime.examples.tests.ps1
+++ Tests/Examples/Route.Uptime.examples.tests.ps1
@@ -1,9 +1,7 @@
 
 describe 'Route.Uptime' {
     it 'Route.Uptime Example 1' {
-        Get-PipeScript -PipeScriptType Route |
-            Where-Object Name -Match 'Uptime' |
-            Foreach-Object { & $_ }
+        Route.Uptime        
     }
 }
 
--- Tests/Examples/Route.VersionInfo.examples.tests.ps1
+++ Tests/Examples/Route.VersionInfo.examples.tests.ps1
@@ -1,9 +1,7 @@
 
 describe 'Route.VersionInfo' {
     it 'Route.VersionInfo Example 1' {
-        Get-PipeScript -PipeScriptType Route |
-            Where-Object Name -Match 'VersionInfo' |
-            Foreach-Object { & $_ }
+        Route.VersionInfo
     }
 }
 
