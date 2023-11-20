2d7d0a1d4e8d92a32ac51e3f0c886040069a9216
James Brundage <@github.com>
2023-11-20 15:54:46 -0800

    feat: Grouping Object Aspects (Fixes #706, Fixes #707)

---
+++ Commands/Aspects/Object-Aspects.ps.ps1
@@ -0,0 +1,52 @@
+Aspect function GroupObjectByTypeName {
+    <#
+    .SYNOPSIS
+        Groups objects by type name
+    .DESCRIPTION
+        Groups objects by the first of their `.pstypenames`
+    .EXAMPLE
+        Get-ChildItem | Aspect.GroupByTypeName
+    #>
+    [Alias('Aspect.GroupByTypeName')]
+    param(<# One or More InputObjects #>[vfp()][PSObject[]]$InputObject)
+
+    begin {$groupedByTypeName = [Ordered]@{}}
+    process {
+        foreach ($inObj in $InputObject) {
+            $pstypeName = $inObj.pstypenames[0]
+            if (-not $groupedByTypeName[$pstypeName]) {
+                $groupedByTypeName[$pstypeName] = [Collections.Generic.List[PSObject]]::new()
+            }
+            $groupedByTypeName[$pstypeName].Add($inObj)
+        }
+    }
+    end {$groupedByTypeName}        
+}
+
+
+Aspect function GroupObjectByType {
+    <#
+    .SYNOPSIS
+        Groups objects by types
+    .DESCRIPTION
+        Groups objects by objects by their .NET type
+    .EXAMPLE
+        Get-ChildItem | Aspect.GroupByType
+    #>
+    [Alias('Aspect.GroupByType')]
+    param(<# One or More Input Objects #>[vfp()][PSObject[]]$InputObject)
+
+    begin {$groupedByType = [Ordered]@{}}
+    process {
+        foreach ($inObj in $InputObject) {
+            $dotNetType = $inObj.GetType()
+            if (-not $groupedByType[$dotNetType]) {
+                $groupedByType[$dotNetType] = [Collections.Generic.List[PSObject]]::new()
+            }
+            $groupedByType[$dotNetType].Add($inObj)
+        }
+    }
+    end {
+        $groupedByType
+    }        
+}
