68a756d5407ae848e1ec717baea8fd19fba325fe
StartAutomating <StartAutomating@github.com>
2023-11-21 01:06:06 +0000

    fix: Export-PipeScript Invoke-Build Handling (Fixes #711)

--- Tests/Examples/Language.Docker.examples.tests.ps1
+++ Tests/Examples/Language.Docker.examples.tests.ps1
@@ -14,7 +14,7 @@ describe 'Language.Docker' {
             # }
             ENV PSModulePath ./Modules
             #{
-            #if ($DockerInstallModules) { "RUN /opt/microsoft/powershell/7/pwsh --noprofile --nologo -c Install-Module Splatter,ugit -Scope CurrentUser -Force"} 
+            #if ($DockerInstallModules) { "RUN /opt/microsoft/powershell/7/pwsh --nologo -c Install-Module Splatter,ugit -Scope CurrentUser -Force"} 
             #}
         '
         $dockerFile | Set-Content .\PipeScript
