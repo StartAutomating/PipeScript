70b52db7e65284e457751f733e085fc22c19c799
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:17 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/PHP/PHP-Language.ps1
+++ Commands/Languages/PHP/PHP-Language.ps1
@@ -14,27 +14,13 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-<#
-.SYNOPSIS
-    PHP Template Transpiler.
-.DESCRIPTION
-    Allows PipeScript to generate PHP.
-    Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
-    JavaScript/CSS/PHP comment blocks like ```/*{}*/``` will also be treated as blocks of PipeScript.
-#>
-[ValidatePattern('\.php$')]
-param()
-    # We start off by declaring a number of regular expressions:
-    $startComment = '(?><\!--|/\*)' # * Start Comments ```<!--```
-    $endComment   = '(?>-->|\*/)'   # * End Comments   ```-->```
-    $Whitespace   = '[\s\n\r]{0,}'
-    # * StartPattern     ```$StartComment + '{' + $Whitespace```
-    $startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
-    # * EndPattern       ```$whitespace + '}' + $EndComment```
-    $endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+$languageDefinition = New-Module {
+    $LanguageName = 'PHP'
+    $startComment = '(?><\!--|/\*)'
+$endComment   = '(?>-->|\*/)'
+$Whitespace   = '[\s\n\r]{0,}'
+$startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+$endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
