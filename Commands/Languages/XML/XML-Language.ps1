42cc94d671ad26a52e832b739d60260890932076
StartAutomating <StartAutomating@github.com>
2023-11-24 22:08:50 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/XML/XML-Language.ps1
+++ Commands/Languages/XML/XML-Language.ps1
@@ -8,19 +8,22 @@ function Language.XML {
     Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
 #>
 [ValidatePattern('\.xml$')]
-param(
-                    
-                )
+param()
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition = New-Module {
     $LanguageName = 'XML'
-    $startComment = '<\!--'
-$endComment   = '-->'
-$Whitespace   = '[\s\n\r]{0,}'
-$startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
-$endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
-$ForeachObject = {
+    
+    # We start off by declaring a number of regular expressions:
+    $startComment = '<\!--' # * Start Comments ```<!--```
+    $endComment   = '-->'   # * End Comments   ```-->```
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * StartPattern     ```$StartComment + '{' + $Whitespace```
+    $startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+    # * EndPattern       ```$whitespace + '}' + $EndComment```
+    $endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+    
+    $ForeachObject = {
         $in = $_
         if (($in -is [string]) -or 
             ($in.GetType -and $in.GetType().IsPrimitive)) {
