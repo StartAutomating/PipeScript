f4c8895cdb5dc5cdcbbf169b6492aa17def77356
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:17 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/PowerShell/PowerShellData-Language.ps1
+++ Commands/Languages/PowerShell/PowerShellData-Language.ps1
@@ -16,34 +16,17 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-<#
-.SYNOPSIS
-    PSD1 Template Transpiler.
-.DESCRIPTION
-    Allows PipeScript to generate PSD1.
-    Multiline comments blocks enclosed with {} will be treated as Blocks of PipeScript.
-    Multiline comments can be preceeded or followed by single-quoted strings, which will be ignored.
-    * ```''```
-    * ```{}```
-#>
-[ValidatePattern('\.psd1$')]
-param()
-# We start off by declaring a number of regular expressions:
-$startComment = '<\#' # * Start Comments ```\*```
-$endComment   = '\#>' # * End Comments   ```/*```
+$languageDefinition = New-Module {
+    $LanguageName = 'PowerShellData'
+    $startComment = '<\#'
+$endComment   = '\#>'
 $Whitespace   = '[\s\n\r]{0,}'
-# * IgnoredContext (single-quoted strings)
 $IgnoredContext = "
 (?<ignore>
     (?>'((?:''|[^'])*)')
     [\s - [ \r\n ] ]{0,}
 ){0,1}"
-# * StartPattern     ```$IgnoredContext + $StartComment + '{' + $Whitespace```
 $StartPattern = [regex]::New("(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)", 'IgnorePatternWhitespace')
-# * EndPattern       ```$whitespace + '}' + $EndComment + $ignoredContext```
 $endPattern   = "(?<PSEnd>$Whitespace\}${endComment}[\s-[\r\n]]{0,}${IgnoredContext})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
