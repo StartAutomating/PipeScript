cd6c67e2ff37a11ecfd7fc70912ffd951ffc38e7
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:18 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/XAML/XAML-Language.ps1
+++ Commands/Languages/XAML/XAML-Language.ps1
@@ -14,27 +14,13 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-<#
-.SYNOPSIS
-    XAML Language Definition.
-.DESCRIPTION
-    Allows PipeScript to generate XAML.
-    Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
-    Executed output will be converted to XAML
-#>
-[ValidatePattern('\.xaml$')]
-param()
-    # We start off by declaring a number of regular expressions:
-    $startComment = '<\!--' # * Start Comments ```<!--```
-    $endComment   = '-->'   # * End Comments   ```-->```
-    $Whitespace   = '[\s\n\r]{0,}'
-    # * StartPattern     ```$StartComment + '{' + $Whitespace```
-    $startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
-    # * EndPattern       ```$whitespace + '}' + $EndComment```
-    $endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+$languageDefinition = New-Module {
+    $LanguageName = 'XAML'
+    $startComment = '<\!--'
+$endComment   = '-->'
+$Whitespace   = '[\s\n\r]{0,}'
+$startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+$endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
