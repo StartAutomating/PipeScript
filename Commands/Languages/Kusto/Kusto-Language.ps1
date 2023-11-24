af863102d8e33b353e5a92fef6c8ced8185c3cdb
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:17 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/Kusto/Kusto-Language.ps1
+++ Commands/Languages/Kusto/Kusto-Language.ps1
@@ -18,31 +18,14 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-    <#
-    .SYNOPSIS
-        Kusto Template Transpiler.
-    .DESCRIPTION
-        Allows PipeScript to generate Kusto files.
-        Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
-        Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
-        The Kusto Template Transpiler will consider the following syntax to be empty:
-        * ```null```
-        * ```""```
-        * ```''```
-    #>
-    [ValidatePattern('\.kql$')]
-    param()
-    # We start off by declaring a number of regular expressions:
-    $startComment = '/\*' # * Start Comments ```\*```
-    $endComment   = '\*/' # * End Comments   ```/*```
-    $Whitespace   = '[\s\n\r]{0,}'
-    # * IgnoredContext ```String.empty```, ```null```, blank strings and characters
-    $IgnoredContext = "(?<ignore>(?>$("null", '""', "''" -join '|'))\s{0,}){0,1}"    
-    $StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"    
-    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+$languageDefinition = New-Module {
+    $LanguageName = 'Kusto'
+    $startComment = '/\*'
+$endComment   = '\*/'
+$Whitespace   = '[\s\n\r]{0,}'
+$IgnoredContext = "(?<ignore>(?>$("null", '""', "''" -join '|'))\s{0,}){0,1}"
+$StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
+$EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
