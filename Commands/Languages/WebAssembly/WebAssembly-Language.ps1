4ed6a0a6fefcd9e963c9631debd1df18f8cc7f42
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:18 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/WebAssembly/WebAssembly-Language.ps1
+++ Commands/Languages/WebAssembly/WebAssembly-Language.ps1
@@ -14,28 +14,13 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-<#
-.SYNOPSIS
-    WebAssembly Template Transpiler.
-.DESCRIPTION
-    Allows PipeScript to generate WebAssembly.    
-    Multiline comments blocks like this ```(;{
-    };)``` will be treated as blocks of PipeScript.
-#>
-[ValidatePattern('\.wat$')]
-param()
-    # We start off by declaring a number of regular expressions:
-    $startComment = '\(\;' # * Start Comments ```(;```
-    $endComment   = '\;\)'   # * End Comments   ```;)```
-    $Whitespace   = '[\s\n\r]{0,}'
-    # * StartPattern     ```$StartComment + '{' + $Whitespace```
-    $startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
-    # * EndPattern       ```$whitespace + '}' + $EndComment```
-    $endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
-    
+$languageDefinition = New-Module {
+    $LanguageName = 'WebAssembly'
+    $startComment = '\(\;'
+$endComment   = '\;\)'
+$Whitespace   = '[\s\n\r]{0,}'
+$startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+$endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
