b868ca81805d31898213cb4647a26e52c60770b9
StartAutomating <StartAutomating@github.com>
2023-11-24 22:52:52 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/ObjectiveC/ObjectiveC-Language.ps1
+++ Commands/Languages/ObjectiveC/ObjectiveC-Language.ps1
@@ -18,8 +18,7 @@ param()
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition = New-Module {
-    $LanguageName = 'ObjectiveC'
-    
+    param()
 # We start off by declaring a number of regular expressions:
     $startComment = '/\*' # * Start Comments ```\*```
     $endComment   = '\*/' # * End Comments   ```/*```
@@ -30,6 +29,7 @@ $languageDefinition = New-Module {
     $StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
     # * EndRegex       ```$whitespace + '}' + $EndComment + $ignoredContext```
     $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+    $LanguageName = 'ObjectiveC'
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
