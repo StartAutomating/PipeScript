cc65660da4072e374542db0a8aa190d65e5e71d4
StartAutomating <StartAutomating@github.com>
2023-11-24 22:52:52 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/OpenSCAD/OpenSCAD-Language.ps1
+++ Commands/Languages/OpenSCAD/OpenSCAD-Language.ps1
@@ -37,8 +37,7 @@ param()
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition = New-Module {
-    $LanguageName = 'OpenSCAD'
-    
+    param()
     # We start off by declaring a number of regular expressions:
     $startComment = '/\*' # * Start Comments ```\*```
     $endComment   = '\*/' # * End Comments   ```/*```
@@ -49,6 +48,7 @@ $languageDefinition = New-Module {
     $StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
     # * EndRegex       ```$whitespace + '}' + $EndComment + $ignoredContext```
     $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+    $LanguageName = 'OpenSCAD'
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
