6cac7d70900508b3cb9d37cfa5373063def2d6fe
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:17 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/Kotlin/Kotlin-Language.ps1
+++ Commands/Languages/Kotlin/Kotlin-Language.ps1
@@ -18,32 +18,14 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-<#
-.SYNOPSIS
-    Kotlin Language Definition.
-.DESCRIPTION
-    Allows PipeScript to generate Kotlin.
-    Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
-    Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
-    The Kotlin Inline PipeScript Transpiler will consider the following syntax to be empty:
-    * ```null```
-    * ```""```
-    * ```''```
-#>
-[ValidatePattern('\.kt$')]
-param()
-# We start off by declaring a number of regular expressions:
-$startComment = '/\*' # * Start Comments ```\*```
-$endComment   = '\*/' # * End Comments   ```/*```
+$languageDefinition = New-Module {
+    $LanguageName = 'Kotlin'
+    $startComment = '/\*'
+$endComment   = '\*/'
 $Whitespace   = '[\s\n\r]{0,}'
-# * IgnoredContext ```String.empty```, ```null```, blank strings and characters
 $IgnoredContext = "(?<ignore>(?>$("null", '""', "''" -join '|'))\s{0,}){0,1}"
 $StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
 $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
-    
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
