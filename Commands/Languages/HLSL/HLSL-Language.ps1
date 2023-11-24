d1fc33967c7bb7d4b1e57c264756b8bc6b753172
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:17 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/HLSL/HLSL-Language.ps1
+++ Commands/Languages/HLSL/HLSL-Language.ps1
@@ -13,28 +13,13 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-    <#
-    .SYNOPSIS
-        HLSL Language Definition.
-    .DESCRIPTION
-        Allows PipeScript to generate HLSL.
-        Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
-    #>
-    [ValidatePattern('\.hlsl$')]
-    param(
-    )
-    
-    # We start off by declaring a number of regular expressions:
-    $startComment = '/\*' # * Start Comments ```\*```
-    $endComment   = '\*/' # * End Comments   ```/*```
-    $Whitespace   = '[\s\n\r]{0,}'
-    # * StartRegex     ```$StartComment + '{' + $Whitespace```
-    $StartPattern = "(?<PSStart>${startComment}\{$Whitespace)"
-    # * EndRegex       ```$whitespace + '}' + $EndComment```
-    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+$languageDefinition = New-Module {
+    $LanguageName = 'HLSL'
+    $startComment = '/\*'
+$endComment   = '\*/'
+$Whitespace   = '[\s\n\r]{0,}'
+$StartPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+$EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
