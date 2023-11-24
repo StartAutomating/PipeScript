93849d687903499368334dc33492a6a22806c2b0
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:16 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/Bicep/Bicep-Language.ps1
+++ Commands/Languages/Bicep/Bicep-Language.ps1
@@ -16,32 +16,14 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-    <#
-    .SYNOPSIS
-        Bicep Language Definition.
-    .DESCRIPTION
-        Allows PipeScript to generate Bicep templates.
-        Multiline comments blocks like ```/*{}*/``` will be treated as blocks of PipeScript.
-        Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
-        * ```''```
-        * ```{}```
-    #>
-    [ValidatePattern('\.bicep$')]
-    param(
-    )
-    # We start off by declaring a number of regular expressions:
-    $startComment = '/\*' # * Start Comments ```\*```
-    $endComment   = '\*/' # * End Comments   ```/*```
-    $Whitespace   = '[\s\n\r]{0,}'
-    # * IgnoredContext 
-    $IgnoredContext = "(?<ignore>(?>$("''", "\{\}" -join '|'))\s{0,}){0,1}"
-    # * StartRegex     ```$IgnoredContext + $StartComment + '{' + $Whitespace```
-    $StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
-    # * EndRegex       ```$whitespace + '}' + $EndComment + $ignoredContext```
-    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+$languageDefinition = New-Module {
+    $LanguageName = 'Bicep'
+    $startComment = '/\*'
+$endComment   = '\*/'
+$Whitespace   = '[\s\n\r]{0,}'
+$IgnoredContext = "(?<ignore>(?>$("''", "\{\}" -join '|'))\s{0,}){0,1}"
+$StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
+$EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
