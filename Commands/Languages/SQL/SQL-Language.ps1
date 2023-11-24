13e3b069dca0884b46569a63759a91a16ac2716f
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:18 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/SQL/SQL-Language.ps1
+++ Commands/Languages/SQL/SQL-Language.ps1
@@ -41,65 +41,22 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-<#
-.SYNOPSIS
-    SQL Template Transpiler.
-.DESCRIPTION
-    Allows PipeScript to generate SQL.
-    PipeScript can be embedded in multiline or singleline format
-    
-    In multiline format, PipeScript will be embedded within: `/*{...}*/`
-    
-    In single line format
-    -- { or -- PipeScript{  begins a PipeScript block
-    -- } or -- }PipeScript  ends a PipeScript block
-    ```SQL    
-    -- {
-    Uncommented lines between these two points will be ignored
-    --  # Commented lines will become PipeScript / PowerShell.
-    -- param($message = 'hello world')
-    -- "-- $message"
-    -- }
-    ```
-.EXAMPLE
-    Invoke-PipeScript {
-        $SQLScript = '    
-    -- {
-    Uncommented lines between these two points will be ignored
-    --  # Commented lines will become PipeScript / PowerShell.
-    -- param($message = "hello world")
-    -- "-- $message"
-    -- }
-    '
-    
-        [OutputFile('.\HelloWorld.ps1.sql')]$SQLScript
-    }
-    Invoke-PipeScript .\HelloWorld.ps1.sql
-#>
-[ValidatePattern('\.sql$')]
-param()
-    
-    # We start off by declaring a number of regular expressions:
+$languageDefinition = New-Module {
+    $LanguageName = 'SQL'
     $startComment = '(?>
         (?>
             (?<IsSingleLine>--)|
             /\*
         )\s{0,}(?:PipeScript)?\s{0,}\{)'
-    $endComment   = '(?>
+$endComment   = '(?>
         --\s{0,}\}\s{0,}(?:PipeScript)?
         |
         \}\*/(?:PipeScript)?\s{0,}
     )    
     '
-    $startPattern   = "(?<PSStart>${startComment})"
-    $endPattern     = [Regex]::new("(?<PSEnd>${endComment})",'IgnoreCase,IgnorePatternWhitespace')
-    # Create a splat containing arguments to the core inline transpiler
-    
-         # Using -LinePattern will skip any inline code not starting with --
-    $LinePattern   = "^\s{0,}--\s{0,}"    
+$startPattern   = "(?<PSStart>${startComment})"
+$endPattern     = [Regex]::new("(?<PSEnd>${endComment})",'IgnoreCase,IgnorePatternWhitespace')
+$LinePattern   = "^\s{0,}--\s{0,}"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
