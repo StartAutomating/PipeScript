acfb26566b721d83058af75a8764dfe8404bee25
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:17 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/ObjectiveC/ObjectiveC-Language.ps1
+++ Commands/Languages/ObjectiveC/ObjectiveC-Language.ps1
@@ -19,34 +19,14 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-<#
-.SYNOPSIS
-    Objective-C Language Definition.
-.DESCRIPTION
-    Allows PipeScript to generate Objective C/C++.
-    Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
-    Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
-    The Objective C Inline Transpiler will consider the following syntax to be empty:
-    * ```null```
-    * ```nil```
-    * ```""```
-    * ```''```
-#>
-[ValidatePattern('\.(?>m|mm)$')]
-param()
-# We start off by declaring a number of regular expressions:
-    $startComment = '/\*' # * Start Comments ```\*```
-    $endComment   = '\*/' # * End Comments   ```/*```
-    $Whitespace   = '[\s\n\r]{0,}'
-    # * IgnoredContext ```String.empty```, ```null```, blank strings and characters
-    $IgnoredContext = "(?<ignore>(?>$("null", "nil", '""', "''" -join '|'))\s{0,}){0,1}"
-    # * StartRegex     ```$IgnoredContext + $StartComment + '{' + $Whitespace```
-    $StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
-    # * EndRegex       ```$whitespace + '}' + $EndComment + $ignoredContext```
-    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+$languageDefinition = New-Module {
+    $LanguageName = 'ObjectiveC'
+    $startComment = '/\*'
+$endComment   = '\*/'
+$Whitespace   = '[\s\n\r]{0,}'
+$IgnoredContext = "(?<ignore>(?>$("null", "nil", '""', "''" -join '|'))\s{0,}){0,1}"
+$StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
+$EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
