42e00a4a567cee2a38d60c4daf381f9c5473ce0b
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:18 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/TOML/TOML-Language.ps1
+++ Commands/Languages/TOML/TOML-Language.ps1
@@ -23,35 +23,12 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-<#
-.SYNOPSIS
-    TOML Language Definition.
-.DESCRIPTION
-    Allows PipeScript to generate TOML.
-    Because TOML does not support comment blocks, PipeScript can be written inline inside of specialized Multiline string
-    PipeScript can be included in a TOML string that starts and ends with ```{}```, for example ```"""{}"""```
-.Example
-    .> {
-        $tomlContent = @'
-[seed]
-RandomNumber = """{Get-Random}"""
-'@
-        [OutputFile('.\RandomExample.ps1.toml')]$tomlContent
-    }
-    .> .\RandomExample.ps1.toml
-#>
-[ValidatePattern('\.toml$')]
-param()
-    # We start off by declaring a number of regular expressions:
-    
+$languageDefinition = New-Module {
+    $LanguageName = 'TOML'
     $startComment = '(?>"""\{)'
-    $endComment   = '(?>\}""")'
-    
-    $startPattern = "(?<PSStart>${startComment})"    
-    $endPattern   = "(?<PSEnd>${endComment})"
+$endComment   = '(?>\}""")'
+$startPattern = "(?<PSStart>${startComment})"
+$endPattern   = "(?<PSEnd>${endComment})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
