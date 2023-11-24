87781b72ce124c76f25dd13fda2692f219263e17
StartAutomating <StartAutomating@github.com>
2023-11-24 22:08:49 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/Ruby/Ruby-Language.ps1
+++ Commands/Languages/Ruby/Ruby-Language.ps1
@@ -8,22 +8,24 @@ function Language.Ruby {
     PipeScript can be embedded in a multiline block that starts with ```=begin{``` and ends with } (followed by ```=end```)
 #>
 [ValidatePattern('\.rb$')]
-param(
-                    
-                )
+param()
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition = New-Module {
     $LanguageName = 'Ruby'
+    
+    # We start off by declaring a number of regular expressions:
+    
     $startComment = '(?>[\r\n]{1,3}\s{0,}=begin[\s\r\n]{0,}\{)'
-$endComment   = '(?>}[\r\n]{1,3}\s{0,}=end)'
-$ignoreEach = '[\d\.]+',
+    $endComment   = '(?>}[\r\n]{1,3}\s{0,}=end)'
+    $ignoreEach = '[\d\.]+',
         '""',
         "''"
-$IgnoredContext = "(?<ignore>(?>$($ignoreEach -join '|'))\s{0,}){0,1}"
-$Interpreter  = @($ExecutionContext.SessionState.InvokeCommand.GetCommand('ruby', 'Application'))[0]
-$startPattern = "(?<PSStart>${IgnoredContext}${startComment})"
-$endPattern   = "(?<PSEnd>${endComment}${IgnoredContext})"
+    $IgnoredContext = "(?<ignore>(?>$($ignoreEach -join '|'))\s{0,}){0,1}"
+    $Interpreter  = @($ExecutionContext.SessionState.InvokeCommand.GetCommand('ruby', 'Application'))[0] # Get the first python, if presentS
+    
+    $startPattern = "(?<PSStart>${IgnoredContext}${startComment})"    
+    $endPattern   = "(?<PSEnd>${endComment}${IgnoredContext})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
