3a5dbc6260a62378e639cf4081a5f93d2b229564
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:16 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/CSS/CSS-Language.ps1
+++ Commands/Languages/CSS/CSS-Language.ps1
@@ -31,52 +31,20 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-    <#
-    .SYNOPSIS
-        CSS Language Definition.
-    .DESCRIPTION
-        Allows PipeScript to generate CSS.
-        Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
-        Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
-        The CSS Inline Transpiler will consider the following syntax to be empty:
-        * ```(?<q>["'])\#[a-f0-9]{3}(\k<q>)```
-        * ```\#[a-f0-9]{6}```
-        * ```[\d\.](?>pt|px|em)```
-        * ```auto```
-    .EXAMPLE
-        .> {
-            $StyleSheet = '
-    MyClass {
-        text-color: "#000000" /*{
-    "''red''", "''green''","''blue''" | Get-Random
-        }*/;
-    }
-    '
-            [Save(".\StyleSheet.ps1.css")]$StyleSheet
-        }
-        .> .\StyleSheet.ps1.css
-    #>
-    [ValidatePattern('\.s{0,1}css$')]
-    param(
-    )
-    # We start off by declaring a number of regular expressions:
-    $startComment = '/\*' # * Start Comments ```\*```
-    $endComment   = '\*/' # * End Comments   ```/*```
-    $Whitespace   = '[\s\n\r]{0,}'
-    $ignoreEach = '[''"]{0,1}\#[a-f0-9]{6}[''"]{0,1}', 
+$languageDefinition = New-Module {
+    $LanguageName = 'CSS'
+    $startComment = '/\*'
+$endComment   = '\*/'
+$Whitespace   = '[\s\n\r]{0,}'
+$ignoreEach = '[''"]{0,1}\#[a-f0-9]{6}[''"]{0,1}', 
         '[''"]{0,1}\#[a-f0-9]{3}[''"]{0,1}',
         '[\d\.]+(?>em|pt|px){0,1}',
         'auto',
         "''"
-    $IgnoredContext = "(?<ignore>(?>$($ignoreEach -join '|'))\s{0,}){0,1}"
-    # * StartRegex     ```$IgnoredContext + $StartComment + '{' + $Whitespace```
-    $StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
-    # * EndRegex       ```$whitespace + '}' + $EndComment + $ignoredContext```
-    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
-    $begin = {
+$IgnoredContext = "(?<ignore>(?>$($ignoreEach -join '|'))\s{0,}){0,1}"
+$StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
+$EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+$begin = {
         filter OutputCSS($depth) {
             $in = $_ # Capture the input object into a variable.
             if ($in -is [string]) { # If the input was a string
@@ -123,8 +91,7 @@ New-Module {
             }
         }
     }
-    
-    $ForeachObject = {
+$ForeachObject = {
         $_ | OutputCSS        
     }
     Export-ModuleMember -Variable * -Function * -Alias *
