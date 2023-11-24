d2505952ed56bb2c3cb21349de12969396d02ba3
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:17 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/Python/Python-Language.ps1
+++ Commands/Languages/Python/Python-Language.ps1
@@ -30,43 +30,13 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-<#
-.SYNOPSIS
-    Python Language Definition.
-.DESCRIPTION
-    Allows PipeScript to generate Python.
-    Because Python does not support multiline comment blocks, PipeScript can be written inline inside of multiline string
-    PipeScript can be included in a Python string that starts and ends with ```{}```, for example ```"""{}"""```
-.Example
-    .> {
-       $pythonContent = @'
-"""{
-$msg = "Hello World", "Hey There", "Howdy" | Get-Random
-@"
-print("$msg")
-"@
-}"""
-'@
-        [OutputFile('.\HelloWorld.ps1.py')]$PythonContent
-    }
-    .> .\HelloWorld.ps1.py
-.EXAMPLE
-    'print("Hello World")' > .\HelloWorld.py
-    Invoke-PipeScript .\HelloWorld.py # Should -Be 'Hello World'
-#>
-[ValidatePattern('\.py$')]
-param()
-    # We start off by declaring a number of regular expressions:
-    
+$languageDefinition = New-Module {
+    $LanguageName = 'Python'
     $startComment = '(?>"""\{)'
-    $endComment   = '(?>\}""")'
-    
-    $startPattern = "(?<PSStart>${startComment})"    
-    $endPattern   = "(?<PSEnd>${endComment})"
-    $Interpreter  = @($ExecutionContext.SessionState.InvokeCommand.GetCommand('python', 'Application'))[0] # Get the first python, if present
+$endComment   = '(?>\}""")'
+$startPattern = "(?<PSStart>${startComment})"
+$endPattern   = "(?<PSEnd>${endComment})"
+$Interpreter  = @($ExecutionContext.SessionState.InvokeCommand.GetCommand('python', 'Application'))[0]
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
