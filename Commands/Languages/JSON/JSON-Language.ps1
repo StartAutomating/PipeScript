de89c7844fd5b6902cb3433269f039522c6f49a3
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:17 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/JSON/JSON-Language.ps1
+++ Commands/Languages/JSON/JSON-Language.ps1
@@ -25,41 +25,15 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-    <#
-    .SYNOPSIS
-        JSON PipeScript Transpiler.
-    .DESCRIPTION
-        Allows PipeScript to generate JSON.
-        Multiline comments blocks like ```/*{}*/``` will be treated as blocks of PipeScript.
-        String output from these blocks will be embedded directly.  All other output will be converted to JSON.
-        Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
-        * ```null```
-        * ```""```
-        * ```{}```
-        * ```[]```
-    .EXAMPLE
-        Invoke-PipeScript {
-            a.json template "{
-            procs : null/*{Get-Process | Select Name, ID}*/
-            }"
-        }
-    #>
-    [ValidatePattern('\.json$')]
-    param(
-    )
-    # We start off by declaring a number of regular expressions:
-    $startComment = '/\*' # * Start Comments ```\*```
-    $endComment   = '\*/' # * End Comments   ```/*```
-    $Whitespace   = '[\s\n\r]{0,}'
-    # * IgnoredContext ```String.empty```, ```null```, blank strings and characters
-    $IgnoredContext = "(?<ignore>(?>$('null', '""', '\{\}', '\[\]' -join '|'))\s{0,}){0,1}"
-    
-    $StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"    
-    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
-    $ForeachObject = {
+$languageDefinition = New-Module {
+    $LanguageName = 'JSON'
+    $startComment = '/\*'
+$endComment   = '\*/'
+$Whitespace   = '[\s\n\r]{0,}'
+$IgnoredContext = "(?<ignore>(?>$('null', '""', '\{\}', '\[\]' -join '|'))\s{0,}){0,1}"
+$StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
+$EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+$ForeachObject = {
         $in = $_
         if (($in -is [string]) -or 
             ($in.GetType -and $in.GetType().IsPrimitive)) {
