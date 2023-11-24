fff45340a4c7e1243993922c75fb10d2749bdd1e
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:16 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/Bash/Bash-Language.ps1
+++ Commands/Languages/Bash/Bash-Language.ps1
@@ -32,45 +32,12 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-    <#
-    .SYNOPSIS
-        Bash Language Definition
-    .DESCRIPTION
-        Defines Bash within PipeScript.
-        This allows Rust to be templated.
-        Heredocs named PipeScript{} will be treated as blocks of PipeScript.
-        ```bash
-        <<PipeScript{}
-        
-        # This will be considered PipeScript / PowerShell, and will return the contents of a bash script.
-        PipeScript{}
-        ```
-    .EXAMPLE
-        Invoke-PipeScript {
-            $bashScript = '
-            echo ''hello world''
-            <<PipeScript{}
-                "echo ''$(''hi'',''yo'',''sup'' | Get-Random)''"
-            PipeScript{}
-        '
-        
-            [OutputFile('.\HelloWorld.ps1.sh')]$bashScript
-        }
-        Invoke-PipeScript .\HelloWorld.ps1.sh
-    #>
-    [ValidatePattern('\.sh$')]
-    param(
-    )
-    # We start off by declaring a number of regular expressions:
-    $startComment = '(?>\<\<PipeScript\{\})' 
-    $endComment   = '(?>PipeScript\{\})'    
-    
-    $StartPattern = "(?<PSStart>${startComment})"
-    
-    $EndPattern   = "(?<PSEnd>${endComment})"    
+$languageDefinition = New-Module {
+    $LanguageName = 'Bash'
+    $startComment = '(?>\<\<PipeScript\{\})'
+$endComment   = '(?>PipeScript\{\})'
+$StartPattern = "(?<PSStart>${startComment})"
+$EndPattern   = "(?<PSEnd>${endComment})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
