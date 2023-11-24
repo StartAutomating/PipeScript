1b959f569eb7ea6d2deebca7fa24ce8493784a45
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:18 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/RSS/RSS-Language.ps1
+++ Commands/Languages/RSS/RSS-Language.ps1
@@ -13,27 +13,13 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-<#
-.SYNOPSIS
-    RSS Language Definition.
-.DESCRIPTION
-    Allows PipeScript to generate RSS.
-    Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
-#>
-[ValidatePattern('\.rss$')]
-param()
-    # We start off by declaring a number of regular expressions:
-    $startComment = '<\!--' # * Start Comments ```<!--```
-    $endComment   = '-->'   # * End Comments   ```-->```
-    $Whitespace   = '[\s\n\r]{0,}'
-    # * StartPattern     ```$StartComment + '{' + $Whitespace```
-    $startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
-    # * EndPattern       ```$whitespace + '}' + $EndComment```
-    $endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
-    
+$languageDefinition = New-Module {
+    $LanguageName = 'RSS'
+    $startComment = '<\!--'
+$endComment   = '-->'
+$Whitespace   = '[\s\n\r]{0,}'
+$startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+$endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
