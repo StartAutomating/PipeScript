7bebc734adbd116bc506ed4c1b49990d0cabcdbc
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:16 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/ATOM/ATOM-Language.ps1
+++ Commands/Languages/ATOM/ATOM-Language.ps1
@@ -15,27 +15,13 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-    <#
-    .SYNOPSIS
-        ATOM Language Definition
-    .DESCRIPTION
-        Defines ATOM within PipeScript.
-        This allows ATOM to be templated.
-        
-        Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
-    #>
-    [ValidatePattern('\.atom$')]
-    param()
-    # We start off by declaring a number of regular expressions:
-    $startComment = '<\!--' # * Start Comments ```<!--```
-    $endComment   = '-->'   # * End Comments   ```-->```
-    $Whitespace   = '[\s\n\r]{0,}'
-    # To support templates, a language has to declare `$StartPattern` and `$EndPattern`:
-    $StartPattern = "(?<PSStart>${startComment}\{$Whitespace)"
-    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+$languageDefinition = New-Module {
+    $LanguageName = 'ATOM'
+    $startComment = '<\!--'
+$endComment   = '-->'
+$Whitespace   = '[\s\n\r]{0,}'
+$StartPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+$EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
