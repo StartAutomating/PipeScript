2f834d1d4b5cc1f7114864605eb6489ed656e45a
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:17 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/Perl/Perl-Language.ps1
+++ Commands/Languages/Perl/Perl-Language.ps1
@@ -25,32 +25,8 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-<#
-.SYNOPSIS
-    Perl Template Transpiler.
-.DESCRIPTION
-    Allows PipeScript to generate Perl.
-    Also Transpiles Plain Old Document
-    PipeScript can be embedded in a Plain Old Document block that starts with ```=begin PipeScript``` and ends with ```=end PipeScript```.    
-.EXAMPLE
-    .> {
-        $HelloWorldPerl = @'
-=begin PipeScript
-$msg = "hello", "hi", "hey", "howdy" | Get-Random
-"print(" + '"' + $msg + '");'
-=end   PipeScript
-'@
-        [Save(".\HelloWorld.ps1.pl")]$HelloWorldPerl
-    }
-    .> .\HelloWorld.ps1.pl
-#>
-[ValidatePattern('\.(?>pl|pod)$')]
-param()
-    # We start off by declaring a number of regular expressions:
-    
+$languageDefinition = New-Module {
+    $LanguageName = 'Perl'
     $startComment = '(?>
         (?>^|\[\r\n]{1,2})\s{0,}
         =begin
@@ -58,15 +34,14 @@ param()
         (?>Pipescript|\{)
         [\s\r\n\{]{0,}
     )'
-    $endComment   = '(?>
+$endComment   = '(?>
         [\r\n]{1,3}
         \s{0,}
         =end
         (?>\}|\s{1,}PipeScript[\s\r\n\}]{0,})
     )'
-    
-    $startPattern = "(?<PSStart>${startComment})"    
-    $endPattern   = "(?<PSEnd>${endComment})"
+$startPattern = "(?<PSStart>${startComment})"
+$endPattern   = "(?<PSEnd>${endComment})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
