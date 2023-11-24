d1ab7e69a03b151c1bcd8b39c2213ad1919c4d06
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:17 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/Razor/Razor-Language.ps1
+++ Commands/Languages/Razor/Razor-Language.ps1
@@ -15,30 +15,13 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-<#
-.SYNOPSIS
-    Razor Template Transpiler.    
-.DESCRIPTION
-    Allows PipeScript to generate Razor.
-    Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
-    JavaScript/CSS comment blocks like ```/*{}*/``` will also be treated as blocks of PipeScript.
-    Razor comment blocks like ```@*{}*@``` will also be treated as blocks of PipeScript.
-#>
-[ValidatePattern('\.(cshtml|razor)$')]
-param(
-)
-    # We start off by declaring a number of regular expressions:
-    $startComment = '(?><\!--|/\*|\@\*)' 
-    $endComment   = '(?>-->|\*/|\*@)'
-    $Whitespace   = '[\s\n\r]{0,}'
-    # * StartPattern     ```$StartComment + '{' + $Whitespace```
-    $startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
-    # * EndPattern       ```$whitespace + '}' + $EndComment```
-    $endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
-    
+$languageDefinition = New-Module {
+    $LanguageName = 'Razor'
+    $startComment = '(?><\!--|/\*|\@\*)'
+$endComment   = '(?>-->|\*/|\*@)'
+$Whitespace   = '[\s\n\r]{0,}'
+$startPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+$endPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
