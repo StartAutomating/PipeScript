a589af1ff0aa6888b1986aa6d5f45a8264464a73
StartAutomating <StartAutomating@github.com>
2023-11-24 21:48:17 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/HCL/HCL-Language.ps1
+++ Commands/Languages/HCL/HCL-Language.ps1
@@ -18,36 +18,14 @@ param(
                 )
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
-$languageDefinition =
-New-Module {
-    
-    <#
-    .SYNOPSIS
-        HCL Template Transpiler.
-    .DESCRIPTION
-        Allows PipeScript to generate HCL.
-        Multiline comments blocks like ```/*{}*/``` will be treated as blocks of PipeScript.
-        Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
-        * ```null```
-        * ```""```
-        * ```{}```
-        * ```[]```
-    #>
-    [ValidatePattern('\.hcl$')]
-    param(
-    )
-    
-    # We start off by declaring a number of regular expressions:
-    $startComment = '/\*' # * Start Comments ```\*```
-    $endComment   = '\*/' # * End Comments   ```/*```
-    $Whitespace   = '[\s\n\r]{0,}'
-    # * IgnoredContext ```String.empty```, ```null```, blank strings and characters
-    $IgnoredContext = "(?<ignore>(?>$("null", '""', "\{\}", "\[\]" -join '|'))\s{0,}){0,1}"
-    
-    $StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
-    
-    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
-    
+$languageDefinition = New-Module {
+    $LanguageName = 'HCL'
+    $startComment = '/\*'
+$endComment   = '\*/'
+$Whitespace   = '[\s\n\r]{0,}'
+$IgnoredContext = "(?<ignore>(?>$("null", '""', "\{\}", "\[\]" -join '|'))\s{0,}){0,1}"
+$StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
+$EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
 $languageDefinition.pstypenames.clear()
