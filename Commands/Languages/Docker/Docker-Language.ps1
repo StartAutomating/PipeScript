39ec5d5b2fdb61f58585ad49e96653ffd8efe436
StartAutomating <StartAutomating@github.com>
2023-11-24 22:08:48 +0000

    fix: Adding LanguageName to Language Definitions (Fixes #563)

--- Commands/Languages/Docker/Docker-Language.ps1
+++ Commands/Languages/Docker/Docker-Language.ps1
@@ -27,21 +27,32 @@ function Language.Docker {
         Invoke-PipeScript .\PipeScript.Example.ps.Dockerfile
     #>
 [ValidatePattern('\.?Dockerfile$')]
-param(
-                    
-                )
+param()
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition = New-Module {
     $LanguageName = 'Docker'
+    
     $SingleLineCommentStart = '\#'
-$startComment = "(?>(?<IsSingleLine>$SingleLineCommentStart)(?>PipeScript|PS)?\{)"
-$endComment   = "(?>$SingleLineCommentStart(?:PipeScript)?\})"
-$StartPattern = "(?<PSStart>${startComment})"
-$EndPattern   = "(?<PSEnd>${endComment})"
-$LinePattern   = "^\s{0,}$SingleLineCommentStart\s{0,}"
-$ReplaceOutputFileName = ".+?Dockerfile", 'Dockerfile'
-$ForeachObject = {
+    # Any Language can be parsed with a series of regular expresssions.
+    # For languages that only support single comments:
+    # * The capture group IsSingleLine must be defined.
+    # * Whitespace should not be allowed (it makes nested blocks hard to end)
+    $startComment = "(?>(?<IsSingleLine>$SingleLineCommentStart)(?>PipeScript|PS)?\{)"
+    $endComment   = "(?>$SingleLineCommentStart(?:PipeScript)?\})"
+    # To support templates, a language has to declare `$StartPattern` and `$EndPattern`:
+    $StartPattern = "(?<PSStart>${startComment})"
+    $EndPattern   = "(?<PSEnd>${endComment})"
+    # A language can also declare a `$LinePattern`.  If it does, any inline code that does not match this pattern will be skipped.
+    # Using -LinePattern will skip any inline code not starting with #
+    # Note:  Parameter blocks cannot begin on the same line as the opening brace
+    $LinePattern   = "^\s{0,}$SingleLineCommentStart\s{0,}"
+    # No matter what the input file was, Docker's output file must be 'Dockerfile'
+    $ReplaceOutputFileName = ".+?Dockerfile", 'Dockerfile'
+    
+    # Any language can define a ForeachObject.
+    # This is how output will be treated in a template.
+    $ForeachObject = {
         
         $striginifed = "$_"
         @(
