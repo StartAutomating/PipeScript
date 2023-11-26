c9954c56de5aecb225f50c48be0c021ff9447aa0
StartAutomating <StartAutomating@github.com>
2023-11-26 22:36:53 +0000

    fix: Update-PipeScript - Keeping repeated blanks ( Fixes #742 )

--- Commands/Languages/BASIC/BASIC-Language.ps1
+++ Commands/Languages/BASIC/BASIC-Language.ps1
@@ -5,14 +5,20 @@ function Language.BASIC {
     BASIC Language Definition.
 .DESCRIPTION
     Allows PipeScript to generate Basic, Visual Basic, and Visual Basic Scripts.
+
     Because Basic only allow single-line comments, this is done using a pair of comment markers.
+
     A single line comment, followed by a { (or PipeScript { ) begins a block of pipescript.
+
     A single line comment, followed by a } (or PipeScript } ) ends a block of pipescript.
+
     Only commented lines within this block will be interpreted as PipeScript.
             
     ```VBScript    
     rem {
+
     rem # Uncommented lines between these two points will be ignored
+
     rem # Commented lines will become PipeScript / PowerShell.
     rem param($message = "hello world")
     rem "CreateObject(`"WScript.Shell`").Popup(`"$message`")" 
@@ -22,14 +28,18 @@ function Language.BASIC {
 Invoke-PipeScript {
     $VBScript = '    
 rem {
+
 rem # Uncommented lines between these two points will be ignored
+
 rem # Commented lines will become PipeScript / PowerShell.
 rem param($message = "hello world")
 rem "CreateObject(`"WScript.Shell`").Popup(`"$message`")" 
 rem }
 '
+
     [OutputFile('.\HelloWorld.ps1.vbs')]$VBScript
 }
+
 Invoke-PipeScript .\HelloWorld.ps1.vbs
 #>
 [ValidatePattern('\.(?>bas|vbs{0,1})$')]
@@ -39,6 +49,7 @@ if (-not $this.Self) {
 $languageDefinition = New-Module {
     param(
 )
+
 # We start off by declaring a number of regular expressions:
 $SingleLineCommentStart = '(?>''|rem)'
 $startComment = "(?>(?<IsSingleLine>$SingleLineCommentStart)\s{0,}(?:PipeScript)?\s{0,}\{)"
