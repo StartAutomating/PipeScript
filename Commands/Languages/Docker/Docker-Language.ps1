88cbf362476fc6cd65889c1b602db0d19e67388d
StartAutomating <StartAutomating@github.com>
2023-11-26 22:36:54 +0000

    fix: Update-PipeScript - Keeping repeated blanks ( Fixes #742 )

--- Commands/Languages/Docker/Docker-Language.ps1
+++ Commands/Languages/Docker/Docker-Language.ps1
@@ -5,20 +5,27 @@ function Language.Docker {
         Docker language definition
     .DESCRIPTION
         Defines the Docker language within PipeScript.
+
         This allows the Dockerfile to be generated with PipeScript.
     .EXAMPLE        
         $DockerFile = '
             FROM mcr.microsoft.com/powershell
+
             #{
+
             ## If we are in a module directory, copy the module
+
             # $LoadedModuleInPath = (Get-Module | Split-Path) -match ([Regex]::Escape($pwd)) | Select -first 1
             # if ($LoadedModuleInPath) { "COPY ./ ./Modules/$($LoadedModuleInPath | Split-Path -Leaf)" } 
+
             #}
+
             #{
             # param($DockerProfileScript)
             # if ($DockerProfileScript) { "COPY ./$DockerProfileScript /root/.config/powershell/Microsoft.PowerShell_profile.ps1"} 
             # }
             ENV PSModulePath ./Modules
+
             #{
             #if ($DockerInstallModules) { "RUN /opt/microsoft/powershell/7/pwsh --nologo -c Install-Module Splatter,ugit -Scope CurrentUser -Force"} 
             #}
@@ -39,13 +46,17 @@ $languageDefinition = New-Module {
     # * Whitespace should not be allowed (it makes nested blocks hard to end)
     $startComment = "(?>(?<IsSingleLine>$SingleLineCommentStart)(?>PipeScript|PS)?\{)"
     $endComment   = "(?>$SingleLineCommentStart(?:PipeScript)?\})"
+
     # To support templates, a language has to declare `$StartPattern` and `$EndPattern`:
     $StartPattern = "(?<PSStart>${startComment})"
     $EndPattern   = "(?<PSEnd>${endComment})"
+
     # A language can also declare a `$LinePattern`.  If it does, any inline code that does not match this pattern will be skipped.
     # Using -LinePattern will skip any inline code not starting with #
+
     # Note:  Parameter blocks cannot begin on the same line as the opening brace
     $LinePattern   = "^\s{0,}$SingleLineCommentStart\s{0,}"
+
     # No matter what the input file was, Docker's output file must be 'Dockerfile'
     $ReplaceOutputFileName = ".+?Dockerfile", 'Dockerfile'
     
