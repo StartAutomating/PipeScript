a3ca5a272161c7da68536ab4e705a5845e00e340
StartAutomating <StartAutomating@github.com>
2023-11-26 22:36:53 +0000

    fix: Update-PipeScript - Keeping repeated blanks ( Fixes #742 )

--- Commands/Languages/ADA/ADA-Language.ps1
+++ Commands/Languages/ADA/ADA-Language.ps1
@@ -5,19 +5,25 @@ function Language.ADA {
         ADA Language Definition
     .DESCRIPTION
         Defines ADA within PipeScript.
+
         This allows ADA to be templated.
         
         Because ADA Scripts only allow single-line comments, this is done using a pair of comment markers.
+
         -- { or -- PipeScript{  begins a PipeScript block
+
         -- } or -- }PipeScript  ends a PipeScript block                
     .EXAMPLE
         Invoke-PipeScript {
             $AdaScript = '    
         with Ada.Text_IO;
+
         procedure Hello_World is
         begin
             -- {
+
             Uncommented lines between these two points will be ignored
+
             --  # Commented lines will become PipeScript / PowerShell.
             -- param($message = "hello world")        
             -- "Ada.Text_IO.Put_Line (`"$message`");"
@@ -27,6 +33,7 @@ function Language.ADA {
         
             [OutputFile('.\HelloWorld.ps1.adb')]$AdaScript
         }
+
         Invoke-PipeScript .\HelloWorld.ps1.adb
     #>
 [ValidatePattern('\.ad[bs]$')]
@@ -35,9 +42,11 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition = New-Module {
     param()
+
     # Any Language can be parsed with a series of regular expresssions.
     $startComment = '(?>(?<IsSingleLine>--)\s{0,}(?:PipeScript)?\s{0,}\{)'
     $endComment   = '(?>--\s{0,}\}\s{0,}(?:PipeScript)?\s{0,})'
+
     # To support templates, a language has to declare `$StartPattern` and `$EndPattern`:
     $StartPattern = "(?<PSStart>${startComment})"
     $EndPattern   = "(?<PSEnd>${endComment})"
