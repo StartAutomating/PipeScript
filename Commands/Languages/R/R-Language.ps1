4caec4c043a1b9fe9f1672e0b35878d91ba89831
StartAutomating <StartAutomating@github.com>
2023-11-26 22:36:55 +0000

    fix: Update-PipeScript - Keeping repeated blanks ( Fixes #742 )

--- Commands/Languages/R/R-Language.ps1
+++ Commands/Languages/R/R-Language.ps1
@@ -5,30 +5,41 @@ function Language.R {
     R Language Definition.
 .DESCRIPTION
     Allows PipeScript to generate R.
+
     Because R Scripts only allow single-line comments, this is done using a pair of comment markers.
+
     # { or # PipeScript{  begins a PipeScript block
+
     # } or # }PipeScript  ends a PipeScript block
+
     ~~~r    
     # {
+
     Uncommented lines between these two points will be ignored
+
     #  # Commented lines will become PipeScript / PowerShell.
     # param($message = "hello world")
     # "puts `"$message`""
+
     # }
     ~~~
 .EXAMPLE
     Invoke-PipeScript {
         $rScript = '    
     # {
+
     Uncommented lines between these two points will be ignored
+
     #  # Commented lines will become PipeScript / PowerShell.
     # param($message = "hello world")
     # "print(`"$message`")"
+
     # }
     '
     
         [OutputFile('.\HelloWorld.ps1.r')]$rScript
     }
+
     Invoke-PipeScript .\HelloWorld.ps1.r
 #>
 [ValidatePattern('\.r$')]
@@ -37,11 +48,14 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition = New-Module {
     param()
+
+
     # We start off by declaring a number of regular expressions:
     $startComment = '(?>(?<IsSingleLine>\#)\s{0,}(?:PipeScript)?\s{0,}\{)'
     $endComment   = '(?>\#\s{0,}\}\s{0,}(?:PipeScript)?\s{0,})'        
     $startPattern = "(?<PSStart>${startComment})"
     $endPattern   = "(?<PSEnd>${endComment})"
+
         # Using -LinePattern will skip any inline code not starting with #
     $LinePattern   = "^\s{0,}\#\s{0,}"
     $LanguageName = 'R'
