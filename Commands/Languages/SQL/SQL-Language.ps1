51d89057daefcfe3289f20c1f51f5f325c6d0fdd
StartAutomating <StartAutomating@github.com>
2023-11-26 22:36:55 +0000

    fix: Update-PipeScript - Keeping repeated blanks ( Fixes #742 )

--- Commands/Languages/SQL/SQL-Language.ps1
+++ Commands/Languages/SQL/SQL-Language.ps1
@@ -5,16 +5,22 @@ function Language.SQL {
     SQL Template Transpiler.
 .DESCRIPTION
     Allows PipeScript to generate SQL.
+
     PipeScript can be embedded in multiline or singleline format
     
     In multiline format, PipeScript will be embedded within: `/*{...}*/`
     
     In single line format
+
     -- { or -- PipeScript{  begins a PipeScript block
+
     -- } or -- }PipeScript  ends a PipeScript block
+
     ```SQL    
     -- {
+
     Uncommented lines between these two points will be ignored
+
     --  # Commented lines will become PipeScript / PowerShell.
     -- param($message = 'hello world')
     -- "-- $message"
@@ -24,7 +30,9 @@ function Language.SQL {
     Invoke-PipeScript {
         $SQLScript = '    
     -- {
+
     Uncommented lines between these two points will be ignored
+
     --  # Commented lines will become PipeScript / PowerShell.
     -- param($message = "hello world")
     -- "-- $message"
@@ -33,6 +41,7 @@ function Language.SQL {
     
         [OutputFile('.\HelloWorld.ps1.sql')]$SQLScript
     }
+
     Invoke-PipeScript .\HelloWorld.ps1.sql
 #>
 [ValidatePattern('\.sql$')]
@@ -56,6 +65,7 @@ $languageDefinition = New-Module {
     '
     $startPattern   = "(?<PSStart>${startComment})"
     $endPattern     = [Regex]::new("(?<PSEnd>${endComment})",'IgnoreCase,IgnorePatternWhitespace')
+
     # Create a splat containing arguments to the core inline transpiler
     
          # Using -LinePattern will skip any inline code not starting with --
