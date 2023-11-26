78918d041f1f1888116bcaad7ec688e3e5decf1a
StartAutomating <StartAutomating@github.com>
2023-11-26 22:36:55 +0000

    fix: Update-PipeScript - Keeping repeated blanks ( Fixes #742 )

--- Commands/Languages/Ruby/Ruby-Language.ps1
+++ Commands/Languages/Ruby/Ruby-Language.ps1
@@ -5,6 +5,7 @@ function Language.Ruby {
     Ruby Language Definition.
 .DESCRIPTION
     Allows PipeScript to generate Ruby.
+
     PipeScript can be embedded in a multiline block that starts with ```=begin{``` and ends with } (followed by ```=end```)
 #>
 [ValidatePattern('\.rb$')]
@@ -13,14 +14,19 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition = New-Module {
     param()
+
+
     # We start off by declaring a number of regular expressions:
     
     $startComment = '(?>[\r\n]{1,3}\s{0,}=begin[\s\r\n]{0,}\{)'
     $endComment   = '(?>}[\r\n]{1,3}\s{0,}=end)'
+
     $ignoreEach = '[\d\.]+',
         '""',
         "''"
+
     $IgnoredContext = "(?<ignore>(?>$($ignoreEach -join '|'))\s{0,}){0,1}"
+
     $Interpreter  = @($ExecutionContext.SessionState.InvokeCommand.GetCommand('ruby', 'Application'))[0] # Get the first python, if presentS
     
     $startPattern = "(?<PSStart>${IgnoredContext}${startComment})"    
