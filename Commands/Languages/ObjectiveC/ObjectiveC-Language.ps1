328ded13aa4bb68577cfd3d712048614ff1fb47d
StartAutomating <StartAutomating@github.com>
2023-11-26 22:36:54 +0000

    fix: Update-PipeScript - Keeping repeated blanks ( Fixes #742 )

--- Commands/Languages/ObjectiveC/ObjectiveC-Language.ps1
+++ Commands/Languages/ObjectiveC/ObjectiveC-Language.ps1
@@ -5,9 +5,13 @@ function Language.ObjectiveC {
     Objective-C Language Definition.
 .DESCRIPTION
     Allows PipeScript to generate Objective C/C++.
+
     Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
+
     Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
+
     The Objective C Inline Transpiler will consider the following syntax to be empty:
+
     * ```null```
     * ```nil```
     * ```""```
