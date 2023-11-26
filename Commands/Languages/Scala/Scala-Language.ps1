7a0f255ed9daf7400f1dec8b5189894295d33b30
StartAutomating <StartAutomating@github.com>
2023-11-26 22:36:55 +0000

    fix: Update-PipeScript - Keeping repeated blanks ( Fixes #742 )

--- Commands/Languages/Scala/Scala-Language.ps1
+++ Commands/Languages/Scala/Scala-Language.ps1
@@ -5,9 +5,13 @@ function Language.Scala {
     Scala Template Transpiler.
 .DESCRIPTION
     Allows PipeScript to generate Scala.
+
     Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
+
     Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
+
     The Scala Template Transpiler will consider the following syntax to be empty:
+
     * ```null```
     * ```""```
     * ```''```
@@ -18,6 +22,7 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition = New-Module {
     param()
+
     # We start off by declaring a number of regular expressions:
     $startComment = '/\*' # * Start Comments ```\*```
     $endComment   = '\*/' # * End Comments   ```/*```
