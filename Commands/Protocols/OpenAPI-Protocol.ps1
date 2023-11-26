c7770c4c8e8df14c06880b585eb43680ccb5ce71
StartAutomating <StartAutomating@github.com>
2023-11-26 22:36:56 +0000

    fix: Update-PipeScript - Keeping repeated blanks ( Fixes #742 )

--- Commands/Protocols/OpenAPI-Protocol.ps1
+++ Commands/Protocols/OpenAPI-Protocol.ps1
@@ -1,10 +1,13 @@
 
 function Protocol.OpenAPI {
+
+
     <#
     .SYNOPSIS
         OpenAPI protocol
     .DESCRIPTION
         Converts an OpenAPI to PowerShell.
+
         This protocol will generate a PowerShell script to communicate with an OpenAPI.
     .EXAMPLE
         # We can easily create a command that talks to a single REST api described in OpenAPI.
@@ -16,6 +19,7 @@ function Protocol.OpenAPI {
                 param()
             }
         }
+
         Get-GitHubIssue -Owner StartAutomating -Repo PipeScript
     .EXAMPLE
         # We can also make a general purpose command that talks to every endpoint in a REST api.
@@ -26,6 +30,7 @@ function Protocol.OpenAPI {
                 param()
             }
         }
+
         GitHubApi '/zen'
     .EXAMPLE
         # We can also use OpenAPI as a command.  Just pass a URL, and get back a script block.
@@ -38,6 +43,7 @@ function Protocol.OpenAPI {
     [Alias('OpenAPI','Swagger')]
     [ValidateScript({
         $commandAst = $_
+
         if ($commandAst -isnot [Management.Automation.Language.CommandAst]) { return $false }
         if ($commandAst.CommandElements.Count -eq 1) { return $false }
         # If neither command element contained a URI
@@ -56,67 +62,87 @@ function Protocol.OpenAPI {
     [Parameter(Mandatory,ParameterSetName='ScriptBlock')]
     [uri]
     $SchemaUri,
+
     # The Command's Abstract Syntax Tree.
     # This is provided by PipeScript when transpiling a protocol.
     [Parameter(Mandatory,ParameterSetName='Protocol')]
     [Management.Automation.Language.CommandAST]
     $CommandAst,
+
     # The ScriptBlock.
     # This is provided when transpiling the protocol as an attribute.
     # Providing a value here will run this script's contents, rather than a default implementation.
     [Parameter(Mandatory,ParameterSetName='ScriptBlock',ValueFromPipeline)]
     [scriptblock]
     $ScriptBlock = {},
+
     # One or more property prefixes to remove.
     # Properties that start with this prefix will become parameters without the prefix.
     [Alias('Remove Property Prefix')]
     [string[]]
     $RemovePropertyPrefix,
+
     # One or more properties to ignore.
     # Properties whose name or description is like this keyword will be ignored.
     [Alias('Ignore Property', 'IgnoreProperty','SkipProperty', 'Skip Property')]
     [string[]]
     $ExcludeProperty,
+
     # One or more properties to include.
     # Properties whose name or description is like this keyword will be included.
     [Alias('Include Property')]
     [string[]]
     $IncludeProperty,
+
     # The name of the parameter used for an access token.
     # By default, this will be 'AccessToken'
     [Alias('Access Token Parameter')]
     [string]
     $AccessTokenParameter,
+
     # The name of the parameter aliases used for an access token
     [Alias('Access Token Aliases')]
     [string[]]
     $AccessTokenAlias,
+
     # If set, will not mark a parameter as required, even if the schema indicates it should be.
     [Alias('NoMandatoryParameters','No Mandatory Parameters', 'NoMandatories', 'No Mandatories')]
     [switch]
     $NoMandatory,
+
     # If provided, will decorate any outputs from the REST api with this typename.
     # If this is not provied, the URL will be used as the typename.
     [Alias('Output Type Name', 'Decorate', 'PSTypeName', 'PsuedoTypeName','Psuedo Type', 'Psuedo Type Name')]
     [string]
     $OutputTypeName
     )
+
 begin {
     # First we declare a few filters we will reuse.
+
     # One resolves schema definitions.
     # The inputObject is the schema.
     # The arguments are the path within the schema.
+
     filter resolvePathDefinition {
+    
+    
             $in = $_.'paths'
+    
             $schemaPath = $args -replace '^#' -replace '^/' -replace '^\$defs' -split '[/\.]' -ne ''        
             foreach ($sp in $schemaPath) {
                 $in = $in.$sp
             }
             $in
         
+    
+    
     }
+
     # Another converts property names into schema parameter names.
     filter getSchemaParameterName {
+    
+    
             $parameterName = $_
             # If we had any prefixes we wished to remove, now is the time.
             if ($RemovePropertyPrefix) {            
@@ -133,8 +159,13 @@ begin {
             # And we should force the first letter to be uppercase.
             $parameterName.Substring(0,1).ToUpper() + $parameterName.Substring(1)
         
+    
+    
     }
+
     filter resolveSchemaDefinition {
+    
+    
             $in = $_
             $schemaPath = $args -replace '^#' -replace '^/' -split '[/\.]' -ne ''
             foreach ($sp in $schemaPath) {
@@ -142,8 +173,13 @@ begin {
             }
             $in
         
+    
+    
     }
+
     filter PropertiesToParameters {
+    
+    
             param(
                 [ValidateSet('Query','Body','Path')]
                 [string]
@@ -152,11 +188,13 @@ begin {
                 [string[]]
                 $RequiredParameter
             )
+    
             $inObject = $_
             if ($inObject -is [Collections.IDictionary]) {
                 $inObject = [PSCustomObject]$inObject
             }
             # Now walk thru each property in the schema
+    
             $newPipeScriptParameters = [Ordered]@{}    
             :nextProperty foreach ($property in $inObject.psobject.properties) {
                 # Filter out excluded properties
@@ -189,11 +227,13 @@ begin {
                 if ($property.value.description) {
                     $newParameterInfo.Description = $property.value.Description
                 }
+    
                 if (-not $NoMandatory -and 
                     (($property.value.required) -or ($RequiredParameter -contains $property.Name))
                 ) {
                     $newParameterInfo.Mandatory = $true
                 }
+    
                 $propertyTypeInfo =                 
                     if ($property.value.'$ref') {
                         # If there was a reference, try to resolve it
@@ -232,6 +272,7 @@ begin {
                             }
                         }
                     }
+    
                 # If there was a single type with an enum
                 if ($propertyTypeInfo.enum) {
                                         
@@ -265,12 +306,15 @@ begin {
                     'Path' {  "/$($property.Name)"  }
                     'Header' { "^$($property.Name)" }
                 }
+    
                 if ($property.Value.default) {
                     $newParameterInfo.Default = $property.Value.default
                 } elseif ($propertyTypeInfo.default) {
                     $newParameterInfo.Default = $propertyTypeInfo.default
                 }
+    
                 $newParameterInfo.Alias = $restfulAliasName 
+    
                 $parameterAttributes += "`$$parameterName"
                 
                 # $parameterList.insert(0, [Ordered]@{$parameterName=$newParameterInfo})
@@ -278,17 +322,23 @@ begin {
             }
             $newPipeScriptParameters
         
+    
+    
     }
+
     # If we have not cached the schema uris, create a collection for it.
     if (-not $script:CachedSchemaUris) {
         $script:CachedSchemaUris = @{}
     }
+
     $beginForEveryScript = {
         $pathVariable = [Regex]::new('(?<Start>\/)\{(?<Variable>\w+)\}', 'IgnoreCase,IgnorePatternWhitespace')
+
     
         # Next declare a script block that will replace the rest variable.
         $ReplacePathVariable = {
             param($match)
+
             if ($restPathParameters -and ($null -ne $restPathParameters[$match.Groups["Variable"].Value])) {
                 return $match.Groups["Start"].Value +
                     ([Web.HttpUtility]::UrlEncode(
@@ -299,6 +349,7 @@ begin {
             }
         }
     }
+
     # Last but not least, we declare a script block with the main contents of a single request
     $ProcessBlockStartForAnEndpoint = {
         # Declare a collection for each type of RESTful parameter.
@@ -307,18 +358,22 @@ begin {
         $restPathParameters   = [Ordered]@{}
         $restHeaderParameters = [Ordered]@{}
     }
+
     $processBlockStartForAllEndpoints = {
         $restBodyParameters   = $BodyParameter
         $restQueryParameters  = $QueryParameter
         $restPathParameters   = $PathParameter
         $restHeaderParameters = $HeaderParameter        
     }
+
     $processForEveryScript = {
         
+
         # Get my script block
         $myScriptBlock = $MyInvocation.MyCommand.ScriptBlock
         $function:MySelf = $myScriptBlock
         $myCmdInfo = $executionContext.SessionState.InvokeCommand.GetCommand('Myself','Function')        
+
         # Next, we'll use our own command's metadata to walk thru the parameters.
         
         foreach ($param in ([Management.Automation.CommandMetaData]$myCmdInfo).Parameters.GetEnumerator()) {                        
@@ -327,10 +382,12 @@ begin {
             # Everything else we need is in the value.
             $param = $param.Value
             $restParameterType = 'Body'
+
             # if there are not attributes, this parameter will not be mapped.
             if (-not $param.Attributes) {
                 continue
             }
+
             $specialAlias = @($param.Attributes.AliasNames) -match '^(?<t>[\./\?\^])(?<n>)'
             
             # Not walk over each parameter attribute:
@@ -347,10 +404,12 @@ begin {
                              '^' { 'Header' }
                          }
                 }
+
                 # If the attribute is not a defaultbindingproperty attribute,
                 if ($attr -isnot [ComponentModel.DefaultBindingPropertyAttribute]) {
                     continue # keep moving.
                 }
+
                 $bindParameterValue = $true
                 # Otherwise, update our object with the value
                 $propName = $($attr.Name)
@@ -362,6 +421,7 @@ begin {
                     default { $restBodyParameters }                    
                 }
             }
+
             # If our bound parameters contains the parameter
             if ($bindParameterValue -and $PSBoundParameters.ContainsKey($paramVariableName)) {
                 # use that value
@@ -382,11 +442,13 @@ begin {
                 }
             }
         }
+
         if ($PSBoundParameters['Path']) {
             # Replace any variables in the URI
             Write-Verbose "Adding $Path to $BaseUri"
             $uri = "${BaseUri}${Path}"
         }
+
         # Replace any variables in the URI
         Write-Verbose "Replacing REST Path Parameters in: $uri"
         $replacedUri = $pathVariable.Replace("$Uri", $ReplacePathVariable)
@@ -409,6 +471,7 @@ begin {
                 }
             ) -join '&')
         } else {''}
+
         # If any were added
         if ($queryString) {
             # append the query string
@@ -419,10 +482,12 @@ begin {
                 
         # Default the method to get, if one has not been provided
         if (-not $Method) { $Method = 'get' }
+
         # Create a cache for authorization headers, if one does not exist
         if (-not $script:CachedAuthorizationHeaders) {
             $script:CachedAuthorizationHeaders = @{}
         }
+
         if ($restHeaderParameters.Count) {
             foreach ($kv in @($restHeaderParameters.GetEnumerator())) {
                 if ($kv.Key -notlike '*:*') { continue }
@@ -436,10 +501,12 @@ begin {
                 $restHeaderParameters.Remove($kv.Key)                
             }
         }
+
         # If we have a cached authorization header and didn't provide one, use it.
         if ($script:CachedAuthorizationHeaders["$BaseUri"] -and -not $restHeaderParameters['Authorization']) {            
             $restHeaderParameters['Authorization'] = $script:CachedAuthorizationHeaders["$BaseUri"]
         }
+
         $invokeSplat = @{
             Uri = $replacedUri
             Method = $method
@@ -450,6 +517,7 @@ begin {
             $invokeSplat.ContentType = 'application/json'
             $invokeSplat.Body = $requestBody
         }
+
         if (-not $OutputTypeName) {
             $OutputTypeName = "$uri"
         }
@@ -468,9 +536,12 @@ begin {
             }
         }
     }
+
+
     $myCmd = $MyInvocation.MyCommand
     
 }
+
 process {
     # If we are being invoked as a protocol
     if ($PSCmdlet.ParameterSetName -eq 'Protocol') {
@@ -491,9 +562,11 @@ process {
             }
         }        
     }
+
     if (-not $SchemaUri.Scheme) {
         $SchemaUri = [uri]"https://$($SchemaUri.OriginalString -replace '://')"
     }
+
     # We will try to cache the schema URI at a given scope.
     $script:CachedSchemaUris[$SchemaUri] = $schemaObject = 
         if (-not $script:CachedSchemaUris[$SchemaUri]) {
@@ -506,11 +579,13 @@ process {
         $requirePSYaml = Invoke-PipeScript { require latest powershell-yaml }
         $schemaObject  = $schemaObject | ConvertFrom-Yaml -Ordered
     }
+
     # If we do not have a schema object, error out.
     if (-not $schemaObject) {
         Write-Error "Could not get Schema from '$schemaUri'"
         return
     }
+
     # If the object does not have .paths, it's not an OpenAPI schema, error out.
     if (-not $schemaObject.paths) {
         Write-Error "'$schemaUri' does not have .paths"
@@ -520,6 +595,7 @@ process {
     # Resolve the schema paths(s) we want to generate.
     $UrlRelativePath = ''
     $RestMethod      = 'get'
+
     $schemaDefinition = 
         if ($SchemaUri.Fragment) {
             $pathFragment = [Web.HttpUtility]::UrlDecode($SchemaUri.Fragment -replace '^\#')
@@ -552,18 +628,23 @@ process {
         } else {
             $null
         }
+
     
     
+
     # Start off by carrying over the summary.
     $newPipeScriptSplat = @{
         Synopsis = $schemaDefinition.summary
     }
+
     if ($schemaDefinition.properties -is [Collections.IDictionary]) {
         $schemaDefinition.properties = [PSCustomObject]$schemaDefinition.properties
     }
+
     $parametersFromProperties = $schemaDefinition.properties | PropertiesToParameters
     # Now walk thru each property in the schema
     $newPipeScriptParameters = [Ordered]@{} + $parametersFromProperties
+
     if ($schemaDefinition.parameters) {        
         foreach ($pathOrQueryParameter in $schemaDefinition.parameters) {
             if ($pathOrQueryParameter.'$ref') {
@@ -588,6 +669,7 @@ process {
         }
     }
     
+
     if ($schemaDefinition.requestBody) {
         $bodySchema = $schemaDefinition.requestBody.content.'application/json'.schema        
         $parametersFromBody = 
@@ -613,16 +695,20 @@ process {
             }
         }
     }
+
+
     # If we did not have a specific Schema Defined by the path.
     if (-not $SchemaDefinition) {
         if ($schemaObject.paths -is [Collections.IDictionary]) {
             $schemaObject.paths = [PSCustomObject]$schemaObject.paths
         }
+
         $validPaths = @(
             foreach ($pathProperty in $schemaObject.paths.psobject.properties) {
                 $pathProperty.name
             }
         )
+
         # we will make a generic command that can invoke any path.
         $newPipeScriptParameters['Path'] = [Ordered]@{
             Name = 'Path'
@@ -632,6 +718,7 @@ process {
             Type = [string[]]
             ValidValues = $validPaths
         }
+
         $newPipeScriptParameters['BodyParameter'] = [Ordered]@{
             Aliases = 'BodyParameters', 'Body'
             Help = 'Parameters passed in the body of the request'
@@ -639,6 +726,7 @@ process {
             Type = [Collections.IDictionary]
             DefaultValue = {[Ordered]@{}}
         }
+
         $newPipeScriptParameters['QueryParameter'] = [Ordered]@{
             Aliases = 'QueryParameters', 'Query'
             Help = 'Parameters passed in the query string of the request'
@@ -646,6 +734,7 @@ process {
             Type = [Collections.IDictionary]
             DefaultValue = {[Ordered]@{}}
         }
+
         $newPipeScriptParameters['PathParameter'] = [Ordered]@{
             Aliases = 'PathParameters'
             Help = 'Parameters passed in the path of the request'
@@ -653,6 +742,7 @@ process {
             Type = [Collections.IDictionary]
             DefaultValue = {[Ordered]@{}}
         }
+
         $newPipeScriptParameters['HeaderParameter'] = [Ordered]@{
             Aliases = 'HeaderParameters', 'Header','Headers'
             Help = 'Parameters passed in the headers of the request'
@@ -660,6 +750,7 @@ process {
             Type = [Collections.IDictionary]
             DefaultValue = {[Ordered]@{}}
         }                
+
         $newPipeScriptParameters['Method'] = [Ordered]@{
             Name = 'Method'
             Aliases = 'HTTPMethod'           
@@ -668,9 +759,11 @@ process {
             Type = [string]
         }
     }
+
     if (-not $AccessTokenParameter) {
         $AccessTokenParameter = 'AccessToken'
     }
+
     if (-not $newPipeScriptParameters[$AccessTokenParameter]) {
         if (-not $AccessTokenAlias) {
             $AccessTokenAlias = 'PersonalAccessToken', 'BearerToken', '^Bearer'
@@ -685,6 +778,7 @@ process {
         }
     }
     $newPipeScriptSplat.Parameter = $newPipeScriptParameters
+
     
     # If there was no scriptblock, or it was nothing but an empty param()
     if ($ScriptBlock -match '^[\s\r\n]{0,}(?:param\(\))?[\s\r\n]{0,}$') {
@@ -715,6 +809,7 @@ process {
         }
         
     }
+
     # If we are transpiling a script block
     if ($PSCmdlet.ParameterSetName -eq 'ScriptBlock') {
         # join the existing script with the schema information
@@ -732,5 +827,8 @@ $(New-PipeScript @newPipeScriptSplat)
         New-PipeScript @newPipeScriptSplat
     }    
 }
+
+
+
 }
 
