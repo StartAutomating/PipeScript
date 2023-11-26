4958d17a8856bfd76d3db27d6c12ffa00add9d4d
StartAutomating <StartAutomating@github.com>
2023-11-26 22:36:53 +0000

    fix: Update-PipeScript - Keeping repeated blanks ( Fixes #742 )

--- Commands/Languages/Arduino/Arduino-Language.ps1
+++ Commands/Languages/Arduino/Arduino-Language.ps1
@@ -5,10 +5,15 @@ function Language.Arduino {
         Arduino Language Definition
     .DESCRIPTION
         Defines Arduino within PipeScript.
+
         This allows Arduino to be templated.
+
         Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
+
         Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
+
         The C++ Inline Transpiler will consider the following syntax to be empty:
+
         * ```null```
         * ```""```
         * ```''```
@@ -19,7 +24,9 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition = New-Module {
     param()
+
     # Any Language can be parsed with a series of regular expresssions.
+
     $startComment = '/\*' # * Start Comments ```\*```
     $endComment   = '\*/' # * End Comments   ```/*```
     $Whitespace   = '[\s\n\r]{0,}'
