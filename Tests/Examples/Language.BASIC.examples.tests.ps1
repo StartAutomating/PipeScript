c189de62a35fe70f1b9af84e1c02bd9cd02621e6
StartAutomating <StartAutomating@github.com>
2023-11-28 03:37:21 +0000

    feat: Import-ModuleMember ( Fixes #743 )

--- Tests/Examples/Language.BASIC.examples.tests.ps1
+++ Tests/Examples/Language.BASIC.examples.tests.ps1
@@ -4,14 +4,18 @@ describe 'Language.BASIC' {
 Invoke-PipeScript {
     $VBScript = '    
 rem {
+
 rem # Uncommented lines between these two points will be ignored
+
 rem # Commented lines will become PipeScript / PowerShell.
 rem param($message = "hello world")
 rem "CreateObject(`"WScript.Shell`").Popup(`"$message`")" 
 rem }
 '
+
     [OutputFile('.\HelloWorld.ps1.vbs')]$VBScript
 }
+
 Invoke-PipeScript .\HelloWorld.ps1.vbs
     }
 }
