020e01598910f6598fda9200a4610f5c96489113
StartAutomating <StartAutomating@github.com>
2023-11-28 03:37:21 +0000

    feat: Import-ModuleMember ( Fixes #743 )

--- Tests/Examples/Language.ADA.examples.tests.ps1
+++ Tests/Examples/Language.ADA.examples.tests.ps1
@@ -4,10 +4,13 @@ describe 'Language.ADA' {
         Invoke-PipeScript {
             $AdaScript = '    
         with Ada.Text_IO;
+
         procedure Hello_World is
         begin
             -- {
+
             Uncommented lines between these two points will be ignored
+
             --  # Commented lines will become PipeScript / PowerShell.
             -- param($message = "hello world")        
             -- "Ada.Text_IO.Put_Line (`"$message`");"
@@ -17,6 +20,7 @@ describe 'Language.ADA' {
         
             [OutputFile('.\HelloWorld.ps1.adb')]$AdaScript
         }
+
         Invoke-PipeScript .\HelloWorld.ps1.adb
     }
 }
