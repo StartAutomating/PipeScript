572683ea17da62aa247e43165dd2ab8412253dd6
StartAutomating <StartAutomating@github.com>
2023-11-28 03:37:21 +0000

    feat: Import-ModuleMember ( Fixes #743 )

--- Tests/Examples/Language.Batch.examples.tests.ps1
+++ Tests/Examples/Language.Batch.examples.tests.ps1
@@ -4,14 +4,19 @@ describe 'Language.Batch' {
 Invoke-PipeScript {
     $batchScript = '    
 :: {
+
 :: # Uncommented lines between these two points will be ignored
+
 :: # Commented lines will become PipeScript / PowerShell.
 :: param($message = "hello world")
 :: "echo $message"
+
 :: }
 '
+
     [OutputFile('.\HelloWorld.ps1.cmd')]$batchScript
 }
+
 Invoke-PipeScript .\HelloWorld.ps1.cmd
     }
 }
