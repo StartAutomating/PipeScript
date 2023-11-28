dd56af392d2263a7b060921ed68c4c4405721872
StartAutomating <StartAutomating@github.com>
2023-11-28 03:37:22 +0000

    feat: Import-ModuleMember ( Fixes #743 )

--- Tests/Examples/Language.TCL.examples.tests.ps1
+++ Tests/Examples/Language.TCL.examples.tests.ps1
@@ -4,15 +4,19 @@ describe 'Language.TCL' {
     Invoke-PipeScript {
         $tclScript = '    
     # {
+
     # # Uncommented lines between these two points will be ignored
+
     #  # Commented lines will become PipeScript / PowerShell.
     # param($message = "hello world")
     # "puts `"$message`""
+
     # }
     '
     
         [OutputFile('.\HelloWorld.ps1.tcl')]$tclScript
     }
+
     Invoke-PipeScript .\HelloWorld.ps1.tcl
     }
 }
