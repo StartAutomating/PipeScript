d41216001628d38960fff096a5ffd347b80f932a
StartAutomating <StartAutomating@github.com>
2023-11-28 03:37:21 +0000

    feat: Import-ModuleMember ( Fixes #743 )

--- Tests/Examples/Language.Markdown.examples.tests.ps1
+++ Tests/Examples/Language.Markdown.examples.tests.ps1
@@ -4,18 +4,24 @@ describe 'Language.Markdown' {
     .> {
         $markdownContent = @'
 # Thinking of a Number Between 1 and 100: `|{Get-Random -Min 1 -Max 100}|` is the number
+
 ### abc
+
 ~~~PipeScript{
     '* ' + @("a", "b", "c" -join ([Environment]::Newline + '* '))
 }
 ~~~
+
 #### Guess what, other code blocks are unaffected
 ~~~PowerShell
 1 + 1 -eq 2
 ~~~
+
+
 '@
         [OutputFile('.\HelloWorld.ps1.md')]$markdownContent
     }
+
     .> .\HelloWorld.ps1.md
     }
 }
