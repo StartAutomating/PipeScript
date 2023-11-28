03bcf0f80a9f59ca2acd1e71208facfbd7369d68
StartAutomating <StartAutomating@github.com>
2023-11-28 03:37:22 +0000

    feat: Import-ModuleMember ( Fixes #743 )

--- Tests/Examples/Language.R.examples.tests.ps1
+++ Tests/Examples/Language.R.examples.tests.ps1
@@ -4,15 +4,19 @@ describe 'Language.R' {
     Invoke-PipeScript {
         $rScript = '    
     # {
+
     Uncommented lines between these two points will be ignored
+
     #  # Commented lines will become PipeScript / PowerShell.
     # param($message = "hello world")
     # "print(`"$message`")"
+
     # }
     '
     
         [OutputFile('.\HelloWorld.ps1.r')]$rScript
     }
+
     Invoke-PipeScript .\HelloWorld.ps1.r
     }
 }
