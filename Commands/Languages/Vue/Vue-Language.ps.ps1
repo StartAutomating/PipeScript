467f6beb5f7a14176f0335011452b3000c382379
James Brundage <@github.com>
2023-11-29 13:59:11 -0800

    feat: Vue Language Support ( Fixes #513 )

---
+++ Commands/Languages/Vue/Vue-Language.ps.ps1
@@ -0,0 +1,26 @@
+Language function Vue {
+<#
+.SYNOPSIS
+    Vue PipeScript Language Definition.
+.DESCRIPTION
+    Allows PipeScript to generate Vue files.
+
+    Multiline comments blocks like this ```<!--{}-->``` will be treated as blocks of PipeScript.
+
+    JavaScript/CSS comment blocks like ```/*{}*/``` will also be treated as blocks of PipeScript.
+#>
+[ValidatePattern('\.vue$')]
+param(
+)
+    $FilePattern = '\.vue$'
+
+    # We start off by declaring a number of regular expressions:
+    $startComment = '(?><\!--|/\*)' # * Start Comments ```<!--```
+    $endComment   = '(?>-->|\*/)'   # * End Comments   ```-->```
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * StartRegex     ```$StartComment + '{' + $Whitespace```
+    $StartPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+    # * EndRegex       ```$whitespace + '}' + $EndComment```
+    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+
+}
