48b28da255f311b227bc576f751a081e26994aec
StartAutomating <StartAutomating@github.com>
2023-11-29 07:39:36 +0000

    feat: Defining PSD1 Interpreter ( Fixes #757, Fixes #758, Fixes #759 )
    and adding FilePattern to a few languages

--- Commands/Languages/PowerShell/PowerShellData-Language.ps1
+++ Commands/Languages/PowerShell/PowerShellData-Language.ps1
@@ -2,7 +2,7 @@
 function Language.PowerShellData {
 <#
 .SYNOPSIS
-    PSD1 Template Transpiler.
+    PSD1 PipeScript Language Definition.
 .DESCRIPTION
     Allows PipeScript to generate PSD1.
 
@@ -20,21 +20,33 @@ if (-not $this.Self) {
 $languageDefinition = New-Module {
     param()
 
+$FilePattern = '\.psd1$'
 
 # We start off by declaring a number of regular expressions:
 $startComment = '<\#' # * Start Comments ```\*```
 $endComment   = '\#>' # * End Comments   ```/*```
 $Whitespace   = '[\s\n\r]{0,}'
 # * IgnoredContext (single-quoted strings)
-$IgnoredContext = "
-(?<ignore>
+$IgnoredContext = [Regex]::New("
+(?<Ignore>
     (?>'((?:''|[^'])*)')
     [\s - [ \r\n ] ]{0,}
-){0,1}"
+){0,1}",'IgnoreCase,IgnorePatternWhitespace')
 # * StartPattern     ```$IgnoredContext + $StartComment + '{' + $Whitespace```
 $StartPattern = [regex]::New("(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)", 'IgnorePatternWhitespace')
 # * EndPattern       ```$whitespace + '}' + $EndComment + $ignoredContext```
-$endPattern   = "(?<PSEnd>$Whitespace\}${endComment}[\s-[\r\n]]{0,}${IgnoredContext})"
+$endPattern   = [regex]::New("(?<PSEnd>$Whitespace\}${endComment}[\s-[\r\n]]{0,}${IgnoredContext})", 'IgnorePatternWhitespace')
+
+$IsDataLanguage = $true
+
+$Interpreter = {
+    param()
+    $Psd1Path, $OtherArgs = $args
+    if (Test-Path $Psd1Path) {
+        Import-LocalizedData -BaseDirectory ($Psd1Path | Split-Path) -FileName ($Psd1Path | Split-Path -Leaf)
+    }
+    
+}
     $LanguageName = 'PowerShellData'
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
