240f0c862269f01ceed12a752b714ea6697709e8
StartAutomating <StartAutomating@github.com>
2023-11-29 08:17:57 +0000

    feat: Interpreters for Lua, PHP, R, Perl ( Fixes #757 )
    and adding FilePattern to a few languages

--- Commands/Languages/Lua/Lua-Language.ps1
+++ Commands/Languages/Lua/Lua-Language.ps1
@@ -2,18 +2,20 @@
 function Language.Lua {
 <#
     .SYNOPSIS
-        LUA Template Transpiler.
+        LUA PipeScript Language Definition.
     .DESCRIPTION
         Allows PipeScript to generate LUA.
 
         Multiline comments like ```--{[[```  ```--}]]``` will be treated as blocks of PipeScript.    
     #>
-[ValidatePattern('\.lua$')]
+
 param()
 $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition = New-Module {
     param()
+
+    $FilePattern  = '\.lua$'
 
     # We start off by declaring a number of regular expressions:
     $startComment = '\-\-\[\[\{' # * Start Comments ```--[[{```
@@ -21,6 +23,28 @@ $languageDefinition = New-Module {
     $Whitespace   = '[\s\n\r]{0,}'        
     $StartPattern = "(?<PSStart>${startComment})"    
     $EndPattern   = "(?<PSEnd>${endComment}\s{0,})"
+
+    # We might have to go looking for R's interpreter
+    $interpreter = $(
+        # (it may just be in the path)
+        if ($ExecutionContext.SessionState.InvokeCommand.GetCommand('lua', 'Application')) {
+            $ExecutionContext.SessionState.InvokeCommand.GetCommand('lua', 'Application')
+        } elseif (
+            # Or, if we're on Windows and there's a LUA %ProgramFiles(x86)% directory
+            $IsWindows -and (Test-Path (Join-Path ${env:ProgramFiles(x86)} 'Lua'))
+        ) {
+            
+            # We can look in there 
+            $ExecutionContext.SessionState.InvokeCommand.GetCommand("$(
+                Join-Path "${env:ProgramFiles(x86)}" 'Lua' | 
+                Get-ChildItem -Directory | 
+                # for the most recent version of R
+                Sort-Object { $_.Name -as [Version]} -Descending  |  
+                Select-Object  -First 1 |                 
+                Join-Path -ChildPath "lua.exe"
+            )", "Application")
+        }
+    )
     $LanguageName = 'Lua'
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
