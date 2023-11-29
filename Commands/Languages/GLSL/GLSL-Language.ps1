bf304036b5989b80e1cdb6574dd67fa0330e271b
StartAutomating <StartAutomating@github.com>
2023-11-29 21:57:56 +0000

    feat: GLSL and Effect File Support ( Fixes #497 )

---
+++ Commands/Languages/GLSL/GLSL-Language.ps1
@@ -0,0 +1,39 @@
+
+function Language.GLSL {
+<#
+    .SYNOPSIS
+        GLSL PipeScript Language Definition.
+    .DESCRIPTION
+        Allows PipeScript to generate GLSL (OpenGL Shader Language).
+
+        Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
+    #>
+[ValidatePattern('\.glsl$')]
+param()
+$this = $myInvocation.MyCommand
+if (-not $this.Self) {
+$languageDefinition = New-Module {
+    param(
+    )
+
+    $FilePattern = '\.glsl$'
+    
+    # We start off by declaring a number of regular expressions:
+    $startComment = '/\*' # * Start Comments ```\*```
+    $endComment   = '\*/' # * End Comments   ```/*```
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * StartRegex     ```$StartComment + '{' + $Whitespace```
+    $StartPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+    # * EndRegex       ```$whitespace + '}' + $EndComment```
+    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+    $LanguageName = 'GLSL'
+    Export-ModuleMember -Variable * -Function * -Alias *
+} -AsCustomObject
+$languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language")
+$languageDefinition.pstypenames.add("Language.GLSL")
+$this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
+}
+$this.Self
+}
+
