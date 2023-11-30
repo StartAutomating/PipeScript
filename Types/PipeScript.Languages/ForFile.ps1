c0d0417b0148551b1e4a8ead276c2c4561b4fec4
James Brundage <@github.com>
2023-11-29 19:44:38 -0800

    feat: PipeScript.Languages.ForFile ( Fixes #772 )
    Only looking at languages with a file pattern

--- Types/PipeScript.Languages/ForFile.ps1
+++ Types/PipeScript.Languages/ForFile.ps1
@@ -5,6 +5,8 @@
     Gets the PipeScript language definitions for a file path.
 .EXAMPLE
     $PSLanguage.ForFile("a.xml")
+.EXAMPLE
+    $PSInterpreters.ForFile("a.js")
 #>
 param(
 [string]
@@ -16,6 +18,7 @@ foreach ($prop in $this.psobject.properties) {
     if ($prop -is [psscriptproperty]) { continue }
     if ($prop.IsInstance -and 
         $prop.Value.LanguageName -and
+        $prop.Value.FilePattern -and
         $filePath -match $prop.Value.FilePattern) {
         $prop.Value
     }
