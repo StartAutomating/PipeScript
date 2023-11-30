b09982062e812c397bb1e29df7e36dd7a9e90b2f
James Brundage <@github.com>
2023-11-29 16:25:47 -0800

    feat: Object keyword ( Fixes #769 )
    Returning [object] with one argument, allowing a variable argument.

--- Transpilers/Keywords/Object.psx.ps1
+++ Transpilers/Keywords/Object.psx.ps1
@@ -2,11 +2,13 @@
 .SYNOPSIS
     Object Keyword
 .DESCRIPTION
-    The Object Keyword helps you create objects
+    The Object Keyword helps you create objects or get the .NET type, `object`.
 .EXAMPLE
     Use-PipeScript { object { $x = 1; $y = 2 }} 
 .EXAMPLE
     Use-PipeScript { object @{ x = 1; y = 2 }}
+.EXAMPLE
+    Use-PipeScript { Object }
 #>
 [ValidateScript({
     $validating = $_
@@ -15,7 +17,7 @@
     if ($validating -isnot [Management.Automation.Language.CommandAst]) { return $false }
     
     # that have exactly two command elements.
-    if ($validating.CommandElements.Count -ne 2) { return $false }
+    if ($validating.CommandElements.Count -gt 2) { return $false }
 
     # The first element must be "Object"
     if ($validating.CommandElements[0].Value -ne 'Object') { return $false }
@@ -24,14 +26,23 @@
 })]
 [Reflection.AssemblyMetadata("PipeScript.Keyword",$true)]
 param(
+# The Command Ast for the Object Keyword
 [Parameter(ValueFromPipeline)]
 [Management.Automation.Language.CommandAst]
 $ObjectCommandAst
 )
 
 process {
+    # If we're not "object", return.
     if ($ObjectCommandAst.CommandElements[0].Value -ne 'Object') { return }
-    if ($ObjectCommandAst.CommandElements.Count -ne 2) { return }
+    # If we have less than two command elements, return.
+    if ($ObjectCommandAst.CommandElements.Count -gt 2) { return }
+    # If we have only one command element
+    if ($ObjectCommandAst.CommandElements.Count -eq 1) {
+        # Create a script that returns [Object]
+        return ([scriptblock]::create("[Object]"))
+    }
+
     switch ($ObjectCommandAst.CommandElements[1]) {
         { $_ -is [Management.Automation.Language.ScriptBlockExpressionAst]} {            
             [ScriptBlock]::Create("New-Module -AsCustomObject $_")
@@ -40,10 +51,7 @@ process {
             [ScriptBlock]::Create("[PSCustomObject][Ordered]$_")
         }
         { $_ -is [Management.Automation.Language.VariableExpressionAst]} {
-            [ScriptBlock]::Create("`$(if ($_ -is [Collections.IDictionary]) { [PSCustomObject][Ordered]@{} + $_ } else { $_ })")
-        }
-        default {
-
-        }
+            [ScriptBlock]::Create("`$(if ($_ -is [Collections.IDictionary]) { [PSCustomObject][Ordered]@{} + $_ } elseif ($_ -is [ScriptBlock]) { New-Module -AsCustomObject $_ } else { $_ })")
+        }        
     }
 }
