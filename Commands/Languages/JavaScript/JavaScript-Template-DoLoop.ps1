5dca567d931022413c0fbc5b1c26edc7c331a784
StartAutomating <StartAutomating@github.com>
2023-12-06 23:49:46 +0000

    feat: Template.RegexLiteral.js ( Fixes #813, Fixes #815 )
    Also fixing aliases in templates and marking JavaScript as CaseSensitive

--- Commands/Languages/JavaScript/JavaScript-Template-DoLoop.ps1
+++ Commands/Languages/JavaScript/JavaScript-Template-DoLoop.ps1
@@ -8,8 +8,7 @@ function Template.DoLoop.js {
         Template for a `do` loop in JavaScript.
     .EXAMPLE
         Template.DoLoop.js -Condition "false" -Body "console.log('This happens once')"
-    #>
-    [Alias('Template.ForInLoop.js')]
+    #>    
     param(    
     # The Loop's Condition.
     # This determines if the loop should continue running.
