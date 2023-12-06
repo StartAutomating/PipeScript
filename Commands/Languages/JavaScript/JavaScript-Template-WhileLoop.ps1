ec8002806eab4b91da2053cf9554dc69af29918f
StartAutomating <StartAutomating@github.com>
2023-12-06 23:25:40 +0000

    feat: Template.WhileLoop.js ( Fixes #811 )

---
+++ Commands/Languages/JavaScript/JavaScript-Template-WhileLoop.ps1
@@ -0,0 +1,44 @@
+
+function Template.WhileLoop.js {
+
+    <#
+    .SYNOPSIS
+        Template for a JavaScript `while` Loop
+    .DESCRIPTION
+        Template for a `while` loop in JavaScript.
+    .EXAMPLE
+        Template.WhileLoop.js -Condition "false" -Body "console.log('This never happens')"
+    #>
+    [Alias('Template.ForInLoop.js')]
+    param(    
+    # The Loop's Condition.
+    # This determines if the loop should continue running.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]
+    $Condition,
+    
+    # The body of the loop
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]
+    $Body
+    )
+
+    process {
+        if ($body -match '^\{') {
+            $body = $body -replace '^\s{0,}\{' -replace '\}\s{0,}$'
+        }        
+        if ($Condition -match '^\$') {
+            $Condition = $Condition -replace '^\$'
+        }        
+        @"
+while ($Condition) {
+    $Body
+} 
+"@
+    }
+
+}
+
+
+
+
