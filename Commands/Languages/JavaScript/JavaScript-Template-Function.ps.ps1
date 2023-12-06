00af126281de45bdb2a886d0312903fa8176583a
James Brundage <@github.com>
2023-12-06 15:28:42 -0800

    feat: Template.Function.js ( Fixes #812 )

---
+++ Commands/Languages/JavaScript/JavaScript-Template-Function.ps.ps1
@@ -0,0 +1,40 @@
+Template function Function.js {
+    <#
+    .SYNOPSIS
+        Template for a JavaScript `function`
+    .DESCRIPTION
+        Template for a `function` in JavaScript.
+    .EXAMPLE
+        Template.Function.js -Name "Hello" -Body "return 'hello'"
+    #>
+    [Alias('Template.ForInLoop.js')]
+    param(    
+    # The name of the function.
+    [vbn()]
+    [string]
+    $Name,
+
+    [vbn()]
+    [Alias('Arguments','Parameter','Parameters')]
+    [string[]]
+    $Argument,
+    
+    # The body of the function.
+    [vbn()]
+    [string[]]
+    $Body
+    )
+
+    process {
+        if ($body -match '^\{') {
+            $body = $body -replace '^\s{0,}\{' -replace '\}\s{0,}$'
+        }                        
+        @"
+function $(if ($name) { $name})($argument) {
+    $($Body -join (';' + [Environment]::newLine + '    '))
+} 
+"@
+    }
+}
+
+
