880cc3c4b105f59fcff896e3f1cceecf64d82693
James Brundage <@github.com>
2023-12-06 18:09:08 -0800

    feat: F# Language Definition ( Fixes #781 )

---
+++ Commands/Languages/FSharp/FSharp-Language.ps.ps1
@@ -0,0 +1,25 @@
+Language function FSharp {
+    <#
+    .SYNOPSIS
+        FSharp PipeScript Language Definition
+    .DESCRIPTION
+        Allows PipeScript to Generate FSharp        
+    #>
+    param()
+
+    # FSharp Files are named `.fs`,`.fsi`,`.fsx`, or `.fsscript`.
+    $FilePattern = '\.fs(?>i|x|script|)$'
+
+    # FSharp Block Comments Start with `(*`
+    $startComment = '\(\*'
+    # FSharp Block Comments End with  `*)'
+    $endComment   = '\*\)'
+    
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * StartRegex     ```$StartComment + '{' + $Whitespace```
+    $StartPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+    # * EndRegex       ```$whitespace + '}' + $EndComment```
+    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+
+    $CaseSensitive = $true
+}
