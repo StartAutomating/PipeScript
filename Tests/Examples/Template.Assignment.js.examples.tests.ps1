e9df79f3c073244495be65eba9c44f712d756590
StartAutomating <StartAutomating@github.com>
2023-12-07 01:49:48 +0000

    feat: Template.Class.js ( Fixes #816 )

---
+++ Tests/Examples/Template.Assignment.js.examples.tests.ps1
@@ -0,0 +1,10 @@
+
+describe 'Template.Assignment.js' {
+    it 'Template.Assignment.js Example 1' {
+        Template.Assignment.js -Left "a" -Right 1
+    }
+    it 'Template.Assignment.js Example 2' {
+        Template.Const.js -VariableName "MyConstant" -Expression 42
+    }
+}
+
