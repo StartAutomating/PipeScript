dc56513b0646e617e18761bc0b7a7b2726b9de39
James Brundage <@github.com>
2023-12-06 17:44:22 -0800

    feat: Template.Class.js ( Fixes #816 )

---
+++ Commands/Languages/JavaScript/JavaScript-Template-Class.ps.ps1
@@ -0,0 +1,41 @@
+Template function Class.js {
+    <#
+    .SYNOPSIS
+        Template for a JavaScript `class`
+    .DESCRIPTION
+        Template for a `class` in JavaScript.
+    .EXAMPLE
+        Template.Class.js -Name "MyClass" -Body "MyMethod() { return 'hello'}"
+    #>
+    param(    
+    # The name of the function.
+    [vbn()]
+    [string]
+    $Name,
+    
+    # The body of the function.
+    [vbn()]
+    [Alias('Member','Members')]
+    [string[]]
+    $Body,
+
+    # If provided, will extend from a base class.
+    [Alias('Extends')]
+    [string]
+    $Extend
+    )
+
+    process {
+        if ($body -match '^\{') {
+            $body = $body -replace '^\s{0,}\{' -replace '\}\s{0,}$'
+        }
+        
+@"
+class $name$(if ($Extend) { " extends $extend"}) {
+    $($Body -join (';' + [Environment]::newLine + '    '))
+}
+"@        
+    }
+}
+
+
