f94bd9258f59dd5e97324f542114bd8d02243dd6
James Brundage <@github.com>
2023-12-06 16:33:19 -0800

    feat: Template.Method.js ( Fixes #817 )

---
+++ Commands/Languages/JavaScript/JavaScript-Template-Method.ps.ps1
@@ -0,0 +1,40 @@
+Template function Method.js {
+    <#
+    .SYNOPSIS
+        Template for a JavaScript method
+    .DESCRIPTION
+        Template for a method in JavaScript.
+    .EXAMPLE
+        Template.Method.js -Name "then" -Argument "(result)=>doSomethingElse(result)"
+    .EXAMPLE
+        "doSomething()" |Template.Method.js -Name "then" -Argument "(result)=>doSomethingElse(result)"
+    #>    
+    param(        
+    # The name of the method.
+    [vbn()]
+    [string]
+    $Name,
+    
+    # The input object (this allows piping this to become chaining methods)
+    [vfp()]
+    [PSObject]
+    $InputObject,
+
+    # The arguments to the method
+    [vbn()]
+    [Alias('Arguments','Parameter','Parameters')]
+    [string[]]
+    $Argument,
+    
+    
+    # If set, the method return will be awaited (this will only work in an async function)
+    [switch]
+    $Await
+    )
+
+    process {                                
+        "$(if ($await) { "await"}) $(if ($inputObject -is [string]) { $InputObject })$(if ($name) { $name -replace "^\.{0,1}", "." })($($argument -join ','))"
+    }
+}
+
+
