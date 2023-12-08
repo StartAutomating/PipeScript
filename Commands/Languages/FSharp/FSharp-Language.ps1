3797bd0ab2d6e48e8a59be2412d2dd0668602d1a
StartAutomating <StartAutomating@github.com>
2023-12-08 06:30:43 +0000

    feat: Wren Language Definition ( Fixes #820 )

---
+++ Commands/Languages/FSharp/FSharp-Language.ps1
@@ -0,0 +1,42 @@
+
+function Language.FSharp {
+<#
+    .SYNOPSIS
+        FSharp PipeScript Language Definition
+    .DESCRIPTION
+        Allows PipeScript to Generate FSharp        
+    #>
+
+param()
+$this = $myInvocation.MyCommand
+if (-not $this.Self) {
+$languageDefinition = New-Module {
+    param()
+
+    # FSharp Files are named `.fs`,`.fsi`,`.fsx`, or `.fsscript`.
+    $FilePattern = '\.fs(?>i|x|script|)$'
+
+    # FSharp Block Comments Start with `(*`
+    $startComment = '\(\*'
+    # FSharp Block Comments End with  `*)'
+    $endComment   = '\*\)'
+    
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * StartRegex     ```$StartComment + '{' + $Whitespace```
+    $StartPattern = "(?<PSStart>${startComment}\{$Whitespace)"
+    # * EndRegex       ```$whitespace + '}' + $EndComment```
+    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,})"
+
+    $CaseSensitive = $true
+    $LanguageName = 'FSharp'
+    Export-ModuleMember -Variable * -Function * -Alias *
+} -AsCustomObject
+$languageDefinition.pstypenames.clear()
+$languageDefinition.pstypenames.add("Language")
+$languageDefinition.pstypenames.add("Language.FSharp")
+$this.psobject.properties.add([PSNoteProperty]::new('Self',$languageDefinition))
+}
+$this.Self
+}
+
+
