48045522a6b0404e66b9e5fa1dea880b71993227
James Brundage <@github.com>
2023-12-08 17:46:42 -0800

    fix: TypeConstraintAst - Documenting .ArgumentList and .Parameter ( Fixes #574 )

--- Types/TypeConstraintAST/get_ArgumentList.ps1
+++ Types/TypeConstraintAST/get_ArgumentList.ps1
@@ -1,3 +1,15 @@
+<#
+.SYNOPSIS
+    Gets arguments of a type constraint
+.DESCRIPTION
+    Gets the arguments from a type constraint.
+
+    This will treat any generic type specifiers as potential parameters, and other type specifiers as arguments.
+.EXAMPLE
+    {
+        [a[b[c],c]]'d'
+    }.Ast.EndBlock.Statements[0].PipelineElements[0].Expression.Type.ArgumentList
+#>
 if (-not $this.TypeName.IsGeneric) { return @() }
 @(foreach ($typeName in $this.TypeName.GenericArguments ) {
     if ($TypeName.IsGeneric) { continue }
