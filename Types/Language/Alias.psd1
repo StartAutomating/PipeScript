8aaae242bf7860de4f2e4da5c075520a09bc7125
James Brundage <@github.com>
2023-12-09 16:23:39 -0800

    feat: Language.Function(s) ( Fixes #829 )

--- Types/Language/Alias.psd1
+++ Types/Language/Alias.psd1
@@ -1,3 +1,4 @@
 @{
     Templates = 'Template'
+    Functions = 'Function'
 }
---
+++ Types/Language/get_Function.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS
+    Gets Language Functions
+.DESCRIPTION
+    Gets Functions related to a language.
+
+    These are functions that either match a language's `.FilePattern` or start with a language name, followed by punctuation.
+#>
+if (-not $global:AllFunctionsAndAliases) {
+    $global:AllFunctionsAndAliases = $global:ExecutionContext.SessionState.InvokeCommand.GetCommand('*','Alias,Function',$true)
+}
+$FunctionsForThisLanguage = [Ordered]@{}
+if ($this.FilePattern) {    
+    foreach ($FunctionForLanguage in $global:AllFunctionsAndAliases -match $this.FilePattern) {
+        $FunctionsForThisLanguage["$FunctionForLanguage"] = $FunctionForLanguage
+    }    
+}
+if ($this.LanguageName) {
+    foreach ($FunctionForLanguage in $global:AllFunctionsAndAliases -match "^$([Regex]::Escape($this.LanguageName))\p{P}") {
+        $FunctionsForThisLanguage["$FunctionForLanguage"] = $FunctionForLanguage
+    }
+}
+$FunctionsForThisLanguage
+
