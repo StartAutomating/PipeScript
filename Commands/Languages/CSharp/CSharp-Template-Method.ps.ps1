b13a0a4783eec9933e46d64fb725161ae171a881
James Brundage <@github.com>
2023-12-09 19:07:03 -0800

    feat: CSharp Method Template ( Fixes #823 )

---
+++ Commands/Languages/CSharp/CSharp-Template-Method.ps.ps1
@@ -0,0 +1,46 @@
+Template function Method.cs {
+    <#
+    .SYNOPSIS
+        Template for CSharp Method
+    .DESCRIPTION
+        A Template for a CSharp Method Definition
+    #>
+    param(
+    # The method name.
+    [vbn()]
+    [Alias('Identifier','Name','MethodName')]
+    [string]
+    $Method,
+    
+    # The method modifiers.  Creates public methods by default.
+    [vbn()]
+    [Alias('Modifiers')]
+    [string[]]
+    $Modifier = 'public',    
+
+    # The return type.  By default, void.
+    [vbn()]    
+    [string]
+    $ReturnType = 'void',
+
+    # One or more method attributes
+    [vbn()]
+    [Alias('Attributes','MethodAttribute','MethodAttributes')]
+    [string[]]
+    $Attribute,
+
+    # The body of the method.
+    [vbn()]
+    [Alias('Members','Member')]
+    [string[]]
+    $Body
+    )
+        
+    process {
+@"
+$(if ($Attribute) {($Attribute -join [Environment]::NewLine) + [Environment]::NewLine})$($Modifier -join ' ') $(if ($ReturnType) { "$returnType "}) $method {
+$($body -join [Environment]::NewLine)
+}
+"@
+    }    
+}
