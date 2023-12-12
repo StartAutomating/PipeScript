56e65da866d1db4e2a8e8ba9fe09114a6969bfd0
James Brundage <@github.com>
2023-12-11 19:24:33 -0800

    feat: CommandInfo Strong Piping ( Fixes #844, Fixes #842 )
    Expanding weak type list

--- Types/CommandInfo/get_IsStronglyPiped.ps1
+++ Types/CommandInfo/get_IsStronglyPiped.ps1
@@ -4,11 +4,12 @@
 .DESCRIPTION
     Determines if a command uses strong typing on at least one ValueFromPipeline parameter.
 #>
+$weakTypeList = [string],[object],[psobject], [string[]],[object[]],[psobject[]]
 foreach ($parameterSet in $this.ParameterSets) {
     foreach ($parameterInSet in $parameterSet.Parameters) {
         if (-not $parameterInSet.ValueFromPipeline) { continue }
         if ((-not $parameterInSet.ParameterType.IsPrimitive) -and  
-            $parameterInSet.ParameterType -notin [string],[object],[psobject]) {
+            $parameterInSet.ParameterType -notin $weakTypeList) {
             return $true
         }        
     }
--- Types/CommandInfo/get_StrongPipeType.ps1
+++ Types/CommandInfo/get_StrongPipeType.ps1
@@ -4,11 +4,12 @@
 .DESCRIPTION
     Gets the strong types that a given command can accept as a ValueFromPipeline parameter.
 #>
+$weakTypeList = [string],[object],[psobject], [string[]],[object[]],[psobject[]]
 , @(foreach ($parameterSet in $this.ParameterSets) {
     foreach ($parameterInSet in $parameterSet.Parameters) {
         if (-not $parameterInSet.ValueFromPipeline) { continue }
         if ((-not $parameterInSet.ParameterType.IsPrimitive) -and  
-            $parameterInSet.ParameterType -notin [string],[object],[psobject]) {
+            $parameterInSet.ParameterType -notin $weakTypeList) {
             $parameterInSet.ParameterType
         }        
     }
