575eff2bfc2bfed8cf50f80dda22cf075c2c84ea
James Brundage <@github.com>
2023-12-17 13:59:28 -0800

    feat: PipeScript.Languages/Interpreters.All ( Fixes #838 )

--- Types/PipeScript.Languages/get_All.ps1
+++ Types/PipeScript.Languages/get_All.ps1
@@ -5,8 +5,7 @@
     Gets all currently loaded language definitions in PipeScript.
 #>
 ,@(foreach ($psProperty in $this.PSObject.properties) {
-    if ($psProperty -isnot [psnoteproperty]) { continue  }    
-    if ($psProperty.Value -isnot [PSObject]) { continue }
+    if ($psProperty -isnot [psnoteproperty]) { continue  }
     if ($prop.IsInstance -and $prop.Value.LanguageName) {
         $psProperty.Value
     }    
