0ee565ee2dc4cd6e96c8a67c40af4de196a1152e
James Brundage <@github.com>
2023-12-17 13:01:02 -0800

    chore: CSharp Hello World ( Fixes #846 )

---
+++ Languages/CSharp/CSharp-Template-HelloWorld.ps.ps1
@@ -0,0 +1,29 @@
+Template function HelloWorld.cs {
+    <#
+    .SYNOPSIS
+        Hello World in CSharp
+    .DESCRIPTION
+        A Template for Hello World, in CSharp.    
+    #>
+    param(
+    # The message to print.  By default, "hello world".
+    [vbn()]
+    [string]
+    $Message = "hello world"
+    )
+    process {
+@"
+    public static class Program {
+        public static void Main(string[] args) {
+            string helloMessage = @"
+$($Message.Replace('"','""'))
+";
+            if (args.Length > 0) {
+                helloMessage = String.Join(" ", args);
+            }
+            System.Console.WriteLine(helloMessage);
+        }
+    }
+"@
+    }
+}
