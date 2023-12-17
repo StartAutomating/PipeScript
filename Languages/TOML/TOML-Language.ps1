bdc1714c607af7a89093736548006ca6c0944e08
StartAutomating <StartAutomating@github.com>
2023-12-17 21:10:08 +0000

    chore: CSharp Hello World ( Fixes #846 )

--- Languages/TOML/TOML-Language.ps1
+++ Languages/TOML/TOML-Language.ps1
@@ -26,17 +26,21 @@ $this = $myInvocation.MyCommand
 if (-not $this.Self) {
 $languageDefinition = New-Module {
     param()
+    # TOML files end in `.toml`
     $FilePattern = '\.toml$'
+
     # We start off by declaring a number of regular expressions:
+    # TOML doesn't have comments per se, but it has literal string blocks `"""`
+    $startComment = '(?>""")'
+    $endComment   = '(?>""")'
     
-    $startComment = '(?>"""\{)'
-    $endComment   = '(?>\}""")'
-    
-    $startPattern = "(?<PSStart>${startComment})"    
-    $endPattern   = "(?<PSEnd>${endComment})"
+    $startPattern = "(?<PSStart>${startComment}\{\s{0,})"    
+    $endPattern   = "(?<PSEnd>\s{0,}\}${endComment})"
 
     # TOML is a DataLanguage
-    $IsDataLanguage = $true
+    $DataLanguage = $true
+    # and it is always case-sensitive.
+    $CaseSensitive = $true
     $LanguageName = 'TOML'
     Export-ModuleMember -Variable * -Function * -Alias *
 } -AsCustomObject
