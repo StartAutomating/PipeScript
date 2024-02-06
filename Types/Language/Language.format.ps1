8411b9442d972c4c833bbf5e08d716ace6597775
James Brundage <@github.com>
2024-02-06 15:56:36 -0800

    feat: Language YAML Custom Formatter ( Fixes #902 )
    Only using note properties

--- Types/Language/Language.format.ps1
+++ Types/Language/Language.format.ps1
@@ -1,5 +1,11 @@
 Write-FormatView -TypeName Language -Property LanguageName, FilePattern
 
 Write-FormatView -TypeName Language -Action {
-    $_ | Format-YAML
+    $NoteProperties = [Ordered]@{}
+    foreach ($prop in $_.psobject.properties) {
+        if ($prop -is [psnoteproperty]) {
+            $NoteProperties[$prop.Name] = $prop.Value
+        }
+    }
+    $NoteProperties | Format-YAML
 }
