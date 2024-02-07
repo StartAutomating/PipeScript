6e65b8aaf8be275a6e7ef78cec361f5fc5304252
James Brundage <@github.com>
2024-02-07 11:06:42 -0800

    feat: Get-Interpreter ( Fixes #747 )

---
+++ Commands/Interpreters/Get-Interpreter.ps.ps1
@@ -0,0 +1,69 @@
+function Get-Interpreter 
+{
+    <#
+    .SYNOPSIS
+        Gets Interpreters
+    .DESCRIPTION
+        Gets PipeScript Interpreters        
+    .EXAMPLE
+        Get-Interpreter
+    .EXAMPLE
+        Get-Interpreter -LanguageName "JavaScript"
+    .NOTES
+        This command accepts open-ended input.
+    #>
+    [Alias('Get-Interpreters','PSInterpreter','PSInterpreters')]
+    [CmdletBinding(PositionalBinding=$false)]
+    param(
+    # The name of one or more languages.
+    [vbn()]
+    [string[]]
+    $LanguageName,
+
+    # Any remaining arguments.
+    [Parameter(ValueFromRemainingArguments)]
+    [Alias('Args','Arguments','ArgList')]
+    [PSObject[]]
+    $ArgumentList,
+
+    # Any input object.
+    [vfp()]
+    [PSObject]
+    $InputObject
+    )
+
+    process {
+        foreach ($arg in $ArgumentList) {
+            if ($arg -is [string] -and $LanguageName -notcontains $arg) {
+                $LanguageName += $arg
+            }
+        }
+        
+        $myVerb, $myNoun = $MyInvocation.InvocationName -split '-', 2
+        if (-not $myNoun) {
+            $myNoun = $myVerb
+            $myVerb = "Get"
+        }
+
+        switch ($myVerb) {
+            Get {
+                if ($LanguageName) {
+            
+                    $PSInterpreters[$LanguageName]
+                } else {
+                    $PSInterpreters
+                }
+            }
+            default {
+                foreach ($arg in $ArgumentList) {
+                    if ($LanguageName) {
+                        $PSInterpreters[$LanguageName]
+                    }
+                }
+
+                
+            }
+        }
+        
+    }
+}
