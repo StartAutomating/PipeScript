83d802cc3d9046308fa5552d0d0b8898f4da5756
StartAutomating <StartAutomating@github.com>
2024-02-07 19:18:53 +0000

    feat: Get-Interpreter ( Fixes #747 )
        Simplifying implementation

---
+++ Tests/Examples/Get-Interpreter.examples.tests.ps1
@@ -0,0 +1,10 @@
+
+describe 'Get-Interpreter' {
+    it 'Get-Interpreter Example 1' {
+        Get-Interpreter
+    }
+    it 'Get-Interpreter Example 2' {
+        Get-Interpreter -LanguageName "JavaScript"
+    }
+}
+
