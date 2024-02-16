ed738fda97c8149b08b0ddbf700d3e2ac9884bdc
James Brundage <@github.com>
2024-02-16 14:00:39 -0800

    feat: C3 Language Definition ( Fixes #870 )

---
+++ Languages/C3/C3-Language.ps.ps1
@@ -0,0 +1,36 @@
+Language function C3 {
+    <#
+    .SYNOPSIS
+        PipeScript C3 Language Definition.
+    .DESCRIPTION
+        Allows PipeScript to generate C3 files.
+
+        Multiline comments with /*{}*/ will be treated as blocks of PipeScript.
+
+        Multiline comments can be preceeded or followed by 'empty' syntax, which will be ignored.
+
+        The C3 will consider the following syntax to be empty:
+
+        * ```null```
+        * ```""```
+        * ```''```
+    #>
+    [ValidatePattern('\.c3$')]
+    param(
+    )
+
+    $FilePattern = '\.c3$'
+
+    # We start off by declaring a number of regular expressions:
+    $startComment = '/\*' # * Start Comments ```\*```
+    $endComment   = '\*/' # * End Comments   ```/*```
+    $Whitespace   = '[\s\n\r]{0,}'
+    # * IgnoredContext ```String.empty```, ```null```, blank strings and characters
+    $IgnoredContext = "(?<ignore>(?>$("null", '""', "''" -join '|'))\s{0,}){0,1}"
+    # * StartRegex     ```$IgnoredContext + $StartComment + '{' + $Whitespace```
+    $StartPattern = "(?<PSStart>${IgnoredContext}${startComment}\{$Whitespace)"
+    # * EndRegex       ```$whitespace + '}' + $EndComment + $ignoredContext```
+    $EndPattern   = "(?<PSEnd>$Whitespace\}${endComment}\s{0,}${IgnoredContext})"
+
+    $Compiler = 'c3c'
+}
