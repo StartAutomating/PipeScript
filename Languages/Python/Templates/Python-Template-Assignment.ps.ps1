5dbcb98f948b7d2ad971a654ef3ff04cb759dab7
James Brundage <@github.com>
2024-02-17 13:51:32 -0800

    feat: Template.Assignment.py ( Fixes #927 )

---
+++ Languages/Python/Templates/Python-Template-Assignment.ps.ps1
@@ -0,0 +1,36 @@
+Template function Assignment.py {
+    <#
+    .SYNOPSIS
+        Template for a Python assignment
+    .DESCRIPTION
+        Template for Python assignment statements.
+    .EXAMPLE
+        Template.Assignment.py -Left "a" -Right 1
+    #>    
+    param(    
+    # The variable name.
+    [vbn()]
+    [Alias('Left','Declaration')]
+    [string]
+    $VariableName,
+
+    # The value expression
+    [Alias('Right','Expression','Initializer')]    
+    [string]
+    $ValueExpression,
+
+    # The assignment operator.  By default, =.    
+    [string]
+    $AssignmentOperator = '=',
+
+    # The indentation level.  By default, 0
+    [int]
+    $Indent = 0
+    )
+
+    process {
+        "$(' ' * $indent)${VariableName}${AssignmentOperator} $ValueExpression"
+    }
+}
+
+
