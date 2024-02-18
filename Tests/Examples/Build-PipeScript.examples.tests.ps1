e3653b8eef58c6120d263429394a9972681a6c0a
StartAutomating <StartAutomating@github.com>
2024-02-18 23:09:08 +0000

    docs: Updating Python Example
        Removing Should

--- Tests/Examples/Build-PipeScript.examples.tests.ps1
+++ Tests/Examples/Build-PipeScript.examples.tests.ps1
@@ -1,7 +1,7 @@
 
 describe 'Build-PipeScript' {
     it 'Build-PipeScript Example 1' {
-        Export-PipeScript -Serial   # (PipeScript builds in parallel by default)
+        Export-PipeScript # (PipeScript can build in parallel)
     }
 }
 
