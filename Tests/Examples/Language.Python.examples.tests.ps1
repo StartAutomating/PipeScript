45138d60a445138c36b8f2c440297a2b16bf1bcf
StartAutomating <StartAutomating@github.com>
2024-02-18 23:47:39 +0000

    fix: Not re-importing in ExamplesShouldWork

--- Tests/Examples/Language.Python.examples.tests.ps1
+++ Tests/Examples/Language.Python.examples.tests.ps1
@@ -1,23 +1,12 @@
 
 describe 'Language.Python' {
     it 'Language.Python Example 1' {
-    .> {
-       $pythonContent = @'
-"""{
-$msg = "Hello World", "Hey There", "Howdy" | Get-Random
-@"
-print("$msg")
-"@
-}"""
-'@
-        [OutputFile('.\HelloWorld.ps1.py')]$PythonContent
-    }
-
-    .> .\HelloWorld.ps1.py
+    'print("Hello World")' > .\HelloWorld.py
+    Invoke-PipeScript .\HelloWorld.py
     }
     it 'Language.Python Example 2' {
-    'print("Hello World")' > .\HelloWorld.py
-    Invoke-PipeScript .\HelloWorld.py |  Should -Be 'Hello World'
+    Template.HelloWorld.py -Message "Hi" | Set-Content ".\Hi.py"
+    Invoke-PipeScript .\Hi.py
     }
 }
 
