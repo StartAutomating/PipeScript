7567cbeabb8c786414ad89bb79518e5b1849727b
StartAutomating <StartAutomating@github.com>
2024-03-06 22:57:03 +0000

    feat: Template.Docker.LabelModule ( Fixes #969 )
        Also, making the docker file names a bit nicer

---
+++ Languages/Docker/Templates/Docker-Template-InstallModule.ps1
@@ -0,0 +1,35 @@
+[ValidatePattern("docker")]
+param()
+
+
+function Template.Docker.InstallModule {
+
+    <#
+    .SYNOPSIS
+        Template for installing a PowerShell module in a Dockerfile.
+    .DESCRIPTION
+        A Template for installing a PowerShell module in a Dockerfile.
+    #>
+    param(
+    # The module to install.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]
+    $ModuleName,
+    
+    # The version of the module to install.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [Version]
+    $ModuleVersion
+    )
+    
+    process {
+        if ($ModuleVersion) {
+            "RUN pwsh -Command Install-Module -Name '$($ModuleName -replace "'","''")' -RequiredVersion '$ModuleVersion' -Force -SkipPublisherCheck -AcceptLicense"
+        } else {
+            "RUN pwsh -Command Install-Module -Name '$($ModuleName -replace "'","''")' -Force -SkipPublisherCheck -AcceptLicense"
+        }        
+    }
+
+}
+
+
