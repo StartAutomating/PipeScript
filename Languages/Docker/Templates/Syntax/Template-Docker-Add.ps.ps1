4f214a93ea178ca8a5e7f36935361429bc166e54
James Brundage <@github.com>
2024-03-05 18:52:37 -0800

    feat: Template.Docker.Add ( Fixes #946 )

---
+++ Languages/Docker/Templates/Syntax/Template-Docker-Add.ps.ps1
@@ -0,0 +1,53 @@
+Template function Docker.Add {
+    <#
+    .SYNOPSIS
+        Template for adding files to a Docker image.
+    .DESCRIPTION
+        A Template for adding files to a Docker image.
+    .LINK
+        https://docs.docker.com/engine/reference/builder/#add        
+    #>
+    [Alias('Template.Docker.NewItem')]
+    param(
+    # The source of the file to add.
+    [vbn()]
+    [string]
+    $Source,
+    
+    # The destination of the file to add.
+    [vbn()]
+    [string]
+    $Destination,
+
+    # Keep git directory
+    [vbn()]
+    [switch]
+    $KeepGit,
+
+    # Verify the checksum of the file
+    [vbn()]
+    [string]
+    $Checksum,
+
+    # Change the owner permissions
+    [Alias('Chown')]
+    [vbn()]
+    [string]
+    $ChangeOwner
+    )
+    
+    process {
+        $AddOptions = @(
+            if ($KeepGit) {
+                "--keep-git-dir=true"
+            }
+            if ($Checksum) {
+                "--checksum=$Checksum"            
+            }
+            if ($ChangeOwner) {
+                "--chown=$ChangeOwner"
+            }
+        ) -join ' '
+        "ADD $addOptions $Source $Destination" -replace '\s{2,}', ' '
+    }
+}
