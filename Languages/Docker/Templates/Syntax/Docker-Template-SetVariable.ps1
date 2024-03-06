67aa8100fc3972f031a29ef5e4d5f0a98f7e5f8d
StartAutomating <StartAutomating@github.com>
2024-03-06 22:57:04 +0000

    feat: Template.Docker.LabelModule ( Fixes #969 )
        Also, making the docker file names a bit nicer

---
+++ Languages/Docker/Templates/Syntax/Docker-Template-SetVariable.ps1
@@ -0,0 +1,33 @@
+[ValidatePattern("docker")]
+param()
+
+
+function Template.Docker.SetVariable {
+
+    <#
+    .SYNOPSIS
+        Template for setting a variable in a Dockerfile.
+    .DESCRIPTION
+        A Template for setting a variable in a Dockerfile.
+    .LINK
+        https://docs.docker.com/engine/reference/builder/#env        
+    #>
+    [Alias('Template.Docker.SetEnvironmentVariable','Template.Docker.SetEnvironment','Template.Docker.Env')]
+    param(
+    # The name of the variable to set.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]
+    $Name,
+    
+    # The value to set the variable to.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]
+    $Value
+    )
+    
+    process {
+        "ENV $Name $Value"
+    }
+
+}
+
