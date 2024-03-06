887b7463f58d34ff5333a2860332c5988003b651
James Brundage <@github.com>
2024-03-05 20:32:37 -0800

    feat: Template.Docker.InstallModule ( Fixes #963 )

---
+++ Languages/Docker/Templates/Template-Docker-InstallModule.ps.ps1
@@ -0,0 +1,27 @@
+Template function Docker.InstallModule {
+    <#
+    .SYNOPSIS
+        Template for installing a PowerShell module in a Dockerfile.
+    .DESCRIPTION
+        A Template for installing a PowerShell module in a Dockerfile.
+    #>
+    param(
+    # The module to install.
+    [vbn()]
+    [string]
+    $ModuleName,
+    
+    # The version of the module to install.
+    [vbn()]
+    [Version]
+    $ModuleVersion
+    )
+    
+    process {
+        if ($ModuleVersion) {
+            "RUN pwsh -Command Install-Module -Name '$($ModuleName -replace "'","''")' -RequiredVersion '$ModuleVersion' -Force -SkipPublisherCheck -AcceptLicense"
+        } else {
+            "RUN pwsh -Command Install-Module -Name '$($ModuleName -replace "'","''")' -Force -SkipPublisherCheck -AcceptLicense"
+        }        
+    }
+}
