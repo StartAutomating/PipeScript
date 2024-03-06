6e65782f3071e79788a69d1361a8172f3d29a6aa
James Brundage <@github.com>
2024-03-05 19:20:14 -0800

    feat: Template.Docker.SetVariable ( Fixes #951 )

---
+++ Languages/Docker/Templates/Syntax/Template-Docker-SetVariable.ps.ps1
@@ -0,0 +1,26 @@
+Template function Docker.SetVariable {
+    <#
+    .SYNOPSIS
+        Template for setting a variable in a Dockerfile.
+    .DESCRIPTION
+        A Template for setting a variable in a Dockerfile.
+    .LINK
+        https://docs.docker.com/engine/reference/builder/#env        
+    #>
+    [Alias('Template.Docker.SetEnvironmentVariable','Template.Docker.SetEnvironment','Template.Docker.Env')]
+    param(
+    # The name of the variable to set.
+    [vbn()]
+    [string]
+    $Name,
+    
+    # The value to set the variable to.
+    [vbn()]
+    [string]
+    $Value
+    )
+    
+    process {
+        "ENV $VariableName $VariableValue"
+    }
+}
