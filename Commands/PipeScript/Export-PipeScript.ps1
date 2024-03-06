dcb61cda1b4b6ac8efc8a9c7eb03bae1aed4c8a7
James Brundage <@github.com>
2024-03-06 10:46:39 -0800

    fix: Reducing Verbosity of Conditional Builds ( re #907, re #914 )

--- Commands/PipeScript/Export-PipeScript.ps1
+++ Commands/PipeScript/Export-PipeScript.ps1
@@ -117,14 +117,13 @@ function Export-Pipescript {
                 if ($commandAttribute.RegexPattern) {
                     if ($env:GITHUB_STEP_SUMMARY) {
                         @(
-                            "* $($CommandInfo) has a Build Validation Pattern: (``$($commandAttribute.RegexPattern)``)."
-                            "* $($CommandInfo) Validating Against: ``$ValidateAgainstString``"
+                            "* $($CommandInfo) has a Build Validation Pattern: (``$($commandAttribute.RegexPattern)``)."                            
                         ) -join [Environment]::Newline | Out-File -Path $env:GITHUB_STEP_SUMMARY -Append
                     }
                     $validationPattern = [Regex]::new($commandAttribute.RegexPattern, $commandAttribute.Options, '00:00:00.1')
                     if (-not $validationPattern.IsMatch($ValidateAgainstString)) {
                         if ($env:GITHUB_STEP_SUMMARY) {
-                            "* 🖐️ Skipping $($CommandInfo) because $ValidateAgainstString did not match ($($commandAttribute.RegexPattern))" | Out-File -Path $env:GITHUB_STEP_SUMMARY -Append
+                            "* 🖐️ Skipping $($CommandInfo) because commit did not match ($($commandAttribute.RegexPattern))" | Out-File -Path $env:GITHUB_STEP_SUMMARY -Append
                         }
                         Write-Warning "Skipping $($CommandInfo) : Did not match $($validationPattern)"
                         return $false
