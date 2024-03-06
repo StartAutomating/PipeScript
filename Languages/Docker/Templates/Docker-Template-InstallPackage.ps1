01eabc8c53b4422e233b926ec85cfd5f71f099e3
StartAutomating <StartAutomating@github.com>
2024-03-06 22:57:03 +0000

    feat: Template.Docker.LabelModule ( Fixes #969 )
        Also, making the docker file names a bit nicer

---
+++ Languages/Docker/Templates/Docker-Template-InstallPackage.ps1
@@ -0,0 +1,25 @@
+[ValidatePattern("docker")]
+param()
+
+
+function Template.Docker.InstallPackage {
+
+    <#
+    .SYNOPSIS
+        Template for installing a package in a Dockerfile.
+    .DESCRIPTION
+        A Template for installing a package in a Dockerfile.
+    #>
+    param(
+    # The package to install.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string[]]
+    $PackageName
+    )
+    
+    process {
+        "RUN apt-get update && apt-get install -y $($PackageName -join ' ')"
+    }
+
+}
+
