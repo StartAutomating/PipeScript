198cde59de16dd34817d572a39ec97042016191b
StartAutomating <StartAutomating@github.com>
2024-03-06 03:32:36 +0000

    feat: Template.Docker.From ( Fixes #953 )

---
+++ Languages/Docker/Templates/Syntax/Template-Docker-From.ps1
@@ -0,0 +1,25 @@
+
+function Template.Docker.From {
+
+    <#
+    .SYNOPSIS
+        Template for a Dockerfile FROM statement.
+    .DESCRIPTION
+        A Template for a Dockerfile FROM statement.
+    .LINK
+        https://docs.docker.com/engine/reference/builder/#from
+    #>
+    param(
+    # The base image to use.  By default, mcr.microsoft.com/powershell.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]
+    $BaseImage = 'mcr.microsoft.com/powershell'
+    )
+    
+    process {
+        "FROM $BaseImage"
+    }
+
+}
+
+
