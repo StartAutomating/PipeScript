ef3868b4f70046c2c34de6364a436650c634e614
StartAutomating <StartAutomating@github.com>
2024-03-06 22:57:03 +0000

    feat: Template.Docker.LabelModule ( Fixes #969 )
        Also, making the docker file names a bit nicer

---
+++ Languages/Docker/Templates/Syntax/Docker-Template-Expose.ps1
@@ -0,0 +1,32 @@
+[ValidatePattern("docker")]
+param()
+
+
+function Template.Docker.Expose {
+
+    <#
+    .SYNOPSIS
+        Template for exposing a port in a Dockerfile.
+    .DESCRIPTION
+        A Template for exposing a port in a Dockerfile.
+    .LINK
+        https://docs.docker.com/engine/reference/builder/#expose
+    #>
+    param(
+    # The port to expose. By default, 80.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [int]
+    $Port = 80,
+
+    # The protocol to expose. By default, tcp.
+    [ValidateSet('tcp','udp')]
+    [string]
+    $Protocol = 'tcp'
+    )
+    
+    process {        
+        "EXPOSE $Port/$Protocol"
+    }
+
+}
+
