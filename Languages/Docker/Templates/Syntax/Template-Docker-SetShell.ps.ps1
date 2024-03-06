40957a29a9abf4264224cb8b4bbed52db2bd0afc
James Brundage <@github.com>
2024-03-05 19:52:57 -0800

    feat: Template.Docker.SetShell ( Fixes #958 )

---
+++ Languages/Docker/Templates/Syntax/Template-Docker-SetShell.ps.ps1
@@ -0,0 +1,28 @@
+[ValidatePattern("docker")]
+param()
+
+Template function Docker.SetShell {
+    <#
+    .SYNOPSIS
+        Template for setting the shell in a Dockerfile.
+    .DESCRIPTION
+        A Template for setting the shell in a Dockerfile.
+    .LINK
+        https://docs.docker.com/engine/reference/builder/#shell        
+    #>
+    [Alias('Template.Docker.Shell')]
+    param(
+    # The shell to set.
+    [vbn()]
+    [string]
+    $Shell,
+
+    # The arguments to pass to the shell.
+    [string[]]
+    $Argument
+    )
+    
+    process {
+        "SHELL [`"$(@(@($Shell) + $Argument) -replace '"', '\"')  -join '", "'`"]"
+    }
+}
