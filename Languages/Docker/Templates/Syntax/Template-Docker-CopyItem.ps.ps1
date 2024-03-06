a5747df52700fc95172473e5ba90f2a0eedb847b
James Brundage <@github.com>
2024-03-05 19:05:52 -0800

    feat: Template.Docker.Copy ( Fixes #949 )

---
+++ Languages/Docker/Templates/Syntax/Template-Docker-CopyItem.ps.ps1
@@ -0,0 +1,27 @@
+Template function Docker.CopyItem {
+    <#
+    .SYNOPSIS
+        Template for copying an item in a Dockerfile.
+    .DESCRIPTION
+        A Template for copying an item in a Dockerfile.
+    .LINK
+        https://docs.docker.com/engine/reference/builder/#copy
+    #>
+    [Alias('Template.Docker.Copy')]
+    param(
+    # The source item to copy.
+    [vbn()]
+    [Alias('SourcePath','SourceItem')]
+    [string]
+    $Source,
+    
+    # The destination to copy the item to.
+    [vbn()]
+    [string]
+    $Destination
+    )
+    
+    process {
+        "COPY $Source $Destination"
+    }
+}
