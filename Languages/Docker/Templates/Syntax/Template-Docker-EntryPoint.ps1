1d95127a57f32494b7d64b462911540cce337f1e
StartAutomating <StartAutomating@github.com>
2024-03-06 03:18:32 +0000

    feat: Template.Docker.EntryPoint ( Fixes #950 )

---
+++ Languages/Docker/Templates/Syntax/Template-Docker-EntryPoint.ps1
@@ -0,0 +1,35 @@
+
+function Template.Docker.EntryPoint {
+
+    <#
+    .SYNOPSIS
+        Template for a Dockerfile ENTRYPOINT statement.
+    .DESCRIPTION
+        A Template for a Dockerfile ENTRYPOINT statement.
+    .LINK
+        https://docs.docker.com/engine/reference/builder/#entrypoint
+    #>
+    param(
+    # The command to run when the container starts.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [string]
+    $Command,
+
+    # The arguments to pass to the command.
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [Alias('Arguments','Args','ArgumentList')]
+    [string[]]
+    $Argument
+    )
+    
+    process {
+        if ($Argument) {
+            "ENTRYPOINT $Command $Argument"
+        } else {
+            "ENTRYPOINT $Command"
+        }
+        
+    }
+
+}
+
