651afc66ef0f74a893f671c3eea63f4ac5f8d9e7
James Brundage <@github.com>
2024-03-05 19:15:14 -0800

    feat: Template.Docker.EntryPoint ( Fixes #950 )

---
+++ Languages/Docker/Templates/Syntax/Template-Docker-EntryPoint.ps.ps1
@@ -0,0 +1,31 @@
+Template function Docker.EntryPoint {
+    <#
+    .SYNOPSIS
+        Template for a Dockerfile ENTRYPOINT statement.
+    .DESCRIPTION
+        A Template for a Dockerfile ENTRYPOINT statement.
+    .LINK
+        https://docs.docker.com/engine/reference/builder/#entrypoint
+    #>
+    param(
+    # The command to run when the container starts.
+    [vbn()]
+    [string]
+    $Command,
+
+    # The arguments to pass to the command.
+    [vbn()]
+    [Alias('Arguments','Args','ArgumentList')]
+    [string[]]
+    $Argument
+    )
+    
+    process {
+        if ($Argument) {
+            "ENTRYPOINT $Command $Argument"
+        } else {
+            "ENTRYPOINT $Command"
+        }
+        
+    }
+}
