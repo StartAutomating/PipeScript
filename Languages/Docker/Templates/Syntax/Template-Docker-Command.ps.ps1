7ccfc306337ff6e66987a5b01e85c45427c9d255
James Brundage <@github.com>
2024-03-05 19:01:09 -0800

    feat: Template.Docker.Command ( Fixes #948 )

---
+++ Languages/Docker/Templates/Syntax/Template-Docker-Command.ps.ps1
@@ -0,0 +1,22 @@
+Template function Docker.Command {
+    <#
+    .SYNOPSIS
+        Template for running a command in a Dockerfile.
+    .DESCRIPTION
+        The CMD instruction sets the command to be executed when running a container from an image.
+
+        There can only be one command. If you list more than one command, only the last one will take effect.
+    .LINK
+        https://docs.docker.com/engine/reference/builder/#cmd
+    #>
+    param(
+    # The command to run.
+    [vbn()]
+    [string]
+    $Command
+    )
+    
+    process {
+        "CMD $Command"
+    }
+}
