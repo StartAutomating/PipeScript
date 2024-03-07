68f021bc75dfc0e9f433585590589fef169f8604
James Brundage <@github.com>
2024-03-06 16:26:32 -0800

    feat: Template.HTML.Element ( Fixes #972 )

---
+++ Languages/HTML/Templates/HTML-Template-Element.ps.ps1
@@ -0,0 +1,46 @@
+Template function HTML.Element {
+    <#
+    .SYNOPSIS
+        Template for an HTML element.
+    .DESCRIPTION
+        A Template for an HTML element.
+    .LINK
+        https://developer.mozilla.org/en-US/docs/Web/HTML/Element
+    #>    
+    param(
+    # The name of the element.
+    [vbn()]
+    [string]
+    $Name,
+    
+    # The attributes of the element.
+    [vbn()]
+    [Alias('Attribute')]
+    [PSObject]
+    $Attributes,
+    
+    # The content of the element.
+    [vbn()]
+    [string]
+    $Content
+    )
+    
+    process {
+        $attributesString = if ($Attributes) {
+            if ($attributes -is [Collections.IDictionary]) {
+                $attributes = [PSCustomObject]$attributes
+            }
+            @(foreach ($property in $attributes.PSObject.Properties) {
+                $propertyName = $property.Name -replace '([A-Z])', '-$1' -replace '^-', ''
+                $propertyValue = $property.Value
+                " $propertyName='$propertyValue'"
+            }) -join ''            
+        }
+
+        if ($content) {
+            "<${Name}${AttributeString} />" -replace "\s{1,}\/\>$", "/>"
+        } else {
+            "<${Name}${attributesString}>$Content</$Name>"
+        }                
+    }
+}
