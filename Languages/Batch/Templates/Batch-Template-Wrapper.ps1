c83060fb68242127ab1a5c73d3386ee8517660ca
StartAutomating <StartAutomating@github.com>
2024-03-08 00:20:05 +0000

    feat: Template.Batch.Wrapper ( Fixes #999 )

---
+++ Languages/Batch/Templates/Batch-Template-Wrapper.ps1
@@ -0,0 +1,49 @@
+[ValidatePattern("batch")]
+param()
+
+
+function Template.Batch.Wrapper {
+
+    <#
+    .Synopsis
+        Wraps PowerShell in a Windows Batch Script
+    .Description
+        Wraps PowerShell in a Windows Batch Script
+    #>
+    param(
+    [Parameter(Mandatory,ValueFromPipeline,ParameterSetName='ScriptInfo')]
+    [Management.Automation.ExternalScriptInfo]
+    $ScriptInfo,
+
+    [Parameter(Mandatory,ValueFromPipeline,ParameterSetName='ScriptBlock')]
+    [ScriptBlock]
+    $ScriptBlock,
+
+    # If set, will use Windows PowerShell core (powershell.exe).  If not, will use PowerShell Core (pwsh.exe)
+    [switch]
+    $WindowsPowerShell
+    )
+
+    process {
+        $powerShellExe = if (-not $WindowsPowerShell) { 'pwsh' } else { 'powershell'} 
+        switch ($PSCmdlet.ParameterSetName) 
+        {
+            ScriptBlock {
+@"
+@echo off
+$powerShellExe -noprofile -nologo -command "& {$($ScriptBlock -replace '"', '\"')} %* ; if (`$error.Count) { exit 1}"
+"@
+            
+            }
+            ScriptInfo {
+            @"
+@echo off
+$powerShellExe -noprofile -nologo -command "& '%~dp0$($ScriptInfo.Name)' %*; if (`$error.Count) { exit 1}"
+"@                
+            }        
+        }
+    }
+
+
+}
+
