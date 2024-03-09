7b4cd05ca30a61b5a206fbc6b61521e035b1a5f4
StartAutomating <StartAutomating@github.com>
2024-03-09 01:17:13 +0000

    feat: Out-JSON ( Fixes #727 )
        Adding Example

---
+++ Tests/Examples/Template.PipeScript.DoubleDot.examples.tests.ps1
@@ -0,0 +1,10 @@
+
+describe 'Template.PipeScript.DoubleDot' {
+    it 'Template.PipeScript.DoubleDot Example 1' {
+        Invoke-PipeScript { .. }
+    }
+    it 'Template.PipeScript.DoubleDot Example 2' {
+        Invoke-PipeScript { ^.. }
+    }
+}
+
