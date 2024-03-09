e9dc6babaee3d4952de079539e358ebd48b2805e
StartAutomating <StartAutomating@github.com>
2024-03-09 17:49:37 +0000

    fix: Fixing Rest Template Begin replication ( Fixes #991 )
        Also rebuilding examples

--- Tests/Examples/Template.PipeScript.ProxyCommand.examples.tests.ps1
+++ Tests/Examples/Template.PipeScript.ProxyCommand.examples.tests.ps1
@@ -1,17 +1,12 @@
 
 describe 'Template.PipeScript.ProxyCommand' {
     it 'Template.PipeScript.ProxyCommand Example 1' {
-        {
-            function [ProxyCommand<'Get-Process'>]GetProcessProxy {}
-        } | .>PipeScript
-    }
-    it 'Template.PipeScript.ProxyCommand Example 2' {
         ProxyCommand -CommandName Get-Process -RemoveParameter *
     }
-    it 'Template.PipeScript.ProxyCommand Example 3' {
+    it 'Template.PipeScript.ProxyCommand Example 2' {
         Invoke-PipeScript -ScriptBlock {[ProxyCommand('Get-Process')]param()}
     }
-    it 'Template.PipeScript.ProxyCommand Example 4' {
+    it 'Template.PipeScript.ProxyCommand Example 3' {
         Invoke-PipeScript -ScriptBlock {
             [ProxyCommand('Get-Process', 
                 RemoveParameter='*',
@@ -21,7 +16,7 @@ describe 'Template.PipeScript.ProxyCommand' {
                 param()
         }
     }
-    it 'Template.PipeScript.ProxyCommand Example 5' {
+    it 'Template.PipeScript.ProxyCommand Example 4' {
         { 
             function Get-MyProcess {
                 [ProxyCommand('Get-Process', 
