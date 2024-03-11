40b5bdf83a3f74580fd4ecea457c0b9836941467
James Brundage <@github.com>
2024-03-11 10:53:42 -0700

    docs: PipeScript.Module.Services Formatting ( Fixes #1022 )

---
+++ Types/PipeScript.Module.Services/PipeScript.Module.Services.Format.ps1
@@ -0,0 +1,29 @@
+Write-FormatView -TypeName PipeScript.Module.Services -Action {
+    Write-FormatViewExpression -ScriptBlock {
+        Show-Markdown -InputObject $_.'README.md'
+    }
+
+    Write-FormatViewExpression -If { $_.Commands } -ScriptBlock {
+        "Commands:" + 
+        [Environment]::NewLine + 
+        ("* ") + (
+            $_.Commands -join ([Environment]::NewLine + ("* "))
+        ) + [Environment]::NewLine
+    } -ForegroundColor Cyan
+
+    Write-FormatViewExpression -If { $_.Commands } -ScriptBlock {
+        "Types:" + 
+        [Environment]::NewLine + 
+        ("* ") + (
+            $_.Type -join ([Environment]::NewLine + ("* "))
+        ) + [Environment]::NewLine
+    } -ForegroundColor Green    
+    
+    Write-FormatViewExpression -If { $_.Variable } -ScriptBlock {
+        "Variable:" + 
+        [Environment]::NewLine + 
+        ("* ") + (
+            $_.Variable -join ([Environment]::NewLine + ("* "))
+        ) + [Environment]::NewLine
+    } -ForegroundColor Blue
+}
