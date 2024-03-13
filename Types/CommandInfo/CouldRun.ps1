4d633305bd563e953d27fc67b4a6afaec1d2906d
James Brundage <@github.com>
2024-03-13 11:40:45 -0700

    feat: CommandInfo.CouldRun accepts psobjects ( Fixes #1032 )

--- Types/CommandInfo/CouldRun.ps1
+++ Types/CommandInfo/CouldRun.ps1
@@ -1,16 +1,39 @@
-param([Collections.IDictionary]$params, [string]$ParameterSetName)
+<#
+.SYNOPSIS
+    Determines if a command Could Run
+.DESCRIPTION
+    Determines if a command Could Run, given a set of parameters (and, optionally, a parameter set name).
+#>
+param(
+    # The parameters to check.    
+    [Alias('Parameters','Param')]
+    [PSObject]
+    $Parameter, 
+
+    # The parameter set name.
+    [string]
+    $ParameterSetName
+)
+
+if ($Parameter -isnot [Collections.IDictionary]) {
+    $parameterDictionary = [Ordered]@{}
+    foreach ($prop in $Parameter.psobject.properties) {
+        $parameterDictionary[$prop.Name] = $prop.Value
+    }
+    $parameter = $parameterDictionary
+}
 
 :nextParameterSet foreach ($paramSet in $this.ParameterSets) {
     if ($ParameterSetName -and $paramSet.Name -ne $ParameterSetName) { continue }
     $mappedParams = [Ordered]@{} # Create a collection of mapped parameters
     $mandatories  =  # Walk thru each parameter of this command
         @(foreach ($myParam in $paramSet.Parameters) {
-            if ($params.Contains($myParam.Name)) { # If this was in Params,
-                $mappedParams[$myParam.Name] = $params[$myParam.Name] # then map it.
+            if ($parameter.Contains($myParam.Name)) { # If this was in Params,
+                $mappedParams[$myParam.Name] = $parameter[$myParam.Name] # then map it.
             } else {
                 foreach ($paramAlias in $myParam.Aliases) { # Otherwise, check the aliases
-                    if ($params.Contains($paramAlias)) { # and map it if the parameters had the alias.
-                        $mappedParams[$myParam.Name] = $params[$paramAlias]
+                    if ($parameter.Contains($paramAlias)) { # and map it if the parameters had the alias.
+                        $mappedParams[$myParam.Name] = $parameter[$paramAlias]
                         break
                     }
                 }
