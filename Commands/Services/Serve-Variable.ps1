d60e9f16f1c6f6ac0acaeed5f03a88a0692a11af
StartAutomating <StartAutomating@github.com>
2024-03-14 21:53:50 +0000

    fix: Serve.Variable Pattern Fix ( Fixes #1037 )
        Regex escaping $

--- Commands/Services/Serve-Variable.ps1
+++ Commands/Services/Serve-Variable.ps1
@@ -14,7 +14,7 @@ function Serve.Variable {
         Additionally, it will not allow the variable to be changed (only viewed)
     #>
     [ValidatePattern(        
-        '/$($this.Variable -replace "^","`$?")/?', Options ='IgnoreCase,IgnorePatternWhitespace'
+        '/$($this.Variable -replace "^","\`$?")/?', Options ='IgnoreCase,IgnorePatternWhitespace'
     )]
     param(
     # The list of variables to serve
