f3335187393b350f9ef4daa9bbe461ef51a93279
StartAutomating <StartAutomating@github.com>
2024-03-19 18:52:47 +0000

    fix: HTML Custom Element Template Attributes ( Fixes #973 )
        Improving Observable Attribute Default

--- Languages/HTML/Templates/Controls/HTML-Template-CustomElement.ps1
+++ Languages/HTML/Templates/Controls/HTML-Template-CustomElement.ps1
@@ -117,8 +117,8 @@ function Template.HTML.CustomElement {
             $ClassName = $ElementName -replace '-','_'
         }
 
-        $Field += @{"#shadow" = "this.attachShadow({mode: 'open'});"}        
-
+        $Field += @{"#shadow" = "this.attachShadow({mode: 'open'});"}
+        
         $allMembers = @(
             if ($field) {
                 foreach ($PropertyBag in @($Field)) {
@@ -140,12 +140,20 @@ function Template.HTML.CustomElement {
             if ($OnAdopted) {
                 "adoptedCallback() { $OnAdopted }"
             }
-            if ($OnAttributeChange) {
-                "attributeChangedCallback(name, oldValue, newValue) { $OnAttributeChange }"
-            }            
             if ($ObservableAttribute) {
                 "static get observedAttributes() { return $(ConvertTo-Json -InputObject $ObservableAttribute -Compress) }"
             }
+            if ($OnAttributeChange) {
+                "attributeChangedCallback(name, oldValue, newValue) { $OnAttributeChange }"
+            } elseif ($ObservableAttribute) {
+                "attributeChangedCallback(name, oldValue, newValue) { 
+                    this.setAttribute(name, newValue);
+                    if (this[name.replace('-','_')] && this[name.replace('-','_')] != newValue) {
+                        this[name.replace('-','_')] = newValue;
+                    }
+                }"
+            }
+            
             if ($property) {
                 foreach ($PropertyBag in @($Property)) {
                     if ($PropertyBag -is [Collections.IDictionary]) {
