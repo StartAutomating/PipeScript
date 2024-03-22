40f9a650795b61315bb0f05c735cdc304886b811
James Brundage <@github.com>
2024-03-22 15:42:00 -0700

    fix: PSModuleInfo.Site - Fixing Mirrors ( re #1038 )

--- Types/PSModuleInfo/get_Site.ps1
+++ Types/PSModuleInfo/get_Site.ps1
@@ -14,32 +14,41 @@ if (-not $this.'.Website') {
     $CombinedSiteData = [Ordered]@{PSTypeName='PipeScript.Module.Website';Mirrors=@()}
     
     $firstPropertyBag = $true
+    $listOfUrls = @()
+    $theseWebsites = 
     foreach ($potentialSite in $this.FindMetadata('Site', 'Sites', 'Website', 'Websites')) {
         if ($potentialSite -is [string]) {
-            if (-not $CombinedSiteData.BaseUrl) {
-                $CombinedSiteData.Url = $potentialSite
-            } else {
-                $CombinedSiteData.Mirrors += $potentialSite                
-            }
+            $listOfUrls+= $potentialSite
         } else {
             if ($potentialSite -is [Collections.IDictionary]) {
-                $potentialSite = [PSCustomObject]$potentialSite
-                $potentialSite.pstypenames.insert(0, 'PipeScript.Module.Website')
-                $potentialSite.pstypenames.insert(0, "$this.Website")
+                $potentialSite = [PSCustomObject]$potentialSite            
             }
-            if (-not $firstPropertyBag) {
-                $CombinedSiteData.Mirrors += $potentialSite
-            } else {
-                $firstPropertyBag = $false
-                foreach ($prop in $potentialSite.psobject.properties) {
-                    $CombinedSiteData[$prop.Name] = $prop.Value
+            $potentialSite.pstypenames.insert(0, 'PipeScript.Module.Website')
+            $potentialSite.pstypenames.insert(0, "$this.Website")
+            if ($listOfUrls) {
+                $firstUrl, $allOtherUrls = $listOfUrls
+                if (-not $potentialSite.Url) {
+                    $potentialSite.psobject.properties.add([psnoteproperty]::new("Url", $firstUrl))                    
+                } else {
+                    $allOtherUrls = $listOfUrls
                 }
+                if ($allOtherUrls) {
+                    if ($potentialSite.Mirrors) {
+                        $potentialSite.Mirrors = @($potentialSite.Mirrors) +  $allOtherUrls
+                    } elseif ($potentialSite.Mirror) {
+                        $potentialSite.Mirror = @($potentialSite.Mirror) +  $allOtherUrls
+                    }
+                    else {
+                        $potentialSite.psobject.properties.add([psnoteproperty]::new("Mirrors", $allOtherUrls))
+                    }                    
+                }
+                $listOfUrls = @()
             }
+            $potentialSite            
         }
     }
-    $CombinedSiteData = [PSCustomObject]$CombinedSiteData
-    $CombinedSiteData.pstypenames.insert(0, "$this.Website")
-    $this | Add-Member NoteProperty '.Website' $CombinedSiteData -Force    
+
+    $this | Add-Member NoteProperty ".Website" $theseWebsites -Force
 }
 
 return $this.'.Website'
